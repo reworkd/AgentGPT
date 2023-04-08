5c53635c1d3eb4a906165d35155af10914186972
Asim Shrestha <asim.shrestha@hotmail.com>
2023-04-08 10:35:27 -0700

    🔥 Better screen breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,19 @@
 /** @type {import("tailwindcss").Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        "xs": "350px",
-        "sm-h": { "raw": "(min-height: 700px)" },
-        "md-h": { "raw": "(min-height: 900px)" },
-        "lg-h": { "raw": "(min-height: 1000px)" }
-      }
-    }
+    screens: {
+      "xs": "300px",
+
+      "sm-h": { "raw": "(min-height: 700px)" },
+      "md-h": { "raw": "(min-height: 900px)" },
+      "lg-h": { "raw": "(min-height: 1000px)" },
+
+      ...defaultTheme.screens
+    },
+    extend: {}
   },
   plugins: []
 };

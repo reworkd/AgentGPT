41a9f7d14da35289e6b57367f91407910ab4db4e
uv-07 <63532070+uv-07@users.noreply.github.com>
2023-04-12 21:49:29 +0530

    restyled layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -91,7 +91,7 @@ const Home: NextPage = () => {
         >
           <div
             id="layout"
-            className="flex h-full w-full max-w-screen-lg flex-col items-center justify-between gap-3 py-5 md:justify-center"
+            className="flex h-full w-full max-w-screen-lg overflow-hidden flex-col items-center justify-between gap-3 py-5 md:justify-center"
           >
             <div
               id="title"

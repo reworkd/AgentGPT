e01ca7fcc66ef912ad24830c617ebbd50c1899dc
awtkns <hello@awtkns.com>
2023-04-15 07:19:01 +0300

    Add discord provider

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -26,6 +26,8 @@ export const serverSchema = z.object({
   GOOGLE_CLIENT_SECRET: requiredForProduction(),
   GITHUB_CLIENT_ID: requiredForProduction(),
   GITHUB_CLIENT_SECRET: requiredForProduction(),
+  DISCORD_CLIENT_ID: requiredForProduction(),
+  DISCORD_CLIENT_SECRET: requiredForProduction(),
 });
 
 /**
@@ -43,6 +45,8 @@ export const serverEnv = {
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  DISCORD_CLIENT_ID:  process.env.DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET
 };
 
 /**

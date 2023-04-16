f2740da8250ca86454930c8de1a1e5b46d752614
awtkns <hello@awtkns.com>
2023-04-16 17:16:03 +0300

    bug: improve task styling

--- src/components/TaskWindow.tsx
+++ src/components/TaskWindow.tsx
@@ -10,7 +10,7 @@ type TaskWindowProps = {
 };
 export const TaskWindow = ({ tasks }: TaskWindowProps) => {
   return (
-    <Expand className="xl mx-2 mt-4 hidden max-h-[65vh] w-[20rem] flex-col items-center rounded-2xl border-2 border-white/20 bg-zinc-900 px-1 font-mono shadow-2xl xl:flex">
+    <Expand className="xl mx-2 mt-4 hidden max-h-[55vh] w-[20rem] flex-col items-center rounded-2xl border-2 border-white/20 bg-zinc-900 px-1 font-mono shadow-2xl xl:flex">
       <div className="sticky top-0 my-2 flex items-center justify-center gap-2 bg-zinc-900 p-2 text-gray-300 ">
         <FaListAlt /> Current tasks
       </div>

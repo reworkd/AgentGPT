7e7a82955b411a15d2fb4d9ed08047d3f51fdd10
matt <matt-hope@hotmail.com>
2023-04-17 19:58:35 -0400

    added key validation to setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 
-is_valid_key() {
+is_valid_sk_key() {
   local api_key=$1
   local pattern="^sk-[a-zA-Z0-9]{48}$"
   [[ $api_key =~ $pattern ]] && return 0 || return 1
 }
 
-echo -n "Enter your OpenAI Key (eg: sk...): "
+echo -n "Enter your OpenAI Key (eg: sk...) or press enter to continue with no key: "
 read OPENAI_API_KEY
 
-if is_valid_key $OPENAI_API_KEY; then
+if is_valid_sk_key $OPENAI_API_KEY || [ -z "$OPENAI_API_KEY" ]; then
   echo "Valid API key"
 else
-  echo "Invalid API key"
+  echo "Invalid API key. Please ensure that you have billing set up on your OpenAI account"
   exit
 fi
 

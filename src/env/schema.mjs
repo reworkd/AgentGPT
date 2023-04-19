344c17bf3b59593cf94f012efde170362a20c0ab
awtkns <hello@awtkns.com>
2023-04-19 08:38:06 +0200

    feat: add agent sharing

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -76,7 +76,7 @@ export const clientSchema = z.object({
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().optional(),
   NEXT_PUBLIC_FF_SUB_ENABLED: stringToBoolean(),
   NEXT_PUBLIC_FF_MOCK_MODE_ENABLED: stringToBoolean(),
-  NEXT_PUBLIC_VERCEL_URL: z.string().url().optional()
+  NEXT_PUBLIC_VERCEL_URL: z.string().optional()
 });
 
 /**

1cf053498f5a7d8ffcd5a2b2128285cc374a031a
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-04-19 19:41:22 +0200

    [mod] Prompts have been modified + inputVariables has been extended at each prompt with the customLanguage property

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -17,18 +17,18 @@ export const createModel = (settings: ModelSettings) =>
 
 export const startGoalPrompt = new PromptTemplate({
   template:
-    "You are an autonomous task creation AI called AgentGPT. You have the following objective `{goal}`. Create a list of zero to three tasks to be completed by your AI system such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse()",
-  inputVariables: ["goal"],
+    "You are an autonomous task creation AI called AgentGPT. You have the following objective `{goal}`. Create a list of zero to three tasks to be completed by your AI system such that your goal is more closely reached or completely reached. Use {customLanguage} code to translate your answer. Return the response as an array of strings that can be used in JSON.parse().",
+  inputVariables: ["goal", "customLanguage"],
 });
 
 export const executeTaskPrompt = new PromptTemplate({
   template:
-    "You are an autonomous task execution AI called AgentGPT. You have the following objective `{goal}`. You have the following tasks `{task}`. Execute the task and return the response as a string.",
-  inputVariables: ["goal", "task"],
+    "You are an autonomous task execution AI called AgentGPT. You have the following objective `{goal}`. You have the following tasks `{task}`. Execute the task and return the response as a string on {customLanguage} language.",
+  inputVariables: ["goal", "task", "customLanguage"],
 });
 
 export const createTasksPrompt = new PromptTemplate({
   template:
-    "You are an AI task creation agent. You have the following objective `{goal}`. You have the following incomplete tasks `{tasks}` and have just executed the following task `{lastTask}` and received the following result `{result}`. Based on this, create a new task to be completed by your AI system ONLY IF NEEDED such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse() and NOTHING ELSE",
-  inputVariables: ["goal", "tasks", "lastTask", "result"],
+    "You are an AI task creation agent. You have the following objective `{goal}`. You have the following incomplete tasks `{tasks}` and have just executed the following task `{lastTask}` and received the following result `{result}`. Based on this, create a new task to be completed by your AI system ONLY IF NEEDED such that your goal is more closely reached or completely reached. Return the response as an array of strings on {customLanguage} language that can be used in JSON.parse() and NOTHING ELSE",
+  inputVariables: ["goal", "tasks", "lastTask", "result", "customLanguage"],
 });

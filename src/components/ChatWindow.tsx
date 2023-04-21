ca3a0d2ca8b88e3ced0df648c534b98cd9a95bbd
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-04-21 18:32:57 +0200

    [-] duplicated Menu declaration removed

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -25,7 +25,6 @@ import WindowButton from "./WindowButton";
 import PDFButton from "./pdf/PDFButton";
 import FadeIn from "./motions/FadeIn";
 import Menu from "./Menu";
-import Menu from "./Menu";
 import type { Message } from "../types/agentTypes";
 import clsx from "clsx";
 
@@ -185,7 +184,7 @@ const MacWindowHeader = (props: HeaderProps) => {
       icon={<FaClipboard size={12} />}
       name={t('Copy')}
     />,
-    <PDFButton key="PDF" messages={props.messages} />,
+    <PDFButton key="PDF" name="PDF" messages={props.messages} />,
   ];
 
   return (

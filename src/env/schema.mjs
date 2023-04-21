4a71a350de21e89aba00644b226a9c3f937bbd9c
Huang Jianhui <109145789+belikers@users.noreply.github.com>
2023-04-21 13:22:28 +0800

    Ability to deploy projects to Vercel with one click (#293)
    * Ability to deploy projects to Vercel with one click
    
    * Filled in the missing Rate limiter

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -1,24 +1,24 @@
 // @ts-check
 import { z } from "zod";
 
-const requiredForProduction = () => process.env.NODE_ENV === "production"
+const requiredForProduction = () =>
+  process.env.NODE_ENV === "production"
     ? z.string().min(1).trim()
-    : z.string().min(1).trim().optional()
+    : z.string().min(1).trim().optional();
+
+const requiredAuthEnabledForProduction = () => {
+  return process.env.NODE_ENV === "production" &&
+    process.env.NEXT_PUBLIC_FF_AUTH_ENABLED === "true"
+    ? z.string().min(1).trim()
+    : z.string().min(1).trim().optional();
+};
 
 function stringToBoolean() {
-  return z.preprocess(
-      (str) => str === "true",
-      z.boolean(),
-  );
+  return z.preprocess((str) => str === "true", z.boolean());
 }
-
 function stringToNumber() {
-  return z.preprocess(
-    (str) => Number(str),
-    z.number(),
-  );
+  return z.preprocess((str) => Number(str), z.number());
 }
-
 /**
  * Specify your server-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
@@ -32,16 +32,16 @@ export const serverSchema = z.object({
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
+    process.env.VERCEL ? z.string() : z.string().url()
   ),
   OPENAI_API_KEY: z.string(),
 
-  GOOGLE_CLIENT_ID: requiredForProduction(),
-  GOOGLE_CLIENT_SECRET: requiredForProduction(),
-  GITHUB_CLIENT_ID: requiredForProduction(),
-  GITHUB_CLIENT_SECRET: requiredForProduction(),
-  DISCORD_CLIENT_ID: requiredForProduction(),
-  DISCORD_CLIENT_SECRET: requiredForProduction(),
+  GOOGLE_CLIENT_ID: requiredAuthEnabledForProduction(),
+  GOOGLE_CLIENT_SECRET: requiredAuthEnabledForProduction(),
+  GITHUB_CLIENT_ID: requiredAuthEnabledForProduction(),
+  GITHUB_CLIENT_SECRET: requiredAuthEnabledForProduction(),
+  DISCORD_CLIENT_ID: requiredAuthEnabledForProduction(),
+  DISCORD_CLIENT_SECRET: requiredAuthEnabledForProduction(),
 
   STRIPE_SECRET_KEY: z.string().optional(),
   STRIPE_WEBHOOK_SECRET: z.string().optional(),
@@ -67,7 +67,7 @@ export const serverEnv = {
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  DISCORD_CLIENT_ID:  process.env.DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 
   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
@@ -88,11 +88,15 @@ export const serverEnv = {
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
   NEXT_PUBLIC_VERCEL_ENV: z.enum(["production", "preview", "development"]),
+  NEXT_PUBLIC_STRIPE_DONATION_ENABLED: z
+    .string()
+    .transform((str) => str === "true")
+    .optional(),
   NEXT_PUBLIC_FF_AUTH_ENABLED: stringToBoolean(),
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().optional(),
   NEXT_PUBLIC_FF_SUB_ENABLED: stringToBoolean(),
   NEXT_PUBLIC_FF_MOCK_MODE_ENABLED: stringToBoolean(),
-  NEXT_PUBLIC_VERCEL_URL: z.string().optional()
+  NEXT_PUBLIC_VERCEL_URL: z.string().optional(),
 });
 
 /**
@@ -102,10 +106,15 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.input<typeof clientSchema>]: string | undefined }}
  */
 export const clientEnv = {
-  NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000",
   NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "development",
-  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  NEXT_PUBLIC_STRIPE_DONATION_ENABLED:
+    process.env.NEXT_PUBLIC_STRIPE_DONATION_ENABLED,
   NEXT_PUBLIC_FF_AUTH_ENABLED: process.env.NEXT_PUBLIC_FF_AUTH_ENABLED,
+  NEXT_PUBLIC_VERCEL_URL:
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000",
+  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   NEXT_PUBLIC_FF_SUB_ENABLED: process.env.NEXT_PUBLIC_FF_SUB_ENABLED,
-  NEXT_PUBLIC_FF_MOCK_MODE_ENABLED: process.env.NEXT_PUBLIC_FF_MOCK_MODE_ENABLED,
+  NEXT_PUBLIC_FF_MOCK_MODE_ENABLED:
+    process.env.NEXT_PUBLIC_FF_MOCK_MODE_ENABLED,
 };

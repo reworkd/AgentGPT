39306a93b443b3a5a7ba6a991ad6f017c907a612
Joe Shen <joeshen123@gmail.com>
2023-04-21 08:54:56 -0700

    feat: add combobox for export options (#292)
    * feat: add combobox for export options
    
    * hide AgentGPT title in chat window when screen size is small
    
    * include save agent button
    
    * implement menu for export buttons
    
    * clean up
    
    * display save Agent button separately

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -60,6 +60,16 @@ const Input = (props: InputProps) => {
         options={options}
         disabled={disabled}
         onChange={setValue}
+        styleClass={{
+          container: "relative w-full",
+          options:
+            "absolute right-0 top-full z-20 mt-1 max-h-48 w-full overflow-auto rounded-xl border-[2px] border-white/10 bg-[#3a3a3a] tracking-wider shadow-xl outline-0 transition-all",
+          input: `border:black delay-50 sm: flex w-full items-center justify-between rounded-xl border-[2px] border-white/10 bg-transparent px-2 py-2 text-sm tracking-wider outline-0 transition-all hover:border-[#1E88E5]/40 focus:border-[#1E88E5] sm:py-3 md:text-lg ${
+            disabled ? "cursor-not-allowed hover:border-white/10" : ""
+          } ${left ? "md:rounded-l-none" : ""}`,
+          option:
+            "cursor-pointer px-2 py-2 font-mono text-sm text-white/75 hover:bg-blue-500 sm:py-3 md:text-lg",
+        }}
       />
     );
   } else if (isTypeTextArea()) {

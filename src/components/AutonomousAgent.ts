21df2ac549af63e6253c29d100a4f319328e3517
awtkns <hello@awtkns.com>
2023-04-22 21:31:07 +0200

    bug: fix invalid custom key

--- src/components/AutonomousAgent.ts
+++ src/components/AutonomousAgent.ts
@@ -220,7 +220,7 @@ class AutonomousAgent {
   }
 
   private shouldRunClientSide() {
-    return this.modelSettings.customApiKey != "";
+    return !!this.modelSettings.customApiKey;
   }
 
   stopAgent() {
@@ -243,10 +243,9 @@ class AutonomousAgent {
   sendLoopMessage() {
     this.sendMessage({
       type: "system",
-      value:
-        this.modelSettings.customApiKey !== ""
-          ? `This agent has maxed out on loops. To save your wallet, this agent is shutting down. You can configure the number of loops in the advanced settings.`
-          : "We're sorry, because this is a demo, we cannot have our agents running for too long. Note, if you desire longer runs, please provide your own API key in Settings. Shutting down.",
+      value: !!this.modelSettings.customApiKey
+        ? `This agent has maxed out on loops. To save your wallet, this agent is shutting down. You can configure the number of loops in the advanced settings.`
+        : "We're sorry, because this is a demo, we cannot have our agents running for too long. Note, if you desire longer runs, please provide your own API key in Settings. Shutting down.",
     });
   }
 
@@ -307,7 +306,7 @@ const testConnection = async (modelSettings: ModelSettings) => {
     {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${modelSettings.customApiKey}`,
+        Authorization: `Bearer ${modelSettings.customApiKey ?? ""}`,
       },
     }
   );

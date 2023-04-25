176e82c6180f1186858f673747c34331d887e39e
awtkns <hello@awtkns.com>
2023-04-25 07:29:58 +0100

    bug: typesafe translations

--- src/hooks/useTypeSafeTranslation.ts
+++ src/hooks/useTypeSafeTranslation.ts
@@ -2,5 +2,5 @@ import { useTranslation } from "next-i18next";
 
 export function useTypeSafeTranslation() {
   const [t] = useTranslation();
-  return (key) => t(key) ?? key;
+  return (key: string) => t(key) ?? key;
 }

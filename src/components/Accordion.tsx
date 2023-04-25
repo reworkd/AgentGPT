55e499d96b6a3d11f9840da5db00ffb1bc797292
awtkns <hello@awtkns.com>
2023-04-25 07:12:42 +0100

    art: Improve setting styling

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -11,13 +11,15 @@ const Accordion = ({ child, name }: AccordionProps) => {
     <AccordionPrimitive>
       {({ open }) => (
         <>
-          <AccordionPrimitive.Button className="border:black delay-50 mb-1 flex w-full items-center justify-between rounded-xl bg-[#4a4a4a] px-3 py-2 text-sm tracking-wider outline-0 transition-all placeholder:text-white/20 hover:border-[#1E88E5]/40 hover:bg-[#6b6b6b] focus:border-[#1E88E5] focus-visible:ring sm:py-3 md:mb-3 md:text-lg">
+          <AccordionPrimitive.Button className="border:black delay-50 flex w-full items-center justify-between rounded-xl bg-[#4a4a4a] px-3 py-2 text-sm tracking-wider outline-0 transition-all placeholder:text-white/20 hover:border-[#1E88E5]/40 hover:bg-[#6b6b6b] focus:border-[#1E88E5] focus-visible:ring sm:py-2  md:text-lg">
             {name}
             <FaChevronDown
               className={`${open ? "rotate-180 transform" : ""} h-5 w-5`}
             />
           </AccordionPrimitive.Button>
-          <AccordionPrimitive.Panel>{child}</AccordionPrimitive.Panel>
+          <AccordionPrimitive.Panel className="rounded-xl bg-[#4a4a4a] p-2">
+            {child}
+          </AccordionPrimitive.Panel>
         </>
       )}
     </AccordionPrimitive>

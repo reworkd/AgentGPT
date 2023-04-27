30cd8c26fcd37310ec1aca0fc56828d55c67785e
awtkns <hello@awtkns.com>
2023-04-27 20:09:12 +0100

    :sparkles: remove thinking message

--- src/pages/agent/index.tsx
+++ src/pages/agent/index.tsx
@@ -42,7 +42,7 @@ const AgentPage: NextPage = () => {
       centered
     >
       <ChatWindow
-        messages={messages}
+        messages={messages.filter((m) => m.type !== "thinking")}
         title={getAgent?.data?.name}
         showDonation={false}
         className="min-h-[80vh] md:w-[80%]"

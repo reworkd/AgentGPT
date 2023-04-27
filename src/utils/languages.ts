eb444f7e11e8d1b449b6f5c616fcbd980142cd3b
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-04-27 12:21:08 +0200

    Translation updates

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,26 +1,24 @@
-import { i18n } from 'next-i18next';
-
 export type Language = {
   code: string;
   name: string;
   flag: string;
 };
 
-export const ENGLISH = { code: "en", name: `${i18n?.t('ENGLISH','ENGLISH',{ns:'languages'})}`, flag: "🇺🇸" };
+export const ENGLISH = { code: "en", name: "ENGLISH", flag: "🇺🇸" };
 
 export const languages: Language[] = [
   ENGLISH,
-  { code: "fr", name: `${i18n?.t('FRENCH','FRENCH',{ns:'languages'})}`, flag: "🇫🇷" },
-  { code: "es", name: `${i18n?.t('SPANISH','SPANISH',{ns:'languages'})}`, flag: "🇪🇸" },
-  { code: "de", name: `${i18n?.t('GERMAN','GERMAN',{ns:'languages'})}`, flag: "🇩🇪" },
-  { code: "ja", name: `${i18n?.t('JAPANESE','JAPANESE',{ns:'languages'})}`, flag: "🇯🇵" },
-  { code: "ko", name: `${i18n?.t('KOREAN','KOREAN',{ns:'languages'})}`, flag: "🇰🇷" },
-  { code: "zh", name: `${i18n?.t('CHINESE','CHINESE',{ns:'languages'})}`, flag: "🇨🇳" },
-  { code: "pt", name: `${i18n?.t('PORTUGEES','PORTUGEES',{ns:'languages'})}`, flag: "🇵🇹" },
-  { code: "it", name: `${i18n?.t('ITALIAN','ITALIAN',{ns:'languages'})}`, flag: "🇮🇹" },
-  { code: "nl", name: `${i18n?.t('DUTCH','DUTCH',{ns:'languages'})}`, flag: "🇳🇱" },
-  { code: "pl", name: `${i18n?.t('POLSKI','POLSKI',{ns:'languages'})}`, flag: "🇵🇱" },
-  { code: "hu", name: `${i18n?.t('HUNGARIAN','HUNGARIAN',{ns:'languages'})}`, flag: "🇭🇺" },
-  { code: "ro", name: `${i18n?.t('ROMANIAN','ROMANIAN',{ns:'languages'})}`, flag: "🇷🇴" },
-  { code: "sk", name: `${i18n?.t('SLOVAK','SLOVAK',{ns:'languages'})}`, flag: "🇸🇰" },
+  { code: "fr", name: "FRENCH", flag: "🇫🇷" },
+  { code: "es", name: "SPANISH", flag: "🇪🇸" },
+  { code: "de", name: "GERMAN", flag: "🇩🇪" },
+  { code: "ja", name: "JAPANESE", flag: "🇯🇵" },
+  { code: "ko", name: "KOREAN", flag: "🇰🇷" },
+  { code: "zh", name: "CHINESE", flag: "🇨🇳" },
+  { code: "pt", name: "PORTUGEES", flag: "🇵🇹" },
+  { code: "it", name: "ITALIAN", flag: "🇮🇹" },
+  { code: "nl", name: "DUTCH", flag: "🇳🇱" },
+  { code: "pl", name: "POLSKI", flag: "🇵🇱" },
+  { code: "hu", name: "HUNGARIAN", flag: "🇭🇺" },
+  { code: "ro", name: "ROMANIAN", flag: "🇷🇴" },
+  { code: "sk", name: "SLOVAK", flag: "🇸🇰" },
 ];

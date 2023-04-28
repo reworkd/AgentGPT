15d32be57a69de659e8f4dd16f6aa74488067c01
Asim Shrestha <asim.shrestha@hotmail.com>
2023-04-28 00:01:46 -0700

    ⚠ Fix build errors

--- src/components/WindowButton.tsx
+++ src/components/WindowButton.tsx
@@ -27,9 +27,9 @@ const WindowButton = ({
         onClick={onClick}
       >
         {ping ? (
-          <span class="absolute right-[-3px] top-[-3px] flex h-3 w-3">
-            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
-            <span class="relative inline-flex h-3 w-3 rounded-full bg-sky-500 opacity-90"></span>
+          <span className="absolute right-[-3px] top-[-3px] flex h-3 w-3">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex h-3 w-3 rounded-full bg-sky-500 opacity-90"></span>
           </span>
         ) : (
           <></>

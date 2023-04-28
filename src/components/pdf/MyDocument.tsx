4a45d39ac813798afee717bdfcdf7c0fdb63fba2
Asim Shrestha <asim.shrestha@hotmail.com>
2023-04-27 23:45:23 -0700

    ⚠ Fix PDF executions

--- src/components/pdf/MyDocument.tsx
+++ src/components/pdf/MyDocument.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Document, Font, Page, StyleSheet, Text } from "@react-pdf/renderer";
+import ReactPDF, {
+  Document,
+  Font,
+  Page,
+  StyleSheet,
+  Text,
+} from "@react-pdf/renderer";
+import View = ReactPDF.View;
 
 Font.register({
   family: "Roboto,SourceHanSansCN",
@@ -19,23 +26,35 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
     padding: 40,
   },
+  horizontalRule: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+    borderBottomStyle: "solid",
+  },
   section: {
     fontSize: 12,
     fontFamily: "Roboto,SourceHanSansCN",
-    marginBottom: 20,
+    marginVertical: 10,
     lineHeight: 1.5,
   },
 });
 
 // NOTE: This should only ever be imported dynamically to reduce load times
 const MyDocument: React.FC<{
-  content: string;
-}> = ({ content }) => (
+  textSections: string[];
+}> = ({ textSections }) => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <Text style={styles.section}>{content}</Text>
+      {textSections.map((text) => (
+        <>
+          <Text style={styles.section}>{text}</Text>
+          <HorizontalRule />
+        </>
+      ))}
     </Page>
   </Document>
 );
+
+const HorizontalRule: React.FC = () => <View style={styles.horizontalRule} />;
 
 export default MyDocument;

4287fa9f1ac266fe048195d6d26d35a67239e70d
Asim Shrestha <asim.shrestha@hotmail.com>
2023-04-28 23:39:49 -0700

    ✨ Fix reset

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useTranslation } from "next-i18next";
 import Button from "./Button";
 import {
   FaKey,
@@ -62,7 +61,6 @@ export const SettingsDialog: React.FC<{
 
   const handleReset = () => {
     customSettings.resetSettings();
-    updateSettings("customApiKey", "");
     close();
   };
 

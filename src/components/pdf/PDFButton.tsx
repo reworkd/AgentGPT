6d357da53f77b01491ae5a5325b1c5949109da62
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-05-06 14:56:34 +0200

    [fix] TypeError: Cannot read properties of undefined (reading 'Unicode') #450

--- src/components/pdf/PDFButton.tsx
+++ src/components/pdf/PDFButton.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import WindowButton from "../WindowButton";
 import { FaFilePdf } from "react-icons/fa";
 import jsPDF from "jspdf";
 import React, { memo, useState, useEffect } from "react";
 import type { Message } from "../../types/agentTypes";
 import { MESSAGE_TYPE_GOAL, MESSAGE_TYPE_TASK } from "../../types/agentTypes";
-import { i18n } from "next-i18next";
+import { useTranslation, i18n } from "next-i18next";
 
 const PDFButton = ({
   messages,
@@ -14,27 +15,35 @@ const PDFButton = ({
   name: string;
 }) => {
   const textSections = getTextSections(messages);
-  const [language, setLanguage] = useState<any>();
+  const [language, setLanguage] = useState<string>();
 
   useEffect(() => {
     setLanguage(i18n?.language);
-  },[i18n?.language])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [i18n?.language]);
 
-  const downloadPDF = async () => {
+  const downloadPDF = () => {
     const doc = new jsPDF();
     const fontUrl = {
       ru: "./fonts/russian.ttf",
       zh: "./fonts/chinese.ttf",
       ja: "./fonts/japanese.ttf",
-    }[language];
+    }[i18n?.language || "en"];
     if (fontUrl) {
       doc.addFont(fontUrl, "customfont", "normal");
       doc.setFont("customfont");
     }
     getTextSections(messages).forEach((text, index) => {
-      const splittedText = doc.splitTextToSize(text, 180);
-      doc.text(splittedText, 20, index * 10 + 10, { align: "left" });
+      if (!text.length) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        doc.text(doc.splitTextToSize(text, 180), 20, index * 10 + 10, {
+          align: "left",
+        });
+      } else {
+        doc.text("", 20, index * 10 + 10, { align: "left" });
+      }
     });
+
     doc.save(`export-${name}.pdf`);
   };
 
@@ -45,10 +54,9 @@ const PDFButton = ({
       name="PDF"
     />
   );
-}
+};
 
 const getTextSections = (messages: Message[]): string[] => {
-
   // Note "Thinking" messages have no `value` so they show up as new lines
   return messages
     .map((message) => {
@@ -59,11 +67,13 @@ const getTextSections = (messages: Message[]): string[] => {
       }
       if (message.type == MESSAGE_TYPE_TASK) {
         if (message.info) {
-          return `${i18n?.t("EXECUTING", { ns: "common" })} "${message.value}": ${
-            message.info
-          }`;
+          return `${i18n?.t("EXECUTING", { ns: "common" })}: "${
+            message.value
+          }": ${message.info}`;
         } else {
-          return `${i18n?.t("ADDING_TASK", { ns: "common" })}: ${message.value}`;
+          return `${i18n?.t("ADDING_TASK", { ns: "common" })}: ${
+            message.value
+          }`;
         }
       }
       return message.value;

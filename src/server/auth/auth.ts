ebd1aef548bc63f5294ba6b7b4ae03e8a75438ab
awtkns <hello@awtkns.com>
2023-05-07 09:50:56 +0200

    :bug: Hotfix discord auth

--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,5 +1,6 @@
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
 import { serverEnv } from "../../env/schema.mjs";
 import type {NextAuthOptions} from "next-auth";
 
@@ -15,7 +16,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: serverEnv.GITHUB_CLIENT_SECRET ?? "",
       allowDangerousEmailAccountLinking: true,
     }),
-    GithubProvider({
+    DiscordProvider({
       clientId: serverEnv.DISCORD_CLIENT_ID ?? "",
       clientSecret: serverEnv.DISCORD_CLIENT_ID ?? "",
       allowDangerousEmailAccountLinking: true,

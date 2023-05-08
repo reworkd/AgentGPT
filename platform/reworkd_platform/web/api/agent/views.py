8932dc5b25abd0f4f6075412ddcd3c73c2e227fb
Asim Shrestha <asim.shrestha@hotmail.com>
2023-05-08 13:39:50 -0700

    🧪 Add Create

--- platform/reworkd_platform/web/api/agent/views.py
+++ platform/reworkd_platform/web/api/agent/views.py
@@ -20,15 +20,39 @@ class AgentRequestBody(BaseModel):
     completedTasks: Optional[List[str]]
 
 
-@router.post("/create")
-async def create_tasks(request_body: AgentRequestBody) -> dict[str, str]:
+class NewTasksResponse(BaseModel):
+    newTasks: List[str]
+
+
+@router.post("/start")
+async def start(request_body: AgentRequestBody) -> NewTasksResponse:
     try:
         new_tasks = await get_agent_service().start_goal_agent(
             request_body.modelSettings,
             request_body.goal,
             request_body.language,
         )
-        return {"newTasks": new_tasks}
+        return NewTasksResponse(newTasks=new_tasks)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"An error occurred while processing the request. {e}"
+        )
+
+
+@router.post("/create")
+async def create_tasks(request_body: AgentRequestBody) -> NewTasksResponse:
+    try:
+        new_tasks = await get_agent_service().create_tasks_agent(
+            request_body.modelSettings,
+            request_body.goal,
+            request_body.language,
+            request_body.tasks,
+            request_body.lastTask,
+            request_body.result,
+            request_body.completedTasks
+        )
+        return NewTasksResponse(newTasks=new_tasks)
     except Exception as e:
         raise HTTPException(
             status_code=500,

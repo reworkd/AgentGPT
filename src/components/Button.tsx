686be0367044e99f64e86f56424fd8c13b24dbb2
Asim Shrestha <50181239+asim-shrestha@users.noreply.github.com>
2023-05-09 10:45:27 -0700

    📄 Add buttons to display TaskWindow on mobile (#485)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,50 +16,47 @@ export interface ButtonProps {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => Promise<void> | void;
 }
 
-const Button = forwardRef(
-  (props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
-    const [loading, setLoading] = useState(false);
-    const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-      if (props.loader == true) setLoading(true);
+const Button = forwardRef((props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
+  const [loading, setLoading] = useState(false);
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (props.loader == true) setLoading(true);
 
-      try {
-        void Promise.resolve(props.onClick?.(e)).then();
-      } catch (e) {
-        setLoading(false);
-      }
-    };
+    try {
+      void Promise.resolve(props.onClick?.(e)).then();
+    } catch (e) {
+      setLoading(false);
+    }
+  };
 
-    return (
-      <button
-        ref={ref}
-        type={props.type}
-        disabled={loading || props.disabled}
-        className={clsx(
-          "text-gray/50 relative rounded-lg border-[2px] border-white/30 px-5 py-2 font-bold transition-all sm:px-10 sm:py-3",
-          props.disabled &&
-            "cursor-not-allowed border-white/10 bg-zinc-900 text-white/30",
-          props.disabled ||
-            "mou cursor-pointer bg-[#1E88E5]/70 text-white/80 hover:border-white/80 hover:bg-[#0084f7] hover:text-white hover:shadow-2xl",
-          props.disabled || props.enabledClassName,
-          props.className
+  return (
+    <button
+      ref={ref}
+      type={props.type}
+      disabled={loading || props.disabled}
+      className={clsx(
+        "text-gray/50 relative rounded-lg border-[2px] border-white/30 px-4 py-1 font-bold transition-all sm:px-10 sm:py-3",
+        props.disabled && "cursor-not-allowed border-white/10 bg-zinc-900 text-white/30",
+        props.disabled ||
+          "mou cursor-pointer bg-[#1E88E5]/70 text-white/80 hover:border-white/80 hover:bg-[#0084f7] hover:text-white hover:shadow-2xl",
+        props.disabled || props.enabledClassName,
+        props.className
+      )}
+      onClick={onClick}
+    >
+      {props.ping ? <Ping color="white" /> : <></>}
+      <div className="flex items-center justify-center">
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            {props.icon ? <div className="mr-2">{props.icon}</div> : null}
+            {props.children}
+          </>
         )}
-        onClick={onClick}
-      >
-        {props.ping ? <Ping color="white" /> : <></>}
-        <div className="flex items-center justify-center">
-          {loading ? (
-            <Loader />
-          ) : (
-            <>
-              {props.icon ? <div className="mr-2">{props.icon}</div> : null}
-              {props.children}
-            </>
-          )}
-        </div>
-      </button>
-    );
-  }
-);
+      </div>
+    </button>
+  );
+});
 
 Button.displayName = "Button";
 export default Button;
--- src/components/TaskWindow.tsx
+++ src/components/TaskWindow.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import FadeIn from "./motions/FadeIn";
 import Expand from "./motions/expand";
-import {
-  MESSAGE_TYPE_TASK,
-  Task,
-  TASK_STATUS_STARTED,
-} from "../types/agentTypes";
+import { MESSAGE_TYPE_TASK, Task, TASK_STATUS_STARTED } from "../types/agentTypes";
 import { getMessageContainerStyle, getTaskStatusIcon } from "./utils/helpers";
-import { useMessageStore } from "./stores";
+import { useAgentStore, useMessageStore } from "./stores";
 import { FaListAlt, FaTimesCircle } from "react-icons/fa";
 import { useTranslation } from "react-i18next";
-import { useAgentStore } from "./stores";
 import clsx from "clsx";
 import Input from "./Input";
 import Button from "./Button";
 import { v1 } from "uuid";
 
-export const TaskWindow = () => {
+export interface TaskWindowProps {
+  visibleOnMobile?: boolean;
+}
+
+export const TaskWindow = ({ visibleOnMobile }: TaskWindowProps) => {
   const [customTask, setCustomTask] = React.useState("");
+  const agent = useAgentStore.use.agent();
   const tasks = useMessageStore.use.tasks();
   const addMessage = useMessageStore.use.addMessage();
   const [t] = useTranslation();
@@ -33,12 +33,20 @@ export const TaskWindow = () => {
   };
 
   return (
-    <Expand className="xl mx-2 mt-4 hidden w-[20rem] flex-col items-center rounded-2xl border-2 border-white/20 bg-zinc-900 px-1 font-mono shadow-2xl xl:flex">
-      <div className="sticky top-0 my-2 flex items-center justify-center gap-2 bg-zinc-900 p-2 text-gray-300 ">
+    <Expand
+      className={clsx(
+        "w-full flex-col items-center rounded-2xl border-2 border-white/20 bg-zinc-900 font-mono shadow-2xl xl:mx-2 xl:flex xl:w-[20rem] xl:px-1",
+        !visibleOnMobile && "hidden"
+      )}
+    >
+      <div className="sticky top-0 my-2 flex items-center justify-center gap-2 bg-zinc-900 p-2 text-gray-100 ">
         <FaListAlt /> {t("Current tasks")}
       </div>
       <div className="flex h-full w-full flex-col gap-2 px-1 py-1">
         <div className="window-heights flex w-full flex-col gap-2 overflow-y-auto overflow-x-hidden pr-1">
+          <p className="w-full p-2 text-center text-xs text-gray-300">
+            This window will display agent tasks as they are created.
+          </p>
           {tasks.map((task, i) => (
             <Task key={i} task={task} />
           ))}
@@ -53,7 +61,7 @@ export const TaskWindow = () => {
           <Button
             className="font-sm px-2 py-[0] text-sm sm:px-2 sm:py-[0]"
             onClick={handleAddTask}
-            disabled={!customTask}
+            disabled={!customTask || agent == null}
           >
             Add
           </Button>
@@ -66,8 +74,7 @@ export const TaskWindow = () => {
 const Task = ({ task }: { task: Task }) => {
   const isAgentStopped = useAgentStore.use.isAgentStopped();
   const deleteTask = useMessageStore.use.deleteTask();
-  const isTaskDeletable =
-    task.taskId && !isAgentStopped && task.status === "started";
+  const isTaskDeletable = task.taskId && !isAgentStopped && task.status === "started";
 
   const handleDeleteTask = () => {
     if (isTaskDeletable) {

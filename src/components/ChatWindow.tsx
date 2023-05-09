d5a6376fdade6ba7a794f56b72ae4818f17ef56e
Pratham Vadhulas <71392285+rp-bot@users.noreply.github.com>
2023-05-09 13:41:10 -0400

    Api Key Toggle View (#489)
    ---------
    
    Co-authored-by: Asim Shrestha <asim.shrestha@hotmail.com>

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "next-i18next";
-import { FaClipboard, FaImage, FaSave, FaPlay, FaPause } from "react-icons/fa";
+import { FaClipboard, FaImage, FaPause, FaPlay, FaSave } from "react-icons/fa";
 import PopIn from "./motions/popin";
 import Expand from "./motions/expand";
 import * as htmlToImage from "html-to-image";
@@ -11,17 +11,17 @@ import FadeIn from "./motions/FadeIn";
 import Menu from "./Menu";
 import type { Message } from "../types/agentTypes";
 import {
-  isAction,
-  getTaskStatus,
-  MESSAGE_TYPE_GOAL,
-  MESSAGE_TYPE_THINKING,
-  MESSAGE_TYPE_SYSTEM,
-  TASK_STATUS_STARTED,
-  TASK_STATUS_EXECUTING,
-  TASK_STATUS_COMPLETED,
-  TASK_STATUS_FINAL,
   AUTOMATIC_MODE,
+  getTaskStatus,
+  isAction,
+  MESSAGE_TYPE_GOAL,
+  MESSAGE_TYPE_SYSTEM,
+  MESSAGE_TYPE_THINKING,
   PAUSE_MODE,
+  TASK_STATUS_COMPLETED,
+  TASK_STATUS_EXECUTING,
+  TASK_STATUS_FINAL,
+  TASK_STATUS_STARTED,
 } from "../types/agentTypes";
 import clsx from "clsx";
 import { getMessageContainerStyle, getTaskStatusIcon } from "./utils/helpers";
@@ -111,8 +111,7 @@ const ChatWindow = ({
       <div
         className={clsx(
           "mb-2 mr-2 ",
-          (fullscreen && "max-h-[75vh] flex-grow overflow-auto") ||
-            "window-heights"
+          (fullscreen && "max-h-[75vh] flex-grow overflow-auto") || "window-heights"
         )}
         ref={scrollRef}
         onScroll={handleScroll}
@@ -143,29 +142,19 @@ const ChatWindow = ({
               <ChatMessage
                 message={{
                   type: MESSAGE_TYPE_SYSTEM,
-                  value:
-                    "👉 " + t("CREATE_AN_AGENT_DESCRIPTION", { ns: "chat" }),
+                  value: "👉 " + t("CREATE_AN_AGENT_DESCRIPTION", { ns: "chat" }),
                 }}
               />
             </PopIn>
             <PopIn delay={1.5}>
               <div className="m-2 flex flex-col justify-between gap-2 sm:m-4 sm:flex-row">
-                <ExampleAgentButton
-                  name="PlatformerGPT 🎮"
-                  setAgentRun={setAgentRun}
-                >
+                <ExampleAgentButton name="PlatformerGPT 🎮" setAgentRun={setAgentRun}>
                   Write some code to make a platformer game.
                 </ExampleAgentButton>
-                <ExampleAgentButton
-                  name="TravelGPT 🌴"
-                  setAgentRun={setAgentRun}
-                >
+                <ExampleAgentButton name="TravelGPT 🌴" setAgentRun={setAgentRun}>
                   Plan a detailed trip to Hawaii.
                 </ExampleAgentButton>
-                <ExampleAgentButton
-                  name="ResearchGPT 📜"
-                  setAgentRun={setAgentRun}
-                >
+                <ExampleAgentButton name="ResearchGPT 📜" setAgentRun={setAgentRun}>
                   Create a comprehensive report of the Nike company
                 </ExampleAgentButton>
               </div>
@@ -195,13 +184,7 @@ const ChatWindow = ({
   );
 };
 
-const SwitchContainer = ({
-  label,
-  children,
-}: {
-  label: string;
-  children: React.ReactNode;
-}) => {
+const SwitchContainer = ({ label, children }: { label: string; children: React.ReactNode }) => {
   return (
     <div className="m-1 flex w-36 items-center justify-center gap-2 rounded-lg border-[2px] border-white/20 bg-zinc-700 px-2 py-1">
       <p className="font-mono text-sm">{label}</p>
@@ -376,7 +359,6 @@ const MacWindowHeader = (props: HeaderProps) => {
       <Menu
         icon={<CgExport />}
         name={`${t("EXPORT", { ns: "common" })}`}
-        onChange={() => null}
         items={exportOptions}
         styleClass={{
           container: "relative",
@@ -399,12 +381,8 @@ const ChatMessage = ({ message }: { message: Message }) => {
       {message.type != MESSAGE_TYPE_SYSTEM && (
         // Avoid for system messages as they do not have an icon and will cause a weird space
         <>
-          <div className="mr-2 inline-block h-[0.9em]">
-            {getTaskStatusIcon(message, {})}
-          </div>
-          <span className="mr-2 font-bold">
-            {t(getMessagePrefix(message), { ns: "chat" })}
-          </span>
+          <div className="mr-2 inline-block h-[0.9em]">{getTaskStatusIcon(message, {})}</div>
+          <span className="mr-2 font-bold">{t(getMessagePrefix(message), { ns: "chat" })}</span>
         </>
       )}
 
@@ -459,10 +437,7 @@ const FAQ = () => {
     <p>
       <br />
       If you are facing issues, please head over to our{" "}
-      <a
-        href="https://reworkd.github.io/AgentGPT-Documentation/docs/faq"
-        className="text-sky-500"
-      >
+      <a href="https://reworkd.github.io/AgentGPT-Documentation/docs/faq" className="text-sky-500">
         FAQ
       </a>
     </p>

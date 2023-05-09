5d0b56ce01a2e1fc6be561d4947e0ad5535e86fa
awtkns <hello@awtkns.com>
2023-05-09 11:26:56 +0200

    :bug: fix store imports

--- src/components/TaskWindow.tsx
+++ src/components/TaskWindow.tsx
@@ -7,10 +7,10 @@ import {
   TASK_STATUS_STARTED,
 } from "../types/agentTypes";
 import { getMessageContainerStyle, getTaskStatusIcon } from "./utils/helpers";
-import { useMessageStore } from "./stores";
+import { useMessageStore } from "../stores";
 import { FaListAlt, FaTimesCircle } from "react-icons/fa";
 import { useTranslation } from "react-i18next";
-import { useAgentStore } from "./stores";
+import { useAgentStore } from "../stores";
 import clsx from "clsx";
 import Input from "./Input";
 import Button from "./Button";

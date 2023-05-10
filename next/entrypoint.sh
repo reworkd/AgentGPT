202976f1e8550d36cbacff09feb815c73a4b65ee
Asim Shrestha <asim.shrestha@hotmail.com>
2023-05-09 17:16:47 -0700

    🐋 Put MySQL DB in docker

--- next/entrypoint.sh
+++ next/entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/env sh
 
 # copy .env file if not exists
-[ ! -f .env ] && cp .env.example .env
-
-# change schema.prisma
-sed -ie 's/mysql/sqlite/g' prisma/schema.prisma
-sed -ie 's/@db.Text//' prisma/schema.prisma
-
-# Add Prisma and generate Prisma client
-npx prisma generate
-# Generate db when not exists
+[ ! -f .env ] && [ -f .env.example ] && cp .env.example .env
 source .env
+
+# Ensure DB is available before running Prisma commands
+./wait-for-db.sh db 3306
+
+# Run Prisma commands
 if [[ ! -f "/app/prisma/${DATABASE_URL:5}" ]]; then
   npx prisma migrate dev --name init
   npx prisma db push
 fi
+
+# Generate Prisma client
+npx prisma generate
 
 # run cmd
 exec "$@"

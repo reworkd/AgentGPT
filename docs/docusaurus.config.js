a84265367233a0f75ba1a3c0535eac7b8b39e83b
awtkns <hello@awtkns.com>
2023-05-10 10:09:28 +0300

    :memo: Update doc build settings

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'docs.reworkd.ai',
+  url: 'https://docs.reworkd.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',

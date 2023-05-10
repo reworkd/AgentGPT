2041ed23b6e9371831fd1699aad8a1c170633e8f
awtkns <hello@awtkns.com>
2023-05-10 10:04:38 +0300

    :memo: Update doc build settings

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'docs.reworkd.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AgentGPT-Documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'reworkd', // Usually your GitHub org/user name.
-  projectName: 'AgentGPT-Documentation', // Usually your repo name.
+  projectName: 'AgentGPT', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',

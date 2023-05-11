673d6958aad947b8e49eade80d95e1a388af58a2
Alessandro Bellia <casildaserrano@yahoo.it>
2023-05-11 23:33:25 +0200

    Fixed Dockerfile and entrypoint.sh to handle Windows line endings (#514)
    Signed-off-by: Alessandro Bellia <casilda.bell85@gmail.com>

--- next/entrypoint.sh
+++ next/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/env sh
 
+cd /next
+dos2unix wait-for-db.sh
+
 # copy .env file if not exists
 [ ! -f .env ] && [ -f .env.example ] && cp .env.example .env
+cp .env .env.temp
+dos2unix .env.temp
+cat .env.temp > .env
+rm .env.temp
+
 source .env
 
 # Ensure DB is available before running Prisma commands

fabe30d694af5c442dfb3335d10f6905fb1ad507
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-05-14 18:07:55 +0200

    [fix] ns can be null Co-authored-by: Adam Watkins <hello@awtkns.com>

--- next/src/utils/translations.ts
+++ next/src/utils/translations.ts
@@ -3,7 +3,7 @@ import type nextI18NextConfig from "../../next-i18next.config.js";
 
 type Namespace = (typeof nextI18NextConfig.ns)[number];
 
-export const translate = (key: string, ns: Namespace | undefined) => {
+export const translate = (key: string, ns?: Namespace | undefined | null) => {
   const opts = !!ns ? { ns } : undefined;
   return i18n?.t(key, key, opts) ?? key;
 };

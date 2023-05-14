2aec96a4be168a09ecc80b70041f6b83afe17750
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-05-14 17:49:35 +0200

    🎏 Automatic namespace declarations from the i18next config

--- next/src/utils/translations.ts
+++ next/src/utils/translations.ts
@@ -1,10 +1,9 @@
-import {i18n} from "next-i18next";
+import { i18n } from "next-i18next";
+import type nextI18NextConfig from "../../next-i18next.config.js";
 
-type Namespace =
-  'errors' | 'drawer'
-
+type Namespace = (typeof nextI18NextConfig.ns)[number];
 
 export const translate = (key: string, ns: Namespace | undefined) => {
-  const opts = !!ns ? { ns } : undefined
+  const opts = !!ns ? { ns } : undefined;
   return i18n?.t(key, key, opts) ?? key;
-}
+};

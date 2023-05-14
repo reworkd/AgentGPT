d54aca982a0ad45f34a7a487f40feba98e561321
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-05-14 17:43:25 +0200

    ➕ exportable language codes as locales

--- next/src/utils/languages.ts
+++ next/src/utils/languages.ts
@@ -33,6 +33,8 @@ export const languages: Language[] = availableLanguages.sort((a, b) =>
   a.name.localeCompare(b.name)
 );
 
+export const locales: string[] = availableLanguages.map((lang) => lang.code);
+
 export const findLanguage = (nameOrLocale: string): Language => {
   const selectedLanguage = languages.find(
     (lang) => lang.code === nameOrLocale || lang.name === nameOrLocale.substring(4).trim()

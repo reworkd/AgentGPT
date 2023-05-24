a526114231c522361e3cc95c7a8e4ac556c429ca
asim-shrestha <asim.shrestha@hotmail.com>
2023-05-24 09:40:38 -0700

    🐛 Use replicate for image generation if available

--- platform/reworkd_platform/web/api/agent/agent_service/open_ai_agent_service.py
+++ platform/reworkd_platform/web/api/agent/agent_service/open_ai_agent_service.py
@@ -37,7 +37,7 @@ class OpenAIAgentService(AgentService):
         chain = LLMChain(llm=llm, prompt=analyze_task_prompt)
 
         pydantic_parser = PydanticOutputParser(pydantic_object=Analysis)
-
+        print(get_tools_overview(get_user_tools(tool_names)))
         completion = await chain.arun(
             {
                 "goal": goal,

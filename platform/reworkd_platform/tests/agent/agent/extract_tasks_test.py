c48269a1298dd90c672044f3d516edca82444fdc
Halil Ibrahim Bestil <hibestil@users.noreply.github.com>
2023-05-27 00:19:41 +0300

    🧪 Parametrize Tests (#600)

--- platform/reworkd_platform/tests/agent/agent/extract_tasks_test.py
+++ platform/reworkd_platform/tests/agent/agent/extract_tasks_test.py
@@ -1,52 +1,40 @@
-from typing import List
+import pytest
 
 from reworkd_platform.web.api.agent.helpers import extract_tasks
 
 
-def test_extract_tasks_no_completed_tasks() -> None:
-    input_text = '["Task 1: Task one", "Task 2: Task two", "Task 3: Task three"]'
-    expected_output = ["Task one", "Task two", "Task three"]
+@pytest.mark.parametrize(
+    "input_text, completed_tasks, expected_output",
+    [
+        (  # no completed tasks
+            '["Task 1: Task one", "Task 2: Task two", "Task 3: Task three"]',
+            [],
+            ["Task one", "Task two", "Task three"]
+        ),
+        (  # tasks with completed tasks
+            '["Task 1: Task one", "Task 2: Task two", "Task 3: Task three"]',
+            ["Task two"],
+            ["Task one", "Task three"]
+        ),
+        (  # only completed tasks
+            '["Task 1: Task one", "Task 2: Task two", "Task 3: Task three"]',
+            ["Task one", "Task two", "Task three"],
+            []
+        ),
+        (  # empty input
+            '[]',
+            [],
+            []
+        ),
+        (  # no valid tasks
+            '["No tasks added", "Task complete", "Do nothing"]',
+            [],
+            []
+        ),
 
-    output = extract_tasks(input_text, [])
+    ]
 
-    assert output == expected_output
-
-
-def test_extract_tasks_with_completed_tasks() -> None:
-    input_text = '["Task 1: Task one", "Task 2: Task two", "Task 3: Task three"]'
-    completed_tasks = ["Task two"]
-    expected_output = ["Task one", "Task three"]
-
+)
+def test_extract_tasks(input_text, completed_tasks, expected_output) -> None:
     output = extract_tasks(input_text, completed_tasks)
-
-    assert output == expected_output
-
-
-def test_extract_tasks_only_completed_tasks() -> None:
-    input_text = '["Task 1: Task one", "Task 2: Task two", "Task 3: Task three"]'
-    completed_tasks = ["Task one", "Task two", "Task three"]
-    expected_output: List[str] = []
-
-    output = extract_tasks(input_text, completed_tasks)
-
-    assert output == expected_output
-
-
-def test_extract_tasks_empty_input() -> None:
-    input_text = "[]"
-    completed_tasks: List[str] = []
-    expected_output: List[str] = []
-
-    output = extract_tasks(input_text, completed_tasks)
-
-    assert output == expected_output
-
-
-def test_extract_tasks_no_valid_tasks() -> None:
-    input_text = '["No tasks added", "Task complete", "Do nothing"]'
-    completed_tasks: List[str] = []
-    expected_output: List[str] = []
-
-    output = extract_tasks(input_text, completed_tasks)
-
     assert output == expected_output

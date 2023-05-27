7e2f5348078aff9214e5a88a056b4f1cce250d23
Palmer Duckworth <34105363+JPDucky@users.noreply.github.com>
2023-05-27 01:40:09 -0500

    Refactor setup scripts for interactive setup and run, and update README (#406)
    * Added an interface for configuration, currently trying to get the api key entry part to return to itself when an invalid key is entered. I will add documentation soon
    
    * Created a setup script and a docker-compose route to make it easier for
    beginners. Currently only docker-compose is working, no errors. I will
    need to do more work on docker and local build tomorrow
    
    * Forgot to uncomment npm install from testing
    
    * changed line 'prepare': 'husky install' to 'prepare': 'npx husky install' because as of husky 5, husky commands must be run via npx
    
    * changed line "prepare": "husky install" to "prepare": "npx husky
    install" because as of husky 5, husky commands must be run via npx
    
    * working
    
    * input.mjs broken, moving to a different host for further troubleshooting
    
    * Changes Made
    - Removed setup.sh and replaced with init.sh and setup.mjs.
    - init.sh is a simple script to install necessary packages and prompt the user to set up the environment using setup.mjs.
    - setup.mjs is a comprehensive script that sets up the environment for the AgentGPT project. This script interacts with the user to collect necessary configuration details like the OpenAI API key, Docker/Node setup preference, and more. It generates the necessary environment variables, handles Docker setup if chosen, and also has logic to handle and retry on errors.
    
    * Updated readme to reflect changes
    
    * removed my settings.json
    
    * 💬 Update error messaging
    
    * 💬 Check if ENV exists
    
    * 💬 Validate against existing ENV
    
    * 💬 Update finalization commands
    
    * 💬 Fix schema
    
    * 💬 Fix execution
    
    * 💬 Revert next
    
    * 💬 Revert main
    
    * 💬 Add package json
    
    * 💬 Add package json
    
    * 💬 Manual
    
    * 🔌 Electrifying devX
    
    ---------
    
    Co-authored-by: Asim Shrestha <asim.shrestha@hotmail.com>
    Co-authored-by: jpducky <34105363+JPDucky@users.noreply.github.com~>
    Co-authored-by: awtkns <32209255+awtkns@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,46 +1,7 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit
+cd "$(dirname "$0")" || exit 1
 
-is_valid_sk_key() {
-  local api_key=$1
-  local pattern="^sk-[a-zA-Z0-9]{48}$"
-  [[ $api_key =~ $pattern ]] && return 0 || return 1
-}
-
-echo -n "Enter your OpenAI Key (eg: sk...) or press enter to continue with no key: "
-read OPENAI_API_KEY
-
-if is_valid_sk_key $OPENAI_API_KEY || [ -z "$OPENAI_API_KEY" ]; then
-  echo "Valid API key"
-else
-  echo "Invalid API key. Please ensure that you have billing set up on your OpenAI account"
-  exit
-fi
-
-NEXTAUTH_SECRET=$(openssl rand -base64 32)
-
-ENV="NODE_ENV=development\n\
-NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
-NEXTAUTH_URL=http://localhost:3000\n\
-NEXT_PUBLIC_BACKEND_URL=http://localhost:3000\n\
-OPENAI_API_KEY=$OPENAI_API_KEY\n\
-DATABASE_URL=file:../db/db.sqlite\n"
-
-cd next
-printf $ENV > .env
-printf $ENV > .env.docker
-
-
-if [ "$1" = "--docker" ]; then
-  source .env.docker
-  docker build --build-arg NODE_ENV=$NODE_ENV -t agentgpt .
-  docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
-elif [ "$1" = "--docker-compose" ]; then
-  cd ..
-  docker-compose up -d --remove-orphans
-else
-  ./prisma/useSqlite.sh
-  npm install
-  prisma db push
-  npm run dev
-fi
+# The CLI will take care of setting up the ENV variables
+cd ./cli || exit 1
+npm install
+npm run start

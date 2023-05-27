da98a107f866f2f81e1d7163279a8bf0283ddb32
awtkns <32209255+awtkns@users.noreply.github.com>
2023-05-26 20:21:38 -0700

    🧹 Fix Mypy

--- platform/reworkd_platform/tests/agent/agent/remove_task_prefix_test.py
+++ platform/reworkd_platform/tests/agent/agent/remove_task_prefix_test.py
@@ -1,4 +1,5 @@
 import pytest
+
 from reworkd_platform.web.api.agent.helpers import remove_task_prefix
 
 
@@ -6,21 +7,36 @@ from reworkd_platform.web.api.agent.helpers import remove_task_prefix
     "task_input, expected_output",
     [
         ("Task: This is a sample task", "This is a sample task"),
-        ("Task 1: Perform a comprehensive analysis of system performance.", "Perform a comprehensive analysis of system performance."),
+        (
+            "Task 1: Perform a comprehensive analysis of system performance.",
+            "Perform a comprehensive analysis of system performance.",
+        ),
         ("Task 2. Create a python script", "Create a python script"),
         ("5 - This is a sample task", "This is a sample task"),
         ("2: This is a sample task", "This is a sample task"),
-        ("This is a sample task without a prefix", "This is a sample task without a prefix"),
+        (
+            "This is a sample task without a prefix",
+            "This is a sample task without a prefix",
+        ),
         ("Step: This is a sample task", "This is a sample task"),
-        ("Step 1: Perform a comprehensive analysis of system performance.", "Perform a comprehensive analysis of system performance."),
+        (
+            "Step 1: Perform a comprehensive analysis of system performance.",
+            "Perform a comprehensive analysis of system performance.",
+        ),
         ("Step 2:Create a python script", "Create a python script"),
         ("Step:This is a sample task", "This is a sample task"),
-        (". Conduct research on the history of Nike", "Conduct research on the history of Nike"),
+        (
+            ". Conduct research on the history of Nike",
+            "Conduct research on the history of Nike",
+        ),
         (".This is a sample task", "This is a sample task"),
-        ("1. Research the history and background of Nike company.", "Research the history and background of Nike company."),
+        (
+            "1. Research the history and background of Nike company.",
+            "Research the history and background of Nike company.",
+        ),
     ],
 )
-def test_remove_task_prefix(task_input, expected_output) -> None:
+def test_remove_task_prefix(task_input: str, expected_output: str) -> None:
     task_input = "Task: This is a sample task"
     output = remove_task_prefix(task_input)
     assert output == "This is a sample task"

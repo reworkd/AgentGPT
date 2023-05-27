1d88b161cc760a39ae99bf120f7efa5c6bc8e41a
awtkns <32209255+awtkns@users.noreply.github.com>
2023-05-26 23:37:56 -0700

    🔌 Electrifying devX

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,5 +1,5 @@
 import inquirer from "inquirer";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { isValidSkKey, printTitle } from "./helpers.js";
 import { doesEnvFileExist, generateEnv, testEnvFile } from "./envGenerator.js";
 
@@ -16,7 +16,8 @@ const questions = [
   {
     type: "input",
     name: "OpenAIApiKey",
-    message: "Enter your openai key (eg: sk...) or press enter to continue with no key:",
+    message:
+      "Enter your openai key (eg: sk...) or press enter to continue with no key:",
     validate: (apikey) => {
       if (isValidSkKey(apikey) || apikey === "") {
         return true;
@@ -26,34 +27,39 @@ const questions = [
     },
   },
   {
-    type: 'input',
-    name: 'serpApiKey',
-    message: 'What is your SERP API key (https://serper.dev/)? Leave empty to disable web search.',
+    type: "input",
+    name: "serpApiKey",
+    message:
+      "What is your SERP API key (https://serper.dev/)? Leave empty to disable web search.",
   },
   {
-    type: 'input',
-    name: 'replicateApiKey',
-    message: 'What is your Replicate API key (https://replicate.com/)? Leave empty to just use DALL-E for image generation.',
+    type: "input",
+    name: "replicateApiKey",
+    message:
+      "What is your Replicate API key (https://replicate.com/)? Leave empty to just use DALL-E for image generation.",
   },
 ];
 
-
 if (doesEnvFileExist()) {
-  console.log("Existing ./next/env file found. Validating...")
-  testEnvFile()
+  console.log("Existing ./next/env file found. Validating...");
+  testEnvFile();
 } else {
   inquirer.prompt(questions).then((answers) => {
-    dotenv.config({ path: './.env' });
+    dotenv.config({ path: "./.env" });
     generateEnv(answers);
 
-    console.log("\nEnv files successfully created!")
-    if (answers.runOption === 'docker-compose') {
+    console.log("\nEnv files successfully created!");
+    if (answers.runOption === "docker-compose") {
       console.log("Please run `docker-compose up --build` in the terminal.");
     }
 
-    if (answers.runOption === 'docker') {
-      console.log("Please go into the ./next and ./platform folders and run the dockerfiles.");
-      console.log("Please use or update the MySQL database configuration in the env file.");
+    if (answers.runOption === "docker") {
+      console.log(
+        "Please go into the ./next and ./platform folders and run the dockerfiles."
+      );
+      console.log(
+        "Please use or update the MySQL database configuration in the env file."
+      );
     }
 
     if (answers.runOption === 'manual') {
@@ -63,4 +69,3 @@ if (doesEnvFileExist()) {
     }
   });
 }
-
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$0")" || exit 1
 
 # The CLI will take care of setting up the ENV variables
-cd ./cli
+cd ./cli || exit 1
 npm install
 npm run start

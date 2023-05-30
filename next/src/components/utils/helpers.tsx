dfc671a4afac5ca769dae6f5a8135c9ea7eb60eb
Adam Watkins <hello@awtkns.com>
2023-05-29 18:05:51 -0700

    ♻️ Improve Error Messages [Part 2] (#626)
    * ♻️ Refactor autonomous agent
    * ♻️ Refactor error handling
    * 🐛 Only show tools if user has setup key
    * 🔌 Enable reload by default
    * 🧪 Add error tests
    * 🐞Fix mypy types
    * ⚠️ Improve Error Messaging

--- next/src/components/utils/helpers.tsx
+++ next/src/components/utils/helpers.tsx
@@ -1,28 +1,34 @@
 import {
   FaBrain,
-  FaCircleNotch,
-  FaRegCheckCircle,
   FaCheckCircle,
+  FaCircleNotch,
+  FaExclamationTriangle,
+  FaRegCheckCircle,
   FaStar,
   FaStopCircle,
   FaThumbtack,
 } from "react-icons/fa";
+import type { Message } from "../../types/agentTypes";
 import {
+  getTaskStatus,
   isTask,
-  TASK_STATUS_STARTED,
-  TASK_STATUS_EXECUTING,
-  TASK_STATUS_COMPLETED,
-  TASK_STATUS_FINAL,
+  MESSAGE_TYPE_ERROR,
   MESSAGE_TYPE_GOAL,
   MESSAGE_TYPE_THINKING,
-  getTaskStatus,
+  TASK_STATUS_COMPLETED,
+  TASK_STATUS_EXECUTING,
+  TASK_STATUS_FINAL,
+  TASK_STATUS_STARTED,
 } from "../../types/agentTypes";
-
-import type { Message } from "../../types/agentTypes";
 
 export const getMessageContainerStyle = (message: Message) => {
   if (!isTask(message)) {
-    return "border-white/10 hover:border-white/40";
+    switch (message.type) {
+      case "error":
+        return "border-red-500";
+      default:
+        return "border-white/10 hover:border-white/40";
+    }
   }
 
   switch (message.status) {
@@ -46,11 +52,16 @@ export const getTaskStatusIcon = (
   const taskStatusIconClass = "mr-1 mb-1 inline-block";
   const { isAgentStopped } = config;
 
-  if (message.type === MESSAGE_TYPE_GOAL) {
-    return <FaStar className="text-yellow-300" />;
-  } else if (message.type === MESSAGE_TYPE_THINKING) {
-    return <FaBrain className="mt-[0.1em] text-pink-400" />;
-  } else if (getTaskStatus(message) === TASK_STATUS_STARTED) {
+  switch (message.type) {
+    case MESSAGE_TYPE_GOAL:
+      return <FaStar className="text-yellow-300" />;
+    case MESSAGE_TYPE_THINKING:
+      return <FaBrain className="mt-[0.1em] text-pink-400" />;
+    case MESSAGE_TYPE_ERROR:
+      return <FaExclamationTriangle className="text-red-500" />;
+  }
+
+  if (getTaskStatus(message) === TASK_STATUS_STARTED) {
     return <FaThumbtack className={`${taskStatusIconClass} -rotate-45`} />;
   } else if (getTaskStatus(message) === TASK_STATUS_EXECUTING) {
     return isAgentStopped ? (
@@ -60,15 +71,11 @@ export const getTaskStatusIcon = (
     );
   } else if (getTaskStatus(message) === TASK_STATUS_COMPLETED) {
     return (
-      <FaRegCheckCircle
-        className={`${taskStatusIconClass} text-green-500 hover:text-green-400`}
-      />
+      <FaRegCheckCircle className={`${taskStatusIconClass} text-green-500 hover:text-green-400`} />
     );
   } else if (getTaskStatus(message) === TASK_STATUS_FINAL) {
     return (
-      <FaCheckCircle
-        className={`${taskStatusIconClass} text-green-500 hover:text-green-400`}
-      />
+      <FaCheckCircle className={`${taskStatusIconClass} text-green-500 hover:text-green-400`} />
     );
   }
 };

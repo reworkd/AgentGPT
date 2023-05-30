4c86a17bf080385735eb9e2e27aeafd6f39826f7
Asim Shrestha <50181239+asim-shrestha@users.noreply.github.com>
2023-05-29 22:00:14 -0700

    🐍 Add basic error wrapping [Part. 1] (#628)

--- platform/reworkd_platform/web/api/agent/helpers.py
+++ platform/reworkd_platform/web/api/agent/helpers.py
@@ -1,1 +1,31 @@
+from typing import TypeVar
 
+from langchain import LLMChain
+from langchain.schema import OutputParserException, BaseOutputParser
+
+from reworkd_platform.web.api.agent.model_settings import create_model, ModelSettings
+from reworkd_platform.web.api.errors import OpenAIError
+
+T = TypeVar("T")
+
+
+def parse_with_handling(parser: BaseOutputParser[T], completion: str) -> T:
+    try:
+        return parser.parse(completion)
+    except OutputParserException as e:
+        raise OpenAIError(
+            e, "There was an issue parsing the response from the AI model."
+        )
+
+
+async def call_model_with_handling(
+    model_settings: ModelSettings, prompt: str, args: dict
+) -> str:
+    try:
+        model = create_model(model_settings)
+        chain = LLMChain(llm=model, prompt=prompt)
+        return await chain.arun(args)
+    except Exception as e:
+        raise OpenAIError(
+            e, "There was an issue getting a response from the AI model."
+        )

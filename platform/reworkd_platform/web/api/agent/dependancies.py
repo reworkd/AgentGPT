2b6241c5f26d2be784be26fc6e6e044d83efed89
awtkns <32209255+awtkns@users.noreply.github.com>
2023-05-31 12:22:44 -0700

    🆎 Fix mypy typing

--- platform/reworkd_platform/web/api/agent/dependancies.py
+++ platform/reworkd_platform/web/api/agent/dependancies.py
@@ -1,11 +1,13 @@
-from typing import Dict
+from typing import Dict, Any, Optional, Callable, Coroutine
 
 from fastapi import Body
 
 from reworkd_platform.schemas import AgentRequestBody
 
 
-def agent_validator(example: Dict[str, str] = None, **kwargs):
+def agent_validator(
+    example: Optional[Dict[str, Any]] = None, **kwargs: Any
+) -> Callable[[AgentRequestBody], Coroutine[Any, Any, AgentRequestBody]]:
     async def func(
         body: AgentRequestBody = Body(example=example, **kwargs),
     ) -> AgentRequestBody:
--- platform/reworkd_platform/web/api/agent/model_settings.py
+++ platform/reworkd_platform/web/api/agent/model_settings.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import openai
 from langchain.chat_models import ChatOpenAI
 
@@ -10,9 +8,7 @@ from reworkd_platform.web.api.agent.api_utils import rotate_keys
 openai.api_base = settings.openai_api_base
 
 
-def create_model(
-    model_settings: Optional[ModelSettings], streaming: bool = False
-) -> ChatOpenAI:
+def create_model(model_settings: ModelSettings, streaming: bool = False) -> ChatOpenAI:
     return ChatOpenAI(
         client=None,  # Meta private value but mypy will complain its missing
         openai_api_key=rotate_keys(
@@ -20,7 +16,7 @@ def create_model(
             secondary_key=settings.secondary_openai_api_key,
         ),
         temperature=model_settings.temperature,
-        model_name=model_settings.model,
+        model=model_settings.model,
         max_tokens=model_settings.max_tokens,
         streaming=streaming,
     )

7bf16ab4869e2f76461483b3e1b5f152c3e804c0
Joe Shen <joeshen123@gmail.com>
2023-06-06 16:06:51 -0700

    add unstyled input element and implement slider for temperature

--- next/src/pages/settings.tsx
+++ next/src/pages/settings.tsx
@@ -1,12 +1,17 @@
 import SidebarLayout from "../layout/sidebar";
 import Combo from "../ui/combox";
+import Input from "../ui/input";
 import { ENGLISH, Language, languages } from "../utils/languages";
 import { GPT_35_TURBO, GPTModelNames, GPT_MODEL_NAMES } from "../types";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useTranslation } from "next-i18next";
 
 const SettingsPage = () => {
+  const [t] = useTranslation();
+
   const [language, setLanguage] = useState<Language>(ENGLISH);
   const [model, setModel] = useState<GPTModelNames>(GPT_35_TURBO)
+  const [temperature, setTemperature] = useState<Number>(0.9);
 
   return (
     <SidebarLayout>
@@ -25,6 +30,24 @@ const SettingsPage = () => {
         onChange={setModel}
         items={GPT_MODEL_NAMES}
       />
+
+      <h1 className="mt-6">Advanced Settings</h1>
+      <Input
+        label={`${t("TEMPERATURE", { ns: "settings" })}`}
+        value={temperature}
+        name="temperature"
+        type="range"
+        onChange={(e) => setTemperature(parseFloat(e.target.value))}
+        attributes={{
+          min: 0,
+          max: 1,
+          step: 0.01
+        }}
+        helpText={`${t("HIGHER_VALUES_MAKE_OUTPUT_MORE_RANDOM", {
+          ns: "settings",
+        })}`}
+      />
+
     </SidebarLayout>
   );
 };
---
+++ next/src/ui/input.tsx
@@ -0,0 +1,39 @@
+interface Props<T> {
+  label: string;
+  value: T | undefined;
+  name: string;
+  type: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>
+  ) => void;
+  attributes?: { [key: string]: string | number | string[] }
+  helpText?: string;
+  placeholder?: string;
+}
+
+const Input = <T, > ({ ...props }: Props<T>) => {
+  return (
+    <div>
+      <label htmlFor={props.name} className="block text-sm font-medium leading-6 text-gray-900">
+        {props.label}
+      </label>
+      <div className="relative mt-2 rounded-md shadow-sm">
+        <input
+          type={props.type}
+          name={props.name}
+          id={props.name}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder={props.placeholder}
+          onChange={props.onChange}
+          {...(props.helpText ? {'aria-describedby': `${props.name}-description`} : {})}
+          {...props.attributes}
+        />
+      </div>
+      <p className="mt-2 text-sm text-gray-500" id={`${props.name}-description`}>
+        {props.helpText}
+      </p>
+    </div>
+  )
+}
+
+export default Input;

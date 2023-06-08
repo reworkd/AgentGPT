f04f4d1e4fc96f63ab76b61466be8519162adb38
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-07 18:44:48 -0700

    ✨ logger exception instead of error

--- platform/reworkd_platform/web/api/memory/memory_with_fallback.py
+++ platform/reworkd_platform/web/api/memory/memory_with_fallback.py
@@ -21,33 +21,33 @@ class MemoryWithFallback(AgentMemory):
         try:
             return self.primary.__enter__()
         except Exception as e:
-            logger.error(e)
+            logger.exception(e)
             return self.secondary.__enter__()
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
         try:
             self.primary.__exit__(exc_type, exc_value, traceback)
         except Exception as e:
-            logger.error(e)
+            logger.exception(e)
             self.secondary.__exit__(exc_type, exc_value, traceback)
 
     def add_tasks(self, tasks: List[str]) -> List[str]:
         try:
             return self.primary.add_tasks(tasks)
         except Exception as e:
-            logger.error(e)
+            logger.exception(e)
             return self.secondary.add_tasks(tasks)
 
     def get_similar_tasks(self, query: str, score_threshold) -> List[str]:
         try:
             return self.primary.get_similar_tasks(query, score_threshold)
         except Exception as e:
-            logger.error(e)
+            logger.exception(e)
             return self.secondary.get_similar_tasks(query, score_threshold)
 
     def reset_class(self) -> None:
         try:
             self.primary.reset_class()
         except Exception as e:
-            logger.error(e)
+            logger.exception(e)
             self.secondary.reset_class()

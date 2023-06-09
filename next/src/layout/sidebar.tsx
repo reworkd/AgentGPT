d0a9a941f1a0db728d7ff33d71080bde118e640e
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-09 16:37:04 -0700

    ✨ Clean up

--- next/src/layout/sidebar.tsx
+++ next/src/layout/sidebar.tsx
@@ -41,7 +41,7 @@ const LinkItem = (props: {
     <a
       href={props.href}
       className={clsx(
-        "cursor-pointer text-neutral-400 hover:bg-neutral-800 hover:text-white cursor-pointer",
+        "cursor-pointer text-neutral-400 hover:bg-neutral-800 hover:text-white",
         "group flex gap-x-3 rounded-md px-2 py-1 text-sm font-semibold leading-6"
       )}
       onClick={(e) => {
@@ -49,7 +49,7 @@ const LinkItem = (props: {
         props.onClick();
       }}
     >
-      <span className=" group-hover:scale-110 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-neutral-700 bg-neutral-800 text-[0.7rem] font-medium text-neutral-400 group-hover:scale-110 group-hover:text-white">
+      <span className="items-center justify-center rounded-lg border border-neutral-700 bg-neutral-800 text-[0.7rem] font-medium text-neutral-400 group-hover:scale-110 group-hover:text-white">
         {props.icon}
       </span>
       <span className="truncate">{props.title}</span>
@@ -60,7 +60,7 @@ const LinkItem = (props: {
 const SidebarLayout = (props: Props) => {
   const router = useRouter();
   const { session, signIn, signOut, status } = useAuth();
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [t] = useTranslation("drawer");
 
   const [showSettings, setShowSettings] = useState(false);

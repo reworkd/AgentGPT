667947a84387ff13333371b9dc8088aa13db2afe
Joe Shen <joeshen123@gmail.com>
2023-06-08 17:32:37 -0700

    remove comment

--- next/src/types/modelSettings.ts
+++ next/src/types/modelSettings.ts
@@ -3,11 +3,6 @@ import { Language } from "../utils/languages";
 export const [GPT_35_TURBO, GPT_4] = ["gpt-3.5-turbo" as const, "gpt-4" as const];
 export const GPT_MODEL_NAMES = [GPT_35_TURBO, GPT_4];
 export type GPTModelNames = "gpt-3.5-turbo" | "gpt-4";
-
-// export const [SETTINGS_LANGUAGE, SETTINGS_MODEL_NAME, SETTINGS_TEMPERATURE, SETTINGS_MAX_LOOPS, SETTINGS_MAX_TOKEN] = [
-//   'language' as const, 'customModelName' as const, 'customTemperature' as const, 'customMaxLoops' as const, 'maxTokens' as const
-// ];
-
 export interface ModelSettings {
   language?: Language;
   customModelName?: GPTModelNames;

bd79668c23be1105324a2a24df093ffd69cd37f8
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-08 20:04:45 -0700

    ✨ Model settings

--- next/src/types/modelSettings.ts
+++ next/src/types/modelSettings.ts
@@ -1,8 +1,9 @@
-import { Language } from "../utils/languages";
+import { type Language } from "../utils/languages";
 
 export const [GPT_35_TURBO, GPT_4] = ["gpt-3.5-turbo" as const, "gpt-4" as const];
 export const GPT_MODEL_NAMES = [GPT_35_TURBO, GPT_4];
 export type GPTModelNames = "gpt-3.5-turbo" | "gpt-4";
+
 export interface ModelSettings {
   language?: Language;
   customModelName?: GPTModelNames;

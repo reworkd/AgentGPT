c6a1211a33dd94b6b28e33d260bbe1103fd98f36
Asim Shrestha <50181239+asim-shrestha@users.noreply.github.com>
2023-06-10 12:33:17 -0700

    💅 Front end (#757)

--- next/src/components/motions/FadeIn.tsx
+++ next/src/components/motions/FadeIn.tsx
@@ -4,13 +4,15 @@ import type { PropsWithChildren } from "react";
 interface MotionProps extends PropsWithChildren {
   className?: string;
   delay?: number;
+  duration?: number;
+  initialY?: number;
 }
 
 const FadeIn = (props: MotionProps) => (
   <motion.div
-    initial={{ opacity: 0, y: -30 }}
+    initial={{ opacity: 0, y: props.initialY ?? -30 }}
     animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5, type: "spring", delay: props.delay ?? 0.3 }}
+    transition={{ duration: props.duration ?? 0.5, type: "spring", delay: props.delay ?? 0.3 }}
     {...props}
   >
     {props.children}

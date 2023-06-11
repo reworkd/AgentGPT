420f47f253135158ec1ab6950abbac01649b916f
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-11 11:44:01 -0700

    ✨ Add on click to PopIn

--- next/src/components/motions/popin.tsx
+++ next/src/components/motions/popin.tsx
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion";
-import type { PropsWithChildren } from "react";
+import type { MouseEventHandler, PropsWithChildren } from "react";
 
 interface MotionProps extends PropsWithChildren {
   className?: string;
   delay?: number;
   duration?: number;
+  onClick?: MouseEventHandler<HTMLDivElement>;
 }
 
 const PopIn = (props: MotionProps) => (
@@ -14,6 +15,7 @@ const PopIn = (props: MotionProps) => (
     animate={{ scale: 1 }}
     transition={{ duration: props.duration ?? 0.3, type: "spring", delay: props.delay ?? 0 }}
     {...props}
+    onClick={props.onClick}
   >
     {props.children}
   </motion.div>

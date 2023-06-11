b992e20d7e3b18d5b92798493244d26051255619
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-11 11:42:06 -0700

    ✨ Update landing

--- next/src/components/PrimaryButton.tsx
+++ next/src/components/PrimaryButton.tsx
@@ -9,7 +9,7 @@ export default function PrimaryButton({ children, onClick }: PrimaryButtonProps)
     <button
       type="button"
       className={clsx(
-        "text-md rounded-full border border-blue-300 bg-blue-500/40 px-4 py-3 font-semibold text-blue-300 shadow-sm ",
+        "text-md rounded-full border border-blue-300 bg-blue-500/40 px-4 py-2 font-semibold text-blue-300 shadow-sm ",
         "transition-colors duration-300 hover:border-blue-200 hover:text-blue-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       )}
       onClick={onClick}

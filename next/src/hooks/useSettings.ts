1c81d1fa2b53d4abb4b5f109eb0ae37466956983
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-11 18:50:46 -0700

    ✨ Update useSettings to use Zustand state

--- next/src/hooks/useSettings.ts
+++ next/src/hooks/useSettings.ts
@@ -1,59 +1,26 @@
-import { useState } from "react";
-import type { ModelSettings, SettingModel } from "../utils/types";
+import { useEffect, useState } from "react";
 
-import { DEFAULT_MAX_LOOPS_CUSTOM_API_KEY, GPT_35_TURBO } from "../utils/constants";
+import { getDefaultModelSettings } from "../utils/constants";
+import type { ModelSettings } from "../types";
+import { useModelSettingsStore } from "../stores";
 
-const SETTINGS_KEY = "AGENTGPT_SETTINGS";
-const DEFAULT_SETTINGS: ModelSettings = {
-  customModelName: GPT_35_TURBO,
-  customTemperature: 0.9 as const,
-  customMaxLoops: DEFAULT_MAX_LOOPS_CUSTOM_API_KEY,
-  maxTokens: 300 as const,
+export type SettingsModel = {
+  settings: ModelSettings;
+  updateSettings: <Key extends keyof ModelSettings>(key: Key, value: ModelSettings[Key]) => void;
 };
 
-const loadSettings = () => {
-  const settings = { ...DEFAULT_SETTINGS };
+export function useSettings(): SettingsModel {
+  const [_modelSettings, set_ModelSettings] = useState<ModelSettings>(getDefaultModelSettings());
+  const modelSettings = useModelSettingsStore.use.modelSettings();
+  const updateSettings = useModelSettingsStore.use.updateSettings();
 
-  if (typeof window === "undefined") {
-    return settings;
-  }
-
-  const data = localStorage.getItem(SETTINGS_KEY);
-  if (!data) {
-    return settings;
-  }
-
-  try {
-    const obj = JSON.parse(data) as ModelSettings;
-    Object.entries(obj).forEach(([key, value]) => {
-      if (settings.hasOwnProperty(key)) {
-        // @ts-ignore
-        settings[key] = value;
-      }
-    });
-  } catch (error) {}
-
-  return settings;
-};
-
-export function useSettings(): SettingModel {
-  const [settings, setSettings] = useState<ModelSettings>(loadSettings);
-
-  const saveSettings = (settings: ModelSettings) => {
-    setSettings(settings);
-    localStorage.setItem(SETTINGS_KEY, JSON.stringify(settings));
-  };
-
-  const resetSettings = () => {
-    localStorage.removeItem(SETTINGS_KEY);
-    setSettings((_) => {
-      return { ...DEFAULT_SETTINGS };
-    });
-  };
+  // The server doesn't have access to local storage so rendering Zustand directly  will lead to a hydration error
+  useEffect(() => {
+    set_ModelSettings(modelSettings);
+  }, [modelSettings]);
 
   return {
-    settings,
-    saveSettings,
-    resetSettings,
+    settings: _modelSettings,
+    updateSettings: updateSettings,
   };
 }

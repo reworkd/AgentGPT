851e14857a8d65787f3e9bec869d0041c42c9804
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-11 23:22:41 -0700

    ✨ Update max loops

--- next/src/utils/constants.ts
+++ next/src/utils/constants.ts
@@ -5,7 +5,7 @@ export const GPT_35_TURBO = "gpt-3.5-turbo" as const;
 export const GPT_4 = "gpt-4" as const;
 export const GPT_MODEL_NAMES = [GPT_35_TURBO, GPT_4];
 
-export const DEFAULT_MAX_LOOPS_FREE = 4 as const;
+export const DEFAULT_MAX_LOOPS_FREE = 25 as const;
 export const DEFAULT_MAX_LOOPS_CUSTOM_API_KEY = 10 as const;
 
 export const getDefaultModelSettings = (): ModelSettings => {

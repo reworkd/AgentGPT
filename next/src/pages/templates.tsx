fec7c3450069722a85b12fde8b2153ddbe86edce
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-06-12 12:36:51 -0700

    fiexed on click styling

--- next/src/pages/templates.tsx
+++ next/src/pages/templates.tsx
@@ -8,23 +8,22 @@ import { GetStaticProps } from "next";
 import AppTitle from "../components/AppTitle";
 
 const categoryColors = {
-    "Games": "red",
-    "Academics": "red",
-    "Travel": "red",
-    "Web": "red",
-    "Social Media": "red",
-    "Communications": "red",
-    "Career": "red",
-    "Creative Writing": "red",
-    "Health & Wellness": "red",
-    "Business": "red",
-    "Personal Finance": "red",
-    "News & Media": "red",
-    "Event Planning": "red",
-    "Blogging": "red"
-}
-
-
+    "Games": "bg-teal-400",
+    "Academics": "bg-pink-900",
+    "Travel": "bg-blue-500",
+    "Web": "bg-green-600",
+    "Social Media": "bg-yellow-500",
+    "Communications": "bg-indigo-500",
+    "Career": "bg-purple-700",
+    "Creative Writing": "bg-red-500",
+    "Health & Wellness": "bg-green-500",
+    "Business": "bg-blue-600",
+    "Personal Finance": "bg-yellow-600",
+    "News & Media": "bg-red-600",
+    "Event Planning": "bg-indigo-600",
+    "Blogging": "bg-purple-500"
+  }
+  
 
 const Template = () => {
   const [activeCard, setActiveCard] = useState(null);
@@ -32,27 +31,27 @@ const Template = () => {
   const [isPromptOpen, setIsPromptOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [category, setCategory] = useState('All');
-  
+  const [promptInput, setPromptInput] = useState("");
   
   const cards = [
-    { name: "PlatformerGPT  🎮", category: "Games", description: "Write some code to make a platformer game.", promptTemplate: "Write some code to make _______" },
-    { name: "ResearchGPT  📜", category: "Academics", description: "Create a comprehensive report of a topic of your choice", promptTemplate: "Create a comprehensive report of _______" },
-    { name: "TravelGPT  🌴", category: "Travel", description: "Plan a detailed trip to destination", promptTemplate: "Plan a detailed trip to _______" },
-    { name: "ScraperGPT  ⚒️", category: "Web", description: "Scrap a website of your choice", promptTemplate: "Scrap _______ website and summarize the details" },
-    { name: "PostGPT  📮", category: "Social Media", description: "Thinks of captions and hashtags for your social media posts", promptTemplate: "Create a caption and hashtags for a social media post about _______" },
-    { name: "EmailGPT  📧 ", category: "Communications", description: "Compose a concise and detailed email", promptTemplate: "Compose a concise email about _______" },
-    { name: "ResumeGPT  📝", category: "Career", description: "Create a professional resume based on your career history.", promptTemplate: "Create a resume detailing your experience in _______" },
-    { name: "NovelGPT  📚", category: "Creative Writing", description: "Start writing a novel in your chosen genre.", promptTemplate: "Start writing a _______ genre novel" },
-    { name: "DietGPT  🥗", category: "Health & Wellness", description: "Plan a personalized diet based on your dietary preferences.", promptTemplate: "Plan a _______ diet" },
-    { name: "FitnessGPT  🏋️", category: "Health & Wellness", description: "Design a workout regimen based on your fitness goals.", promptTemplate: "Design a workout regimen for _______" },
-    { name: "MarketingGPT  📈", category: "Business", description: "Create a comprehensive marketing plan for your business.", promptTemplate: "Create a comprehensive marketing plan for _______" },
-    { name: "BudgetGPT  💰", category: "Personal Finance", description: "Prepare a personal or family budget plan.", promptTemplate: "Prepare a budget for _______" },
-    { name: "StudyGPT  📖", category: "Academics", description: "Create a study schedule based on your academic goals.", promptTemplate: "Create a study schedule for _______" },
-    { name: "NewsGPT  📰", category: "News & Media", description: "Write a detailed news article on a topic of your choice.", promptTemplate: "Write a news article on _______" },
-    { name: "EventPlannerGPT  🎉", category: "Event Planning", description: "Plan a detailed schedule for your upcoming event.", promptTemplate: "Plan a detailed schedule for _______ event" },
-    { name: "BlogGPT  📝", category: "Blogging", description: "Compose a blog post on a topic of your choice.", promptTemplate: "Compose a blog post about _______" }
-
-];
+    { name: "PlatformerGPT  🎮", category: "Games", description: "Write some code to make a platformer game.", promptTemplate: "Write some code to make a platformer game about 'Mario'.", placeholder: "Mario"},
+    { name: "ResearchGPT  📜", category: "Academics", description: "Create a comprehensive report of a topic of your choice", promptTemplate: "Create a comprehensive report of 'Global Warming'.", placeholder: "Global Warming" },
+    { name: "TravelGPT  🌴", category: "Travel", description: "Plan a detailed trip to destination", promptTemplate: "Plan a detailed trip to 'Paris'.", placeholder: "Paris" },
+    { name: "ScraperGPT  ⚒️", category: "Web", description: "Scrap a website of your choice", promptTemplate: "Scrap 'IMDb' website and summarize the details", placeholder: "IMDb" },
+    { name: "PostGPT  📮", category: "Social Media", description: "Thinks of captions and hashtags for your social media posts", promptTemplate: "Create a caption and hashtags for a social media post about 'Summer Vacation'.", placeholder: "Summer Vacation" },
+    { name: "EmailGPT  📧 ", category: "Communications", description: "Compose a concise and detailed email", promptTemplate: "Compose a concise email about 'Project Update'.", placeholder: "Project Update" },
+    { name: "ResumeGPT  📝", category: "Career", description: "Create a professional resume based on your career history.", promptTemplate: "Create a resume detailing your experience in 'Marketing'.", placeholder: "Marketing" },
+    { name: "NovelGPT  📚", category: "Creative Writing", description: "Start writing a novel in your chosen genre.", promptTemplate: "Start writing a 'Fantasy' genre novel", placeholder: "Fantasy" },
+    { name: "DietGPT  🥗", category: "Health & Wellness", description: "Plan a personalized diet based on your dietary preferences.", promptTemplate: "Plan a 'Vegetarian' diet", placeholder: "Vegetarian" },
+    { name: "FitnessGPT  🏋️", category: "Health & Wellness", description: "Design a workout regimen based on your fitness goals.", promptTemplate: "Design a workout regimen for 'Weight Loss'.", placeholder: "Weight Loss" },
+    { name: "MarketingGPT  📈", category: "Business", description: "Create a comprehensive marketing plan for your business.", promptTemplate: "Create a comprehensive marketing plan for 'Startup'.", placeholder: "Startup" },
+    { name: "BudgetGPT  💰", category: "Personal Finance", description: "Prepare a personal or family budget plan.", promptTemplate: "Prepare a budget for 'Family Vacation'.", placeholder: "Family Vacation" },
+    { name: "StudyGPT  📖", category: "Academics", description: "Create a study schedule based on your academic goals.", promptTemplate: "Create a study schedule for 'Final Exams'.", placeholder: "Final Exams" },
+    { name: "NewsGPT  📰", category: "News & Media", description: "Write a detailed news article on a topic of your choice.", promptTemplate: "Write a news article on 'Technology Advancements'.", placeholder: "Technology Advancements" },
+    { name: "EventPlannerGPT  🎉", category: "Event Planning", description: "Plan a detailed schedule for your upcoming event.", promptTemplate: "Plan a detailed schedule for 'Music Festival' event", placeholder: "Music Festival" },
+    { name: "BlogGPT  📝", category: "Blogging", description: "Compose a blog post on a topic of your choice.", promptTemplate: "Compose a blog post about 'Healthy Living'.", placeholder: "Healthy Living" }
+  ];
+  
 
 const filteredCards = cards.filter(card => 
     (card.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
@@ -61,23 +60,38 @@ const filteredCards = cards.filter(card =>
     (category === 'All' || card.category === category)
   );
 
-  const deployPrompt = () => {
-    const combinedPrompt = cards[activeCard]?.promptTemplate.replace("_______", prompt);
+const deployPrompt = () => {
+  const currentCard = cards[activeCard];
+  if (currentCard) {
+    const combinedPrompt = currentCard.promptTemplate.replace(currentCard.placeholder, promptInput);
     console.log("Prompt:", combinedPrompt);
     setIsPromptOpen(false);
-  };
+  }
+};
+
+  
 
   return (
     <SidebarLayout>
-      <AppTitle />
+      <AppTitle/>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="m-3 bg-white rounded">
-            <div className="relative m-4 flex w-full flex-wrap items-stretch">
-                <div className="flex w-full items-center justify-between space-x-3">
-                  <div className="w-1/3">
-                    <select id="category" name="category" className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" onChange={(e) => setCategory(e.target.value)}>
-                      <option>All</option>
-
+        <div className="m-4 bg-black shadow-md rounded-lg p-2 flex flex-col sm:flex-row items-start sm:items-center space-y-2 sm:space-y-0 sm:space-x-2">
+            <div className="flex-grow flex space-x-2 w-full">
+                <input
+                type="search"
+                className="flex-grow px-3 py-1 border border-gray-300 rounded-md placeholder-gray-500 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button className="hidden sm:inline-flex items-center px-3 py-1 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Enter
+                </button>
+            </div>
+            <div className="w-full sm:w-auto">
+                <select id="category" name="category" className="mt-1 block w-full py-1 px-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" onChange={(e) => setCategory(e.target.value)}>
+                    <option>All</option>
                     <option>Games</option>
                     <option>Academics</option>
                     <option>Travel</option>
@@ -92,20 +106,7 @@ const filteredCards = cards.filter(card =>
                     <option>News & Media</option>
                     <option>Event Planning</option>
                     <option>Blogging</option>
-                    {/* Add more categories as needed */}
-                    </select>
-                  </div>
-                  <div className="w-2/3">
-                    <input
-                      type="search"
-                      className="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      placeholder="Search"
-                      aria-label="Search"
-                      aria-describedby="button-addon2" 
-                      onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                  </div>
-                </div>
+                </select>
             </div>
         </div>
 
@@ -113,41 +114,42 @@ const filteredCards = cards.filter(card =>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {filteredCards.map((card, index) => (
             <Expand
-                className={`p-2 aspect-content aspect-w-1 aspect-h-1 border-black rounded bg-${activeCard === index ? 'white' : 'gray-500'} transform transition-transform duration-500 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer shadow-2xl`}
+                className={`p-2 aspect-content aspect-w-1 min-h-[125px]  border-white rounded bg-${activeCard === index ? 'black' : 'black'} transform transition-transform duration-500 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer shadow-2xl`}
                 key={index}
                 onClick={() => {
                 setActiveCard(index);
                 setIsPromptOpen(true);
                 }}
             >
-                <div className={`text-xs mb-1 inline-block px-2 bg-${categoryColors[card.category]}-500 rounded-full`}>{card.category}</div>
-                <div className={`h-full overflow-auto ${activeCard === index ? 'scale-110' : ''}`}>
-                <div className={`font-bold text-lg text-${activeCard === index ? 'gray-900' : 'white'} mb-2`}>{card.name}</div>
-                <div className={`text-sm text-${activeCard === index ? 'gray-700' : 'gray-300'}`}>
-                    {activeCard === index ? card.promptTemplate : card.description}
-                </div>
+                    <div className={`text-xs mb-1 inline-block px-2 ${categoryColors[card.category]} rounded-full`}>{card.category}</div>
+                    <div className={`h-full overflow-auto ${activeCard === index ? 'scale-100' : ''}`}>
+                    <div className={`font-bold text-lg text-${activeCard === index ? 'white' : 'white'} mb-2`}>{card.name}</div>
+                    <div className={`text-sm text-${activeCard === index ? 'white' : 'white'}`}>
+                        {activeCard === index ? card.promptTemplate : card.description}
+                    </div>
                 </div>
             </Expand>
             ))}
-
-
         </div> 
       </div>
     </div>
     {isPromptOpen && (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 transition-all duration-300 ease-in-out">
-        <div className="p-4 border border-gray-200 rounded shadow-lg transform scale-110 bg-white cursor-pointer transition-all duration-300 ease-in-out">
+        <div className="p-4 border w-80 h-70 border-gray-200 rounded shadow-lg transform scale-110 bg-black cursor-pointer transition-all duration-300 ease-in-out">
           <div className="space-y-4">
-            <div className="font-bold text-lg">{cards[activeCard]?.name}</div>
-            <div className="text-sm">{cards[activeCard]?.promptTemplate}</div>
+            <div className="font-bold text-lg text-white">{cards[activeCard]?.name}</div>
+            <div className={`text-xs mb-1 inline-block px-2 ${categoryColors[cards[activeCard]?.category]} rounded-full`}>{cards[activeCard]?.category}</div>
+            <div className="text-sm text-white">{cards[activeCard]?.promptTemplate}</div>
             <input
                 type="search"
-                class="..."
-                placeholder="Search"
-                aria-label="Search"
-                aria-describedby="button-addon2" 
-                onChange={(e) => setSearchQuery(e.target.value)}
+                className="w-full px-3 py-2 text-black placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                placeholder="Enter your variable"
+                aria-label="Variable"
+                aria-describedby="button-addon2"
+                onChange={(e) => setPromptInput(e.target.value)}
             />
+
+
 
             <div className="flex justify-end space-x-2">
               <button

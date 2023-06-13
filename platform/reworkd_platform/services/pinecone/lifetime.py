932750d9cf58eddce79532edef7d0e94d4aca165
awtkns <32209255+awtkns@users.noreply.github.com>
2023-06-12 19:38:29 -0700

    🍦Add pinecone

--- platform/reworkd_platform/services/pinecone/lifetime.py
+++ platform/reworkd_platform/services/pinecone/lifetime.py
@@ -1,9 +1,9 @@
 import pinecone
 
-from reworkd_platform.settings import Settings
+from reworkd_platform.settings import settings
 
 
-def init_pinecone(settings: Settings) -> None:
+def init_pinecone() -> None:
     if settings.pinecone_api_key and settings.pinecone_environment:
         pinecone.init(
             api_key=settings.pinecone_api_key,

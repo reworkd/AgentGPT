5de6595a7e3eb125377da5679d7cf4a3caf782bf
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-06-13 17:57:31 -0700

    Template card styling

--- next/src/components/templates/TemplateCard.tsx
+++ next/src/components/templates/TemplateCard.tsx
@@ -1,23 +1,26 @@
 import clsx from "clsx";
 import type { TemplateModel } from "./TemplateData";
+import { categoryColors } from './TemplateCategoryColours';
 
 type TemplateCardProps = {
   model: TemplateModel;
 };
 
 const TemplateCard = ({ model }: TemplateCardProps) => {
+
   return (
     <div
       className={clsx(
-        "max-w-full cursor-pointer rounded-lg border-2 border-white/20 bg-zinc-900 p-2 text-white sm:max-w-[350px]",
-        "transition-colors hover:bg-zinc-800"
+        "h-34 w-72 space-y-2 whitespace-normal rounded-2xl border max-w-sm border-white/20 p-4 text-left transition-all duration-100 ",
+        "bg-zinc-900 transition-colors hover:bg-zinc-800"
       )}
     >
-      <div className={`h-full min-h-[100px]`}>
-        <div className={`text-md mb-2 font-bold`}>{model.name}</div>
-        <div className={`mb-1 text-xs text-gray-400`}>Category: {model.category}</div>
-        <div className={`text-sm`}>{model.description}</div>
-      </div>
+        <div className="flex items-center">
+          <div className="text-xl text-white mr-2">{model.icon}</div>
+          <div className={`text-md text-white mb-0 font-bold`}>{model.name}</div>
+        </div>
+        <div className={clsx(`mb-2 text-xs inline-block rounded-full px-0 text-gray-400`, )}>{model.category}</div>
+        <div className={`text-sm text-gray-200 `}>{model.description}</div>
     </div>
   );
 };
--- next/src/pages/templates.tsx
+++ next/src/pages/templates.tsx
@@ -1,26 +1,38 @@
 import SidebarLayout from "../layout/sidebar";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import React from "react";
+import React, { useState } from "react";
 import nextI18NextConfig from "../../next-i18next.config.js";
 import { languages } from "../utils/languages";
 import type { GetStaticProps } from "next";
 import { TEMPLATE_DATA } from "../components/templates/TemplateData";
 import TemplateCard from "../components/templates/TemplateCard";
 import FadeIn from "../components/motions/FadeIn";
+import SearchBar from "../components/templates/TemplateSearch";
 
 const Templates = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [category, setCategory] = useState("");
+
+  const filteredData = TEMPLATE_DATA.filter((model) => {
+    const matchQuery = model.name.toLowerCase().includes(searchQuery.toLowerCase());
+    const matchCategory = category === "" || model.category === category;
+    return matchQuery && matchCategory;
+  });
+
   return (
     <SidebarLayout>
-      <div className="h-full w-full p-10">
+      <div className="flex flex-col p-10 h-full w-full ">
         <FadeIn initialX={-45} initialY={0}>
-          <h1 className="text-4xl font-bold text-white">Templates</h1>
-          <h2 className="mb-4 text-xl font-thin text-white">
-            Customizable and ready to deploy agents
-          </h2>
+          <div className="ml-12">
+            <h1 className="text-4xl font-bold text-white">Templates</h1>
+            <h2 className="mb-4 text-xl font-thin text-white">
+              Customizable and ready to deploy agents
+            </h2>
+          </div>
         </FadeIn>
         <FadeIn initialY={45}>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {TEMPLATE_DATA.map((model) => (
+          <div className="flex flex-wrap gap-4 justify-center">
+            {filteredData.map((model) => (
               <TemplateCard key={model.name + model.description} model={model} />
             ))}
           </div>

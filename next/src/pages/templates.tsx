ba300112c6e04bc930ba83b05b070848ce639b22
Jasan Gill <56135840+jasangill1@users.noreply.github.com>
2023-06-13 19:15:47 -0700

    💅 Template styling and search (#799)

--- next/src/pages/templates.tsx
+++ next/src/pages/templates.tsx
@@ -1,26 +1,40 @@
 import SidebarLayout from "../layout/sidebar";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import React from "react";
+import React, { useState } from "react";
 import nextI18NextConfig from "../../next-i18next.config.js";
 import { languages } from "../utils/languages";
 import type { GetStaticProps } from "next";
 import { TEMPLATE_DATA } from "../components/templates/TemplateData";
 import TemplateCard from "../components/templates/TemplateCard";
 import FadeIn from "../components/motions/FadeIn";
+import SearchBar from "../components/templates/TemplateSearch";
 
 const Templates = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [category, setCategory] = useState("");
+
+  const filteredData = TEMPLATE_DATA.filter((model) => {
+    const matchQuery = model.name.toLowerCase().includes(searchQuery.toLowerCase());
+    const matchCategory =
+      category === "" || model.category.toLowerCase() === category.toLowerCase();
+    return matchQuery && matchCategory;
+  });
+
   return (
     <SidebarLayout>
-      <div className="h-full w-full p-10">
+      <div className="flex h-full w-full flex-col p-10">
         <FadeIn initialX={-45} initialY={0}>
-          <h1 className="text-4xl font-bold text-white">Templates</h1>
-          <h2 className="mb-4 text-xl font-thin text-white">
-            Customizable and ready to deploy agents
-          </h2>
+          <div>
+            <h1 className="text-4xl font-bold text-white">Templates</h1>
+            <h2 className="mb-4 text-xl font-thin text-white">
+              Customizable and ready to deploy agents
+            </h2>
+          </div>
         </FadeIn>
         <FadeIn initialY={45}>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {TEMPLATE_DATA.map((model) => (
+          <SearchBar setSearchQuery={setSearchQuery} setCategory={setCategory} />
+          <div className="flex flex-wrap justify-center gap-4">
+            {filteredData.map((model) => (
               <TemplateCard key={model.name + model.description} model={model} />
             ))}
           </div>

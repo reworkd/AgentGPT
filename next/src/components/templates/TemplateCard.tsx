9280993ca185708c87bef84fcc4564f211aa84b3
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-06-13 20:19:56 -0700

    Fix

--- next/src/components/templates/TemplateCard.tsx
+++ next/src/components/templates/TemplateCard.tsx
@@ -9,7 +9,7 @@ const TemplateCard = ({ model }: TemplateCardProps) => {
   return (
     <div
       className={clsx(
-        "h-34 w-full space-y-2 whitespace-normal rounded-2xl border max-w-lg border-white/20 p-4 text-left transition-all duration-100 ",
+        "h-34 w-full space-y-2 whitespace-normal rounded-2xl border max-w-lg border-white/20 p-4 text-left transition-all duration-100",
         "bg-zinc-900 transition-colors hover:bg-zinc-800"
       )}
     >
@@ -17,17 +17,10 @@ const TemplateCard = ({ model }: TemplateCardProps) => {
         <div className="mr-2 text-xl text-white">{model.icon}</div>
         <div className={`text-md mb-0 font-bold text-white`}>{model.name}</div>
       </div>
-      <div className={clsx(`mb-2 inline-block rounded-full px-0 text-xs text-gray-400`)}>
-        {model.category}
-      <div className="flex items-center">
-        <div className="mr-2 text-xl text-white">{model.icon}</div>
-        <div className={`text-md mb-0 font-bold text-white`}>{model.name}</div>
-      </div>
-      <div className={clsx(`mb-2 inline-block rounded-full px-0 text-xs text-gray-400`)}>
+      <div className={clsx(`mb-2 inline-block rounded-full px-2 text-xs text-gray-400`)}>
         {model.category}
       </div>
-      <div className={`text-sm text-gray-200 `}>{model.description}</div>
-      <div className={`text-sm text-gray-200 `}>{model.description}</div>
+      <div className={`text-sm text-gray-200`}>{model.description}</div>
     </div>
   );
 };

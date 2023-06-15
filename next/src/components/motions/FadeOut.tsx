e30f04b4c81b879611d2423a508fc13d49b86ed7
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-14 19:43:10 -0700

    💅 Revert

--- next/src/components/motions/FadeOut.tsx
+++ next/src/components/motions/FadeOut.tsx
@@ -4,13 +4,11 @@ import type { PropsWithChildren } from "react";
 interface MotionProps extends PropsWithChildren {
   className?: string;
   delay?: number;
-  exitY?: number;
-  exitX?: number;
 }
 
 const FadeOut = (props: MotionProps) => (
   <motion.div
-    exit={{ opacity: 0, x: props.exitX ?? -100, y: props.exitY ?? 0 }}
+    exit={{ opacity: 0, x: -100 }}
     animate={{ scale: 1 }}
     transition={{ duration: 0.5, type: "spring", delay: props.delay ?? 0 }}
     {...props}

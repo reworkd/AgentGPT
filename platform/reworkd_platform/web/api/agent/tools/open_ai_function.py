e81947a6574e832d6cd6dd14dd133d62583abfec
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-16 22:20:20 -0700

    ✨ Create method to make an OpenAI function from a given tool

--- platform/reworkd_platform/web/api/agent/tools/open_ai_function.py
+++ platform/reworkd_platform/web/api/agent/tools/open_ai_function.py
@@ -15,6 +15,34 @@ class FunctionDescription(TypedDict):
     """The parameters of the function."""
 
 
+def get_tool_function(tool: Type[Tool]) -> FunctionDescription:
+    """A function that will return the tool's function specification"""
+    name = get_tool_name(tool)
+
+    return {
+        "name": name,
+        "description": tool.description,
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "reasoning": {
+                    "type": "string",
+                    "description": (
+                        f"Reasoning is how the task will be accomplished with {name} tool. "
+                        "Detail your overall plan along with any concerns you have."
+                        "Ensure this reasoning value is in the user defined langauge "
+                    ),
+                },
+                "arg": {
+                    "type": "string",
+                    "description": tool.arg_description,
+                },
+            },
+            "required": ["reasoning", "arg"],
+        },
+    }
+
+
 def analysis_function(tools: List[Type[Tool]]) -> FunctionDescription:
     """A function that will return the tool specifications from OpenAI"""
     tool_names = [get_tool_name(tool) for tool in tools]

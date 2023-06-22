3f8f7a6d28b67724bd21ddecf18eb663af0ae313
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-22 13:15:27 -0700

    🔥 Add with retries

--- next/src/services/agent/agent-api.ts
+++ next/src/services/agent/agent-api.ts
@@ -10,12 +10,10 @@ type ApiProps = Pick<RequestBody, "model_settings" | "goal"> & {
 
 export class AgentApi {
   readonly props: ApiProps;
-  readonly onError: (e: unknown) => never;
   runId: string | undefined;
 
-  constructor(apiProps: ApiProps, onError: (e: unknown) => never) {
+  constructor(apiProps: ApiProps) {
     this.props = apiProps;
-    this.onError = onError;
   }
 
   async getInitialTasks(): Promise<string[]> {
@@ -51,7 +49,6 @@ export class AgentApi {
     url: string,
     data: Omit<RequestBody, "goal" | "model_settings" | "run_id">
   ) {
-    useAgentStore.getState().setIsAgentThinking(true);
     const requestBody: RequestBody = {
       model_settings: this.props.model_settings,
       goal: this.props.goal,
@@ -60,6 +57,7 @@ export class AgentApi {
     };
 
     try {
+      useAgentStore.getState().setIsAgentThinking(true);
       const { run_id, ...data } = await apiUtils.post<T & { run_id: string }>(
         url,
         requestBody,
@@ -69,9 +67,32 @@ export class AgentApi {
       if (this.runId === undefined) this.runId = run_id;
       return data;
     } catch (e) {
-      this.onError(e);
+      throw e;
     } finally {
       useAgentStore.getState().setIsAgentThinking(false);
     }
+  }
+}
+
+export async function withRetries(
+  fn: () => Promise<void>,
+  onSuccess: () => Promise<void>,
+  onError: (error: unknown) => void,
+  retries = 3
+): Promise<void> {
+  const RETRY_TIMEOUT = 2000;
+
+  for (let i = 1; i < retries + 1; i++) {
+    try {
+      await fn();
+      await onSuccess();
+      return;
+    } catch (error) {
+      onError(error);
+      if (i === retries) {
+        return;
+      }
+    }
+    await new Promise((r) => setTimeout(r, RETRY_TIMEOUT));
   }
 }

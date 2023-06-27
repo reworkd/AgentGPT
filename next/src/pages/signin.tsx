643129aef8ce9d237ef4d32371c7afcaa938eb7c
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-27 16:29:44 -0700

    ✨Update sign in

--- next/src/pages/signin.tsx
+++ next/src/pages/signin.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import clsx from "clsx";
-import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import type { GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import { FaDiscord, FaGithub, FaGoogle } from "react-icons/fa";
+import type { ClientSafeProvider } from "next-auth/react";
 import { getProviders, signIn, useSession } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../server/auth/auth";
@@ -11,15 +12,18 @@ import FadeIn from "../components/motions/FadeIn";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Input from "../components/Input";
+import type { LiteralUnion } from "next-auth/react/types";
+import type { BuiltInProviderType } from "next-auth/providers";
 
-const SignIn = ({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const SignIn = ({ providers }: { providers: Provider }) => {
   const { data: session } = useSession();
   const { push } = useRouter();
-  const [usernameValue, setUsernameValue] = useState("");
 
-  if (session) {
-    push("/").catch(console.error);
-  }
+  if (session) push("/").catch(console.error);
+
+  const details = Object.values(providers)
+    .map((provider) => providerButtonDetails[provider.id])
+    .filter((detail): detail is ButtonDetail => detail !== undefined);
 
   return (
     <>
@@ -27,56 +31,31 @@ const SignIn = ({ providers }: InferGetServerSidePropsType<typeof getServerSideP
         <title>Sign in - AgentGPT</title>
       </Head>
 
-      <div className="radial-background-1 h-screen w-screen bg-black">
-        <div className="flex h-full w-full items-center justify-center pb-8">
+      <div className="radial-background-1 grid h-screen w-screen place-items-center bg-black">
+        <div className="flex h-full w-full max-w-screen-lg flex-col items-center justify-center gap-10">
           <FadeIn
             duration={1.5}
-            delay={0}
-            className="flex max-w-screen-lg flex-1 flex-col items-center justify-center text-white"
+            initialY={-50}
+            className="flex flex-col items-center justify-center gap-6 text-white"
           >
             <div className="flex flex-row gap-6">
               <Image src="logo-white.svg" width="56" height="56" alt="Reworkd AI" />
-
               <h1
                 className={`${clsx(
                   "bg-gradient-to-br from-white via-neutral-300 to-neutral-500 bg-clip-text text-transparent",
-                  "text-center text-4xl font-bold leading-[1.1em] tracking-[-0.64px] sm:text-5xl"
+                  "text-center text-3xl font-bold leading-[1.1em] tracking-[-0.64px] md:text-5xl"
                 )}`}
               >
-                Welcome to AgentGPT
+                <span className="hidden sm:flex">Welcome to AgentGPT</span>
+                <span className="flex sm:hidden">AgentGPT</span>
               </h1>
             </div>
-            {providers["credentials"] && (
-              <div>
-                <Input
-                  value={usernameValue}
-                  onChange={(e) => setUsernameValue(e.target.value)}
-                  placeholder="Enter Username"
-                  type="text"
-                />
-                <button
-                  onClick={() => {
-                    if (!usernameValue) {
-                      return;
-                    }
-
-                    signIn("credentials", {
-                      callbackUrl: "/",
-                      name: usernameValue,
-                    }).catch(console.error);
-                  }}
-                  className={`mb-4 mt-4 flex items-center rounded-md bg-white px-10 py-3 text-base font-semibold text-black shadow-md transition-colors duration-300 hover:bg-gray-200 sm:px-16 sm:py-5 sm:text-lg`}
-                >
-                  Sign in {providers["credentials"]?.name}
-                </button>
-              </div>
-            )}
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <div key={provider.id}>
-                  <SignInBtn provider={buttonDetails[provider.id]} id={provider.id} />
-                </div>
-              ))}
+          </FadeIn>
+          <FadeIn duration={1.5} delay={0.4} initialY={50}>
+            {providers.credentials && <InsecureSignin />}
+            {details.map((detail) => (
+              <ProviderSignInButton key={detail.id} detail={detail} />
+            ))}
           </FadeIn>
         </div>
       </div>
@@ -84,51 +63,72 @@ const SignIn = ({ providers }: InferGetServerSidePropsType<typeof getServerSideP
   );
 };
 
-const SignInBtn: React.FC<{
-  provider: { name: string; icon: JSX.Element; color: string } | undefined;
-  id: string;
-}> = (props) => {
-  const { provider, id } = props;
-  return provider ? (
-    <button
-      onClick={() => {
-        signIn(id, { callbackUrl: "/" }).catch(console.error);
-      }}
-      className={`${provider.color}  mb-4 flex items-center rounded-md px-10 py-3 text-base font-semibold shadow-md transition-colors duration-300 sm:px-16 sm:py-5 sm:text-xl`}
-    >
-      {provider.icon}
-      Sign in with {provider.name}
-    </button>
-  ) : (
-    <></>
+const InsecureSignin = () => {
+  const [usernameValue, setUsernameValue] = useState("");
+  return (
+    <div>
+      <Input
+        value={usernameValue}
+        onChange={(e) => setUsernameValue(e.target.value)}
+        placeholder="Enter Username"
+        type="text"
+      />
+      <button
+        onClick={() => {
+          if (!usernameValue) return;
+
+          signIn("credentials", { callbackUrl: "/", name: usernameValue }).catch(console.error);
+        }}
+        className={clsx(
+          "mb-4 mt-4 flex items-center rounded-md bg-white px-10 py-3 text-sm font-semibold text-black sm:text-base",
+          "transition-colors duration-300 hover:bg-gray-200",
+          !usernameValue && "cursor-not-allowed"
+        )}
+      >
+        Sign in with username (Insecure)
+      </button>
+    </div>
   );
 };
 
-type ButtonDetail = {
-  [key: string]:
-    | {
-        name: string;
-        icon: JSX.Element;
-        color: string;
-      }
-    | undefined;
-};
-const buttonDetails: ButtonDetail = {
+type Provider = Record<LiteralUnion<BuiltInProviderType>, ClientSafeProvider>;
+
+interface ButtonDetail {
+  id: string;
+  icon: JSX.Element;
+  color: string;
+}
+
+const providerButtonDetails: { [key: string]: ButtonDetail } = {
   google: {
-    name: "Google",
+    id: "google",
     icon: <FaGoogle className="mr-2" />,
     color: "bg-white hover:bg-gray-200 text-black",
   },
   discord: {
-    name: "Discord",
+    id: "discord",
     icon: <FaDiscord className="mr-2" />,
-    color: "bg-purple-700 hover:bg-purple-600 text-white",
+    color: "bg-blue-600 hover:bg-blue-700 text-white",
   },
   github: {
-    name: "Github",
+    id: "github",
     icon: <FaGithub className="mr-2" />,
-    color: "bg-gray-900 hover:bg-gray-800 text-white",
+    color: "bg-gray-800 hover:bg-gray-900 text-white",
   },
+};
+
+const ProviderSignInButton = ({ detail }: { detail: ButtonDetail }) => {
+  return (
+    <button
+      onClick={() => {
+        signIn(detail.id, { callbackUrl: "/" }).catch(console.error);
+      }}
+      className={`${detail.color} mb-4 flex items-center rounded-md px-10 py-3 text-base font-semibold shadow-md transition-colors duration-300 sm:px-16 sm:py-5 sm:text-xl`}
+    >
+      {detail.icon}
+      Sign in with {detail.id}
+    </button>
+  );
 };
 
 export default SignIn;
@@ -146,6 +146,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   const providers = await getProviders();
   return {
-    props: { providers: providers ?? [] },
+    props: { providers: providers ?? {} },
   };
 }

c3a33e239ad5ca2b7b251c967923ceac002893d1
asim-shrestha <asim.shrestha@hotmail.com>
2023-06-29 12:03:45 -0700

    ✨ Fix tests

--- next/__tests__/agent-api.test.ts
+++ next/__tests__/agent-api.test.ts
@@ -1,85 +1,70 @@
-import "../__mocks__/matchMedia.mock"
-import { AgentApi, withRetries } from "../src/services/agent/agent-api";
-import type {ApiModelSettings} from "../src/utils/interfaces";
+import "../__mocks__/matchMedia.mock";
+import { withRetries } from "../src/services/agent/agent-api";
 
 describe("withRetries", () => {
-  let instance: AgentApi;
-  const modelSettings: ApiModelSettings = {
-    language: "", max_tokens: 0, model: "gpt-4", temperature: 0
-
-  }
-
-  beforeEach( () => {
-    instance = new AgentApi({goal: "", model_settings: modelSettings})
-  })
-
   it("should retry 3 times by default", async () => {
-    let nbTries = 0;
+    let numTries = 0;
 
     await withRetries(
       (): Promise<void> => {
-        ++nbTries;
-        throw new Error()
+        ++numTries;
+        throw new Error();
       },
       (): Promise<boolean> => {
-        return Promise.resolve(true)
-      },
-    )
+        return Promise.resolve(true);
+      }
+    );
 
-    expect(nbTries).toEqual(4)
+    expect(numTries).toEqual(4);
   });
 
-  it("should retry nbRetries times on error", async () => {
-    const nbRetries = 5;
-    let nbTries = 0;
+  it("should retry numRetries times on error", async () => {
+    const numRetries = 5;
+    let numTries = 0;
 
     await withRetries(
       (): Promise<void> => {
-        ++nbTries;
-        throw new Error()
+        ++numTries;
+        throw new Error();
       },
       (): Promise<boolean> => {
-        return Promise.resolve(true)
+        return Promise.resolve(true);
       },
-      nbRetries
-    )
+      numRetries
+    );
 
-    expect(nbTries).toEqual(nbRetries+1)
+    expect(numTries).toEqual(numRetries + 1);
   });
 
-  it("should not retry", async () => {
-    const nbRetries = 0;
-    let nbTries = 0;
+  it("should retry if onError returns true", async () => {
+    let numTries = 0;
 
     await withRetries(
       (): Promise<void> => {
-        ++nbTries;
-        throw new Error()
+        ++numTries;
+        throw new Error();
       },
       (): Promise<boolean> => {
-        return Promise.resolve(true)
-      },
-      nbRetries
-    )
+        return Promise.resolve(true);
+      }
+    );
 
-    expect(nbTries).toEqual(1)
+    expect(numTries).toBeGreaterThan(1);
   });
 
-  it("should stop on error", async () => {
-    const nbRetries = 3;
-    let nbTries = 0;
+  it("should stop if onError returns false", async () => {
+    let numTries = 0;
 
     await withRetries(
       (): Promise<void> => {
-        ++nbTries;
-        throw new Error()
+        ++numTries;
+        throw new Error();
       },
       (): Promise<boolean> => {
-        return Promise.resolve(false)
-      },
-      nbRetries
-    )
+        return Promise.resolve(false);
+      }
+    );
 
-    expect(nbTries).toEqual(1)
+    expect(numTries).toEqual(1);
   });
 });

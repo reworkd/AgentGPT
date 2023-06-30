21939545668db4806722d64d8c21c5bc3d92b3e9
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-06-30 13:01:23 -0700

    Update Hero title Gradient

--- next/src/components/landing/Hero.tsx
+++ next/src/components/landing/Hero.tsx
@@ -5,7 +5,6 @@ import PrimaryButton from "../PrimaryButton";
 import Backing from "./Backing";
 import React from "react";
 import { useRouter } from "next/router";
-import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   const router = useRouter();
@@ -27,16 +26,30 @@ const Hero = () => {
         </div>
         <h1
           className={clsx(
-            "bg-gradient-to-br from-white via-neutral-300 to-neutral-500 bg-clip-text text-transparent",
-            "text-center text-4xl font-bold leading-[42px] tracking-[-0.64px] sm:text-6xl sm:leading-[1.1em]"
+            "text-left text-4xl font-bold leading-[42px] tracking-[-0.64px] sm:text-6xl sm:leading-[1.1em]",
+            "bg-clip-text text-transparent",
+            "bg-gradient-to-br from-white via-neutral-300 to-neutral-500"
           )}
+          style={{
+            backgroundImage:
+              "linear-gradient(172.22deg, #FFFFFF 13.42%, rgba(255, 255, 255, 0.22) 154.02%)",
+            fontFamily: "Satoshi Variable",
+            fontSize: "68px",
+            fontWeight: 700,
+            lineHeight: "68px",
+            letterSpacing: "0px",
+            textAlign: "left",
+          }}
         >
           <div>
-            Autonomous AI Agents
+            Autonomous AI
             <br />
-            at Your Fingertips
+            Agents At Your
+            <br />
+            Fingertips
           </div>
         </h1>
+
         <p className="my-2 mb-9 inline-block w-full text-center align-top font-thin text-neutral-300">
           The leading web based autonomous agent platform. Automate business processes at scale.
         </p>
@@ -47,26 +60,7 @@ const Hero = () => {
         delay={0.6}
         className="flex w-full flex-col items-center gap-2 sm:flex-row"
       >
-        <div className="w-full cursor-text rounded-full border border-white/30 p-2 px-4 font-thin text-neutral-100 transition-colors hover:border-white/60 sm:flex-1">
-          <TypeAnimation
-            sequence={[
-              1750,
-              "Research the latest trends in AI and ML 🔍",
-              1500,
-              "Plan me a destination wedding in Hawaii 🌴",
-              1500,
-              "Write me a NextJS Todo App using server components 🗒️",
-              1500,
-              "Create infinite paper clips 📎",
-              1500,
-              "Create infinite paper clips 📎... 😈",
-              3000,
-            ]}
-            wrapper="span"
-            speed={75}
-            repeat={Infinity}
-          />
-        </div>
+        <div className="w-full cursor-text rounded-full border border-white/30 p-2 px-4 font-thin text-neutral-100 transition-colors hover:border-white/60 sm:flex-1"></div>
         <PrimaryButton
           onClick={() => {
             router.push("/").catch(console.error);

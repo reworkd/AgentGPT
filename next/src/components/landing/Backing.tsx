5129db55390e02b83aebf4784a64a2ba08069faa
Jasan Gill <56135840+jasangill1@users.noreply.github.com>
2023-06-30 18:53:19 -0700

    👈 Updated Backing for Hero Component  (#919)

--- next/src/components/landing/Backing.tsx
+++ next/src/components/landing/Backing.tsx
@@ -1,28 +1,28 @@
 const Backing = () => {
   return (
-    <div className="font-white relative flex flex-col items-center gap-x-2 text-xs sm:flex-row md:text-sm">
-      <div className="font-thin">Backed by</div>
-      <div className="flex flex-row items-center gap-2">
+    <div className="font-white relative flex flex-col items-center text-xs md:text-sm">
+      <div className="flex flex-row items-center gap-1 font-thin text-gray-400">
+        <div className="mr-1 font-thin">Backed By:</div>
         <a
-          className="flex cursor-pointer flex-row items-center gap-1 font-bold transition-transform duration-300 hover:scale-105"
+          className="flex cursor-pointer flex-row items-center gap-1 transition-transform duration-300 hover:scale-105"
           href="https://www.ycombinator.com/companies/reworkd"
           target="_blank"
         >
           <div className="relative p-[1px]">
-            <div className="absolute inset-0 rotate-180 rounded-lg bg-gradient-to-br from-orange-100 via-orange-400 to-orange-700"></div>
-            <div className="relative flex items-center justify-center rounded-lg bg-gradient-to-br from-orange-400 via-orange-500 to-orange-700 px-1.5 py-0.5">
+            <div className="absolute inset-0 bg-gray-400"></div>
+            <div className="relative flex items-center justify-center px-2 font-sans text-lg text-black">
               Y
             </div>
           </div>
           <div>Combinator</div>
         </a>
-        <div className="font-thin">&</div>
+        <div>&</div>
         <a
-          className="cursor-pointer font-bold transition-transform duration-300 hover:scale-105"
+          className="cursor-pointer transition-transform duration-300 hover:scale-105"
           href="https://www.panache.vc/"
           target="_blank"
         >
-          Panache ventures
+          Panache Ventures
         </a>
       </div>
     </div>

7acc3ec7ac5c8aeefd21053b4802bdc4a050fd01
Joe Shen <joeshen123@gmail.com>
2023-06-30 18:54:55 -0700

    🔆 feat(#750): implement toggle for light/dark mode (#894)

--- next/src/hooks/useTheme.ts
+++ next/src/hooks/useTheme.ts
@@ -1,36 +1,43 @@
-import { type Theme, THEMES } from "../types";
 import { useEffect } from "react";
-const DARK_THEME = "dark";
+import { useThemeStore } from "../stores";
 
-export const handleTheme = (theme, event?) => {
+export const handleTheme = (theme) => {
   if (typeof document === "undefined" || typeof window === "undefined") {
     return;
   }
-
   const classList = document.documentElement.classList;
+  const DARK_THEME = "dark";
 
   // true if user's system has dark theme
-  const isSystemThemeDark = (event || window?.matchMedia(`(prefers-color-scheme: ${DARK_THEME})`))
-    .matches;
-
+  const isSystemThemeDark = window?.matchMedia(`(prefers-color-scheme: ${DARK_THEME})`).matches;
   // determine whether App should have dark theme
   const shouldAppThemeBeDark = theme === DARK_THEME || (theme === "system" && isSystemThemeDark);
 
   if (shouldAppThemeBeDark && !classList.contains(DARK_THEME)) {
     classList.add(DARK_THEME);
-  } else {
+  } else if (!shouldAppThemeBeDark) {
     classList.remove(DARK_THEME);
   }
 };
 
-export const useTheme = (theme: Theme) => {
-  theme = THEMES.includes(theme) ? theme : "system";
+export const useTheme = () => {
+  const theme = useThemeStore.use.theme();
+  const setTheme = useThemeStore.use.setTheme();
+  const handleSetTheme = (theme) => {
+    setTheme(theme);
+    handleTheme(theme);
+  };
 
   useEffect(() => {
-    const prefersDark = window.matchMedia(`(prefers-color-scheme: ${DARK_THEME})`);
+    const prefersDark = window.matchMedia(`(prefers-color-scheme: dark)`);
 
-    prefersDark.addEventListener("change", (event) => {
-      handleTheme(theme, event);
+    prefersDark.addEventListener("change", () => {
+      handleTheme(theme);
     });
   }, []);
+
+  return {
+    theme,
+    setTheme: handleSetTheme,
+  };
 };

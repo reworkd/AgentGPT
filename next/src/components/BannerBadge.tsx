e421ffa024cafe9769dbd43b52d2e6c8fc6b2de8
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-02 12:59:23 -0700

    Update banner
    ## About
    
    -Fixed artifact
    -Used arrow
    
    ## Notes
    
    Is this animation what we want or do we want the pulse runing through the banner not just hugging one side ?
    
    -Also is inline cs sytling alright for this ?

--- next/src/components/BannerBadge.tsx
+++ next/src/components/BannerBadge.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaStar } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 import clsx from "clsx";
 
 type BannerBadgeProps = {
@@ -8,18 +8,30 @@ type BannerBadgeProps = {
 };
 
 const BannerBadge = ({ children, onClick }: BannerBadgeProps) => {
+  const badgeStyles = clsx(
+    "relative flex w-max cursor-pointer items-center gap-1 rounded-full font-thin",
+    "border-2 border-purple-300/20 p-1 text-sm text-[#CC98FF] transition-colors duration-300",
+    "before:absolute before:inset-0 before:animate-pulse before:bg-gradient-to-r before:from-purple-300/20 before:to-transparent",
+    "before:content-' ' before:overflow-hidden before:rounded-full"
+  );
+
   return (
-    <div
-      className={clsx(
-        "relative flex w-max cursor-pointer items-center gap-1 rounded-full font-thin",
-        "border-2 border-purple-300/20 p-1 text-xs text-purple-300 transition-colors duration-300",
-        "before:absolute before:inset-0 before:animate-pulse before:bg-gradient-to-r before:from-purple-300/20 before:to-transparent"
-      )}
-      onClick={onClick}
-    >
-      <FaStar className="ml-2 text-purple-300" />
+    <div className={badgeStyles} onClick={onClick}>
+      <style>{`
+        @keyframes pulse-animation {
+          0% {
+            background-position: 0% 50%;
+          }
+          50% {
+            background-position: 100% 50%;
+          }
+          100% {
+            background-position: 0% 50%;
+          }
+        }
+      `}</style>
       <span>{children}</span>
-      <div />
+      <FaArrowRight className="mx-1 text-[#CC98FF]" />
     </div>
   );
 };

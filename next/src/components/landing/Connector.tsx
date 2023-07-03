f3e38454797343664383e9e378422de0c6ddac8e
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-02 20:09:39 -0700

    Changed slate to purple

--- next/src/components/landing/Connector.tsx
+++ next/src/components/landing/Connector.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Connector = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-slate-900 from-10% via-transparent via-70% to-transparent "></div>
+    <div className="flex h-screen w-screen items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-950 from-5% via-black via-70% to-transparent "></div>
   );
 };
 

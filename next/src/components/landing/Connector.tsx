6a4ecaa9aa28f0a8b913d12944692323e5ca4b3a
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-02 21:46:01 -0700

    😵‍💫 We love gradients

--- next/src/components/landing/Connector.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ConnectorSection = () => {
-  return (
-    <div className="flex h-screen w-full items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-950 from-5% via-black via-70% to-transparent "></div>
-  );
-};
-
-export default ConnectorSection;
---
+++ next/src/components/landing/ConnectorSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ConnectorSection = () => {
+  return (
+    <div className="from-1% h-screen w-screen items-center justify-center overflow-hidden bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-950 via-black via-70% to-transparent opacity-60 "></div>
+  );
+};
+
+export default ConnectorSection;

29f7a4c82a6ab73e7064850a7cdbd8e4fb663bfd
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-02 23:09:23 -0700

    ✨ Fix sidebar

--- next/src/components/drawer/Sidebar.tsx
+++ next/src/components/drawer/Sidebar.tsx
@@ -29,85 +29,85 @@ const Sidebar = ({ show, setShow }: SidebarProps) => {
   const userAgents = data ?? [];
 
   return (
-    // <SidebarTransition show={show}>
-    <nav className="flex flex-1 flex-col bg-neutral-900 px-4 py-3 ring-1 ring-white/10">
-      <div className="flex flex-row items-center justify-between">
-        <Image src="logo-white.svg" width="25" height="25" alt="Reworkd AI" className="ml-2" />
-        <h1 className="font-mono font-extrabold text-gray-200">My Agents</h1>
-        <button
-          className="hover:background-color-2 rounded-md border border-transparent text-white transition-all hover:border-white/20"
-          onClick={() => setShow(!show)}
-        >
-          <FaBars size="15" className="z-20 m-2" />
-        </button>
-      </div>
-      <FadingHr className="my-2" />
-      <div className="-mr-2 mb-2 flex-1 overflow-y-auto">
-        {status === "unauthenticated" && (
-          <div className="p-1 font-mono text-sm text-white">
-            <a className="link" onClick={() => void signIn()}>
-              {t("SIGN_IN")}
-            </a>{" "}
-            {t("SIGN_IN_NOTICE")}
-          </div>
-        )}
-        {status === "authenticated" && !isLoading && userAgents.length === 0 && (
-          <div className="p-1 font-mono text-sm text-white">
-            {t("NEED_TO_SIGN_IN_AND_CREATE_AGENT_FIRST")}
-          </div>
-        )}
-        {userAgents.map((agent, index) => (
-          <DrawerItemButton
-            key={`${index}-${agent.name}`}
-            className="flex w-full rounded-md p-2 font-mono text-sm font-semibold"
-            text={agent.name}
-            onClick={() => void router.push(`/agent?id=${agent.id}`)}
-          />
-        ))}
-      </div>
-      <ul role="list" className="flex flex-col">
-        <ul className="mb-2">
-          <div className="mb-2 ml-2 text-xs font-semibold text-neutral-400">Pages</div>
-          {PAGE_LINKS.map((link) => {
-            if (router.route == link.href) {
-              return null;
-            }
+    <SidebarTransition show={show}>
+      <nav className="flex flex-1 flex-col bg-neutral-900 px-4 py-3 ring-1 ring-white/10">
+        <div className="flex flex-row items-center justify-between">
+          <Image src="logo-white.svg" width="25" height="25" alt="Reworkd AI" className="ml-2" />
+          <h1 className="font-mono font-extrabold text-gray-200">My Agents</h1>
+          <button
+            className="hover:background-color-2 rounded-md border border-transparent text-white transition-all hover:border-white/20"
+            onClick={() => setShow(!show)}
+          >
+            <FaBars size="15" className="z-20 m-2" />
+          </button>
+        </div>
+        <FadingHr className="my-2" />
+        <div className="-mr-2 mb-2 flex-1 overflow-y-auto">
+          {status === "unauthenticated" && (
+            <div className="p-1 font-mono text-sm text-white">
+              <a className="link" onClick={() => void signIn()}>
+                {t("SIGN_IN")}
+              </a>{" "}
+              {t("SIGN_IN_NOTICE")}
+            </div>
+          )}
+          {status === "authenticated" && !isLoading && userAgents.length === 0 && (
+            <div className="p-1 font-mono text-sm text-white">
+              {t("NEED_TO_SIGN_IN_AND_CREATE_AGENT_FIRST")}
+            </div>
+          )}
+          {userAgents.map((agent, index) => (
+            <DrawerItemButton
+              key={`${index}-${agent.name}`}
+              className="flex w-full rounded-md p-2 font-mono text-sm font-semibold"
+              text={agent.name}
+              onClick={() => void router.push(`/agent?id=${agent.id}`)}
+            />
+          ))}
+        </div>
+        <ul role="list" className="flex flex-col">
+          <ul className="mb-2">
+            <div className="mb-2 ml-2 text-xs font-semibold text-neutral-400">Pages</div>
+            {PAGE_LINKS.map((link) => {
+              if (router.route == link.href) {
+                return null;
+              }
 
-            return (
-              <LinkItem
-                key={link.name}
-                title={link.name}
-                icon={link.icon}
-                href={link.href}
-                badge={link.badge}
-                onClick={() => {
-                  void router.push(link.href);
-                }}
-              />
-            );
-          })}
+              return (
+                <LinkItem
+                  key={link.name}
+                  title={link.name}
+                  icon={link.icon}
+                  href={link.href}
+                  badge={link.badge}
+                  onClick={() => {
+                    void router.push(link.href);
+                  }}
+                />
+              );
+            })}
+          </ul>
+          <li className="mb-2">
+            <div className="flex items-center justify-center gap-3">
+              {SOCIAL_LINKS.map((link) => (
+                <LinkIconItem
+                  key={link.name}
+                  icon={link.icon}
+                  href={link.href}
+                  onClick={() => {
+                    void router.push(link.href);
+                  }}
+                />
+              ))}
+            </div>
+          </li>
+          <li>
+            <FadingHr />
+            <AuthItem session={session} signOut={signOut} signIn={signIn} />
+          </li>
         </ul>
-        <li className="mb-2">
-          <div className="flex items-center justify-center gap-3">
-            {SOCIAL_LINKS.map((link) => (
-              <LinkIconItem
-                key={link.name}
-                icon={link.icon}
-                href={link.href}
-                onClick={() => {
-                  void router.push(link.href);
-                }}
-              />
-            ))}
-          </div>
-        </li>
-        <li>
-          <FadingHr />
-          <AuthItem session={session} signOut={signOut} signIn={signIn} />
-        </li>
-      </ul>
-    </nav>
-    // </SidebarTransition>
+      </nav>
+    </SidebarTransition>
   );
 };
 
@@ -146,6 +146,17 @@ const SidebarTransition = ({ children, show }: SidebarTransitionProps) => {
         </div>
       </div>
     </Transition.Root>
+  );
+};
+
+export const SidebarControlButton = ({ show, setShow }: SidebarProps) => {
+  return (
+    <button
+      className="background-color-1 hover:background-color-2 fixed z-20 m-1 rounded-md border border-white/20 text-white transition-all"
+      onClick={() => setShow(!show)}
+    >
+      <FaBars size="15" className="m-2" />
+    </button>
   );
 };
 export default Sidebar;
--- next/src/layout/dashboard.tsx
+++ next/src/layout/dashboard.tsx
@@ -1,11 +1,10 @@
 import type { PropsWithChildren } from "react";
 import { useState } from "react";
-import { FaBars } from "react-icons/fa";
 import clsx from "clsx";
 import DottedGridBackground from "../components/DottedGridBackground";
 import AppHead from "../components/AppHead";
 import { useTheme } from "../hooks/useTheme";
-import Sidebar from "../components/drawer/Sidebar";
+import Sidebar, { SidebarControlButton } from "../components/drawer/Sidebar";
 
 const DashboardLayout = (props: PropsWithChildren) => {
   const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
@@ -19,32 +18,19 @@ const DashboardLayout = (props: PropsWithChildren) => {
       <AppHead />
 
       {/*Mobile sidebar*/}
-      <div className="flex h-screen max-h-screen w-64 max-w-xs flex-1 lg:hidden">
-        <Sidebar show={mobileSidebarOpen} setShow={setMobileSidebarOpen} />
+      <Sidebar show={mobileSidebarOpen} setShow={setMobileSidebarOpen} />
+      <div className={mobileSidebarOpen ? "hidden" : "lg:hidden"}>
+        <SidebarControlButton show={mobileSidebarOpen} setShow={setMobileSidebarOpen} />
       </div>
-      <button
-        className={clsx(
-          mobileSidebarOpen ? "hidden" : "lg:hidden",
-          "background-color-1 hover:background-color-2 fixed z-20 m-2 rounded-md border border-white/20 text-white transition-all"
-        )}
-        onClick={() => setMobileSidebarOpen(true)}
-      >
-        <FaBars size="15" className="z-20 m-2" />
-      </button>
 
       {/* Desktop sidebar */}
       <div className={clsx("hidden lg:fixed lg:inset-y-0  lg:flex lg:w-64 lg:flex-col")}>
         <Sidebar show={desktopSidebarOpen} setShow={setDesktopSidebarOpen} />
       </div>
-      <button
-        className={clsx(
-          desktopSidebarOpen ? "hidden" : "hidden lg:block",
-          "background-color-1 hover:background-color-2 fixed z-20 m-2 rounded-md border border-white/20 text-white transition-all"
-        )}
-        onClick={() => setDesktopSidebarOpen(!desktopSidebarOpen)}
-      >
-        <FaBars size="15" className="z-20 m-2" />
-      </button>
+      <div className={desktopSidebarOpen ? "hidden" : "hidden lg:block"}>
+        <SidebarControlButton show={desktopSidebarOpen} setShow={setDesktopSidebarOpen} />
+      </div>
+
       <main
         className={clsx(
           "bg-gradient-to-b from-[#2B2B2B] to-[#1F1F1F] duration-300",

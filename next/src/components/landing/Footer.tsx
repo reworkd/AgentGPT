ffbda43e4a17978150389591aeb6b590fd7dc2be
awtkns <32209255+awtkns@users.noreply.github.com>
2023-07-02 21:01:59 -0700

    ♻️ Fixup links

--- next/src/components/landing/Footer.tsx
+++ next/src/components/landing/Footer.tsx
@@ -11,17 +11,24 @@ const Footer = (props: Props) => {
   return (
     <footer className={clsx(props.className, "relative w-full text-sm font-extralight")}>
       <div className="ml-auto mr-auto w-full max-w-7xl px-8 pb-16 text-center">
-        <div className="flex w-full flex-col items-baseline justify-between gap-16 sm:flex-row sm:justify-start sm:gap-0">
+        <div className="flex w-full flex-col items-center justify-between gap-16 sm:flex-row sm:items-baseline sm:justify-start sm:gap-0">
           <div className="flex">
             <Image src="wordmark.svg" width="200" height="20" alt="Reworkd AI" className="mr-2" />
           </div>
           <div className="flex flex-1 justify-center">
             <div className="flex flex-col gap-6 px-8 text-center text-gray-400 sm:flex-row sm:text-left md:tracking-wider">
+              <a
+                className="text-bold text-center"
+                href="https://www.ycombinator.com/companies/reworkd/jobs"
+                target="_blank"
+              >
+                Careers
+              </a>
               <a className="text-center" href="https://reworkd.ai/privacyPolicy" target="_blank">
                 Privacy Policy
               </a>
               <a className="text-center" href="https://reworkd.ai/terms" target="_blank">
-                Terms and Conditions
+                Terms & Conditions
               </a>
             </div>
           </div>
@@ -35,7 +42,7 @@ const Footer = (props: Props) => {
                 className="transition-all hover:rotate-6 hover:text-purple-400"
               >
                 <span className="sr-only">{link.name}</span>
-                <link.icon size={48} className="sm:hidden" />
+                <link.icon size={32} className="sm:hidden" />
                 <link.icon size={24} className="hidden sm:flex" />
               </a>
             ))}

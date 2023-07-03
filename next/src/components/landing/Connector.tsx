f5567a4eea16783b296acff63436987367bfb1b2
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-02 19:49:53 -0700

    Gradient ajustments

--- next/src/components/landing/Connector.tsx
+++ next/src/components/landing/Connector.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Connector = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-slate-950 from-5% via-transparent via-50% to-transparent "></div>
+    <div className="flex h-screen w-screen items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-slate-900 from-10% via-transparent via-70% to-transparent "></div>
   );
 };
 

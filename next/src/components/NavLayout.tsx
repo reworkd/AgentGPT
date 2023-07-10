7b0c29fc002bc02aa505c81dc47b08eedff6e57f
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-10 15:57:22 -0700

    ✨ Remove non-hero sections

--- next/src/components/NavLayout.tsx
+++ next/src/components/NavLayout.tsx
@@ -4,7 +4,6 @@ import { FaBars, FaQuestion } from "react-icons/fa";
 import FadeIn from "./motions/FadeIn";
 import clsx from "clsx";
 import Image from "next/image";
-import Footer from "./landing/Footer";
 
 const navigation = [
   { name: "Home", href: "#" },
@@ -89,7 +88,6 @@ export default function NavLayout({ children }: { children: ReactNode }) {
       </FadeIn>
 
       <main className="min-w-screen min-h-screen">{children}</main>
-      <Footer className="mt-32" />
     </>
   );
 }
--- next/src/pages/landing-page.tsx
+++ next/src/pages/landing-page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import NavLayout from "../components/NavLayout";
 import Hero from "../components/landing/Hero";
-import Sections from "../components/landing/Section";
 
 const HomePage = () => {
   return (
@@ -11,12 +10,6 @@ const HomePage = () => {
           <div className="flex h-screen w-full flex-col items-start justify-center overflow-x-hidden px-4 text-white lg:pl-16">
             <Hero />
           </div>
-          <div id="section" className="flex min-h-screen w-full items-center">
-            <div className="flex w-full grid-cols-4 grid-rows-2 flex-col items-center justify-center gap-4 sm:grid">
-              <Sections />
-            </div>
-          </div>
-          <div className="relative flex w-full items-center"></div>
         </div>
       </div>
     </NavLayout>

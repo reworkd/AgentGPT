bcbacd0088883c1764819c2dfe45fa6d3bab608c
Joe Shen <joeshen123@gmail.com>
2023-07-09 17:30:29 -0700

    implement contact us and explore AI Agents buttons

--- next/src/components/PrimaryButton.tsx
+++ next/src/components/PrimaryButton.tsx
@@ -1,24 +1,24 @@
 import clsx from "clsx";
+import Button from "../ui/button";
+import React from "react";
 
 type PrimaryButtonProps = {
-  children: string;
+  children: JSX.Element;
+  icon?: React.ReactNode;
   onClick?: () => void;
-  outline?: boolean;
 };
-export default function PrimaryButton({ children, onClick, outline }: PrimaryButtonProps) {
+export default function PrimaryButton({ children, onClick, icon }: PrimaryButtonProps) {
   return (
-    <button
-      type="button"
-      className={clsx(
-        "text-md rounded-lg px-4 py-2.5  shadow-sm ",
-        "transition-colors duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-        outline
-          ? "text-[#BF7DFF]/90 ring-1 ring-[#BF7DFF] hover:bg-[#BF7DFF]/20"
-          : "bg-[#BF7DFF] text-white hover:border-blue-200 hover:bg-[#9124ff] hover:text-zinc-200 "
-      )}
+    <Button
       onClick={onClick}
+      className={clsx(
+        "rounded-full border-[1px] border-black shadow-sm",
+        "transition duration-200 ease-in-out hover:hover:bg-white/90 focus-visible:bg-white/90 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-white/30",
+        "bg-white text-black"
+      )}
     >
+      {icon}
       {children}
-    </button>
+    </Button>
   );
 }

5c2db4cd63298c34b1e8ef4d8eb4a851107c7912
Joe Shen <joeshen123@gmail.com>
2023-07-10 15:42:08 -0700

    ⭐️ feat(#1007): implement backing component (#1010)

--- next/src/components/landing/Backing.tsx
+++ next/src/components/landing/Backing.tsx
@@ -1,27 +1,25 @@
+import YCLogo from "../../../public/logos/yc-default-solid.svg";
+import PanacheLogo from "../../../public/logos/panache-default-solid.svg";
+
 const Backing = () => {
   return (
-    <div className="font-white relative flex flex-col items-center text-xs md:text-sm">
-      <div className="flex flex-row items-center gap-1 font-thin text-gray-400">
-        <div className="mr-1 font-thin tracking-wide">Backed By:</div>
-      </div>
-      <div className="flex flex-row items-center gap-1 font-thin tracking-wide text-gray-400">
+    <div className="relative flex flex-col items-center font-inter text-xs font-normal leading-6 text-white/50 md:text-sm">
+      <div className="flex flex-row items-center gap-x-1">
+        <div className="flex flex-row items-center">
+          <YCLogo className="z-10 mr-[-4px]" />
+          <PanacheLogo />
+        </div>
+        <div className="tracking-wide">Backed By</div>
         <a
-          className="flex cursor-pointer flex-row items-center gap-1 transition-transform duration-300 hover:-translate-x-0.5 hover:scale-105"
+          className="flex cursor-pointer flex-row items-center gap-1 font-medium text-white/95 transition-transform duration-300 hover:scale-105"
           href="https://www.ycombinator.com/companies/reworkd"
           target="_blank"
         >
-          <div className="relative">
-            <div className="absolute inset-0 rounded-sm bg-gradient-to-bl from-orange-50 via-orange-500 to-[#f26522]"></div>
-            <div className="absolute inset-[2px] rounded-sm bg-gradient-to-t from-[#f26522] to-[#ffa437] ring-1 ring-[#f26522]/50"></div>
-            <div className="relative flex items-center justify-center px-2 font-sans text-lg font-medium text-white">
-              Y
-            </div>
-          </div>
-          <div className="ml-1">Combinator</div>
+          Y Combinator
         </a>
-        <div className="mx-1 font-medium">&</div>
+        <span>and</span>
         <a
-          className="cursor-pointer transition-transform duration-300 hover:translate-x-0.5 hover:scale-105"
+          className="cursor-pointer font-medium text-white/95 transition-transform duration-300 hover:scale-105"
           href="https://www.panache.vc/"
           target="_blank"
         >

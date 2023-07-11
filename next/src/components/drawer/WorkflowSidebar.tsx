4fa74286933203f9bebc912ddc52783f2c2589ad
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-10 19:23:57 -0700

    ✨ Update definition

--- next/src/components/drawer/WorkflowSidebar.tsx
+++ next/src/components/drawer/WorkflowSidebar.tsx
@@ -2,6 +2,7 @@ import type { DisplayProps } from "./Sidebar";
 import Sidebar from "./Sidebar";
 import React from "react";
 import { FaBars } from "react-icons/fa";
+import type { NodeBlockDefinition } from "../../services/workflow/node-block-definitions";
 import { getNodeBlockDefinitions } from "../../services/workflow/node-block-definitions";
 
 const WorkflowSidebar = ({ show, setShow }: DisplayProps) => {
@@ -15,15 +16,28 @@ const WorkflowSidebar = ({ show, setShow }: DisplayProps) => {
           >
             <FaBars size="15" className="z-20 m-2" />
           </button>
-          <div className="ml-5 font-bold">Nodes</div>
+          <div className="ml-5 font-bold">Block</div>
         </div>
         {getNodeBlockDefinitions().map((nodeBlockDefinition) => (
-          <div key={nodeBlockDefinition.type} className="ml-5 font-bold">
-            {nodeBlockDefinition.type}
-          </div>
+          <NodeBlock key={nodeBlockDefinition.type} definition={nodeBlockDefinition} />
         ))}
       </div>
     </Sidebar>
+  );
+};
+
+type NodeBlockProps = {
+  definition: NodeBlockDefinition;
+};
+const NodeBlock = ({ definition }: NodeBlockProps) => {
+  return (
+    <div className="flex flex-row gap-2 rounded-md border border-white/20 p-2">
+      <img src={definition.image_url} alt={definition.type} width={30} height={30} />
+      <div className="flex-shrink">
+        <h3 className="font-medium">{definition.type}</h3>
+        <p className="text-sm font-thin">{definition.description}</p>
+      </div>
+    </div>
   );
 };
 
--- next/src/services/workflow/node-block-definitions.ts
+++ next/src/services/workflow/node-block-definitions.ts
@@ -1,13 +1,13 @@
-interface NodeBlockDefinitions {
+export interface NodeBlockDefinition {
   type: string;
   description: string;
   image_url: string;
 }
 
-const UrlStatusCheckBlockDefinition: NodeBlockDefinitions = {
+const UrlStatusCheckBlockDefinition: NodeBlockDefinition = {
   type: "UrlStatusCheck",
   description: "Check the status of a URL",
-  image_url: "",
+  image_url: "/tools/web.png",
 };
 
 export const getNodeBlockDefinitions = () => {

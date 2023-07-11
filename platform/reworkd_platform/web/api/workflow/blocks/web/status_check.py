8396efdbc7ccc080757439837ee2318d99d30758
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-10 18:42:52 -0700

    ✨ Fix URL check types

--- platform/reworkd_platform/web/api/workflow/blocks/web/status_check.py
+++ platform/reworkd_platform/web/api/workflow/blocks/web/status_check.py
@@ -3,35 +3,32 @@ from typing import Optional
 import requests
 from requests import RequestException
 
-from reworkd_platform.web.api.workflow.nodes.node import (
-    WorkflowNode,
-    WorkflowNodeIOBase,
-)
+from reworkd_platform.web.api.workflow.schemas import Block, BlockIOBase
 
 
-class UrlStatusCheckNodeInput(WorkflowNodeIOBase):
+class UrlStatusCheckBlockInput(BlockIOBase):
     url: str
 
 
-class UrlStatusCheckNodeOutput(WorkflowNodeIOBase):
+class UrlStatusCheckBlockOutput(BlockIOBase):
     code: Optional[int]
 
 
-class UrlStatusCheckNode(WorkflowNode):
+class UrlStatusCheckBlock(Block):
     type = "UrlStatusCheckNode"
     description = "Outputs the status code of a GET request to a URL"
     image_url = ""
-    input_config: UrlStatusCheckNodeInput
+    input_config: UrlStatusCheckBlockInput
 
-    def __init__(self, node_id: str, input_config: UrlStatusCheckNodeInput):
+    def __init__(self, node_id: str, input_config: UrlStatusCheckBlockInput):
         super().__init__(node_id, input_config)
 
-    def run(self) -> WorkflowNodeIOBase:
+    def run(self) -> BlockIOBase:
         try:
             response = requests.get(self.input_config.url)
             code = response.status_code
         except RequestException:
             code = None
 
-        output = UrlStatusCheckNodeOutput(code=code)
+        output = UrlStatusCheckBlockOutput(code=code)
         return output

e182af8a59fd25059f34a912aa0f19df8fea1bad
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-11 17:48:44 -0700

    ✨ Rm clsx and changed to typescript

--- next/src/components/landing/FooterLinks.tsx
+++ next/src/components/landing/FooterLinks.tsx
@@ -1,24 +1,26 @@
-import clsx from "clsx";
+interface FooterLinkProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}
 
-const FooterLink = ({ href, children }) => (
+const FooterLink: React.FC<FooterLinkProps> = ({ href, children, className }) => (
   <a
     href={href}
-    className={clsx(
-      "w-full rounded-full bg-transparent text-white/20 ",
-      "transition duration-200 ease-in-out hover:text-white/90",
-      " focus:outline-none focus:ring-2 focus:ring-white/60 focus:ring-offset-2 focus:ring-offset-transparent"
-    )}
+    className={`w-full rounded-full bg-transparent text-white/50 transition duration-200 
+    ease-in-out hover:text-white/90 focus:outline-none focus:ring-2 focus:ring-white/60 
+    focus:ring-offset-2 focus:ring-offset-transparent ${className}`}
   >
     {children}
   </a>
 );
 
-const FooterLinks = () => {
+const FooterLinks: React.FC = () => {
   return (
     <div className="flex cursor-pointer justify-between space-x-8">
       <FooterLink href="https://www.ycombinator.com/companies/reworkd/jobs">Careers</FooterLink>
       <div className="group flex items-center space-x-2">
-        <div className="pr-1 text-white/20">Status</div>
+        <div className="pr-1 text-white/50">Status</div>
         <div className="relative flex h-2 w-2 items-center justify-center rounded-full bg-green-500">
           <div className="absolute h-4 w-4 rounded-full bg-green-500 opacity-50 transition-opacity duration-200"></div>
         </div>

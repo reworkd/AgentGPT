266c5d52f570270ec40bc7f60c43a17b09b033e4
awtkns <32209255+awtkns@users.noreply.github.com>
2023-07-12 00:33:58 -0700

    🎨 Landing page updates

--- next/src/components/landing/Backing.tsx
+++ next/src/components/landing/Backing.tsx
@@ -3,10 +3,10 @@ import PanacheLogo from "../../../public/logos/panache-default-solid.svg";
 
 const Backing = () => {
   return (
-    <div className="relative flex flex-col items-center font-inter text-xs font-normal leading-6 text-white/50 md:text-sm">
+    <div className="relative flex flex-col items-center font-inter text-xs font-normal text-white/50 md:text-sm">
       <div className="flex flex-row items-center gap-x-1">
-        <div className="flex flex-row items-center">
-          <YCLogo className="z-10 mr-[-4px]" />
+        <div className="ml-2 mr-1 flex flex-row items-center">
+          <YCLogo className="z-10 -mr-2" />
           <PanacheLogo />
         </div>
         <div className="tracking-wide">Backed By</div>
--- next/src/pages/landing-page.tsx
+++ next/src/pages/landing-page.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <NavLayout>
       <div className="flex w-full justify-center">
-        <div className="flex max-w-screen-xl flex-col items-center justify-center overflow-x-hidden px-5 text-white">
+        <div className="flex max-w-screen-2xl flex-col items-center justify-center overflow-x-hidden px-5 text-white">
           <div className="flex h-screen w-full flex-col items-start justify-center overflow-x-hidden px-4 text-white lg:pl-16">
             <Hero />
           </div>

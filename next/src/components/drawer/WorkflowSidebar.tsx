e69ca280b3fbe7316c37eb1aa671481bf0518262
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-12 10:16:42 -0700

    ✨ Fix sidebar

--- next/src/components/drawer/WorkflowSidebar.tsx
+++ next/src/components/drawer/WorkflowSidebar.tsx
@@ -24,7 +24,7 @@ export const getWorkflowSidebar = (createNode: createNodeType) => {
 const WorkflowSidebar = ({ show, setShow, createNode }: WorkflowSidebarProps) => {
   return (
     <Sidebar show={show} setShow={setShow} side="right">
-      <div className="flex h-screen flex-col gap-2 text-white">
+      <div className="text-color-primary flex h-screen flex-col gap-2">
         <div className="flex flex-row items-center gap-1">
           <button
             className="neutral-button-primary rounded-md border-none transition-all"
@@ -56,8 +56,10 @@ const NodeBlock = ({ definition, createNode }: NodeBlockProps) => {
       className="flex cursor-pointer flex-row gap-2 rounded-md border border-white/20 p-2 hover:bg-white/10"
       onClick={() => createNode(definition)}
     >
-      <img src={definition.image_url} alt={definition.type} width={30} height={30} />
-      <div className="flex-shrink">
+      <div className="h-[30px] w-[30px]">
+        <img src={definition.image_url} alt={definition.type} width={30} />
+      </div>
+      <div>
         <h3 className="font-medium">{definition.type}</h3>
         <p className="text-sm font-thin">{definition.description}</p>
       </div>

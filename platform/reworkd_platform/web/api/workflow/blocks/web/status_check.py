47c884da74e342bd8e70697bd99432f7d950d33b
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-13 11:16:11 -0700

    ✨ Logger

--- platform/reworkd_platform/web/api/workflow/blocks/web/status_check.py
+++ platform/reworkd_platform/web/api/workflow/blocks/web/status_check.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 import requests
+from loguru import logger
 from requests import RequestException
 
 from reworkd_platform.web.api.workflow.schemas import Block, BlockIOBase
@@ -21,11 +22,13 @@ class UrlStatusCheckBlock(Block):
     input: UrlStatusCheckBlockInput
 
     async def run(self) -> BlockIOBase:
+        logger.info("Starting UrlStatusCheckBlock")
         try:
-            response = requests.get(self.input.url)
+            response = requests.get("https://twitter.com/")
             code = response.status_code
         except RequestException:
             code = None
 
+        logger.info("UrlStatusCheckBlock Code", code)
         output = UrlStatusCheckBlockOutput(code=code)
         return output

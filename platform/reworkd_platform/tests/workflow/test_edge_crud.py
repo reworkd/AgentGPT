7b9622bbb8999851dbfcde08cce67216e294e604
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-12 18:19:52 -0700

    ✨ Edge CRUD

---
+++ platform/reworkd_platform/tests/workflow/test_edge_crud.py
@@ -0,0 +1,40 @@
+import pytest
+from pytest_mock import MockFixture
+
+from reworkd_platform.db.crud.edge import EdgeCRUD
+from reworkd_platform.web.api.workflow.schemas import EdgeUpsert
+
+
+class TestEdgeCRUD:
+    def test_add_edge_to_empty_map(self, mocker: MockFixture):
+        edge_crud = EdgeCRUD(mocker.Mock())
+        assert edge_crud.add_edge("source", "target")
+        assert edge_crud.source_target_map == {"source": {"target"}}
+
+    def test_add_edge_to_non_empty_map(self, mocker: MockFixture):
+        edge_crud = EdgeCRUD(mocker.Mock())
+        edge_crud.source_target_map = {"source": {"target1"}}
+
+        assert edge_crud.add_edge("source", "target2")
+        assert edge_crud.source_target_map == {"source": {"target1", "target2"}}
+
+    @pytest.mark.asyncio
+    async def test_delete_old_edges_not_present_in_edges_to_keep(self, mocker):
+        edge_to_keep = EdgeUpsert(id="0", source="source", target="target")
+        mock_session = mocker.Mock()
+        mock_edge = mocker.Mock(id="1")
+        mock_all_edges = {"1": mock_edge, "0": edge_to_keep}
+        mocker.patch.object(mock_edge, "delete", mocker.AsyncMock())
+
+        mock_edges_to_keep = [edge_to_keep]
+        await EdgeCRUD(mock_session).delete_old_edges(
+            mock_edges_to_keep, mock_all_edges
+        )
+
+        mock_edge.delete.assert_called_once_with(mock_session)
+
+    def test_add_edge_with_existing_source_and_target(self, mocker: MockFixture):
+        edge_crud = EdgeCRUD(mocker.Mock())
+        edge_crud.source_target_map = {"source": {"target"}}
+        assert not edge_crud.add_edge("source", "target")
+        assert edge_crud.source_target_map == {"source": {"target"}}

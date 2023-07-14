4005f3abb45606065d2fda2cc3e06bf5afeffb15
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-13 18:53:47 -0700

    ✨ Fix slack web hook input

--- platform/reworkd_platform/schemas/workflow/blocks/slack_webhook.py
+++ platform/reworkd_platform/schemas/workflow/blocks/slack_webhook.py
@@ -4,14 +4,26 @@ from loguru import logger
 from reworkd_platform.schemas.workflow.base import Block, BlockIOBase
 
 
+class SlackWebhookInput(BlockIOBase):
+    url: str
+    message: str
+
+
 class SlackWebhook(Block):
     type = "SlackWebhook"
     description = "Sends a message to a slack webhook"
-    # TODO: Make URL and message a dynamic field
-    url = "https://hooks.slack.com/workflows/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"
-    message = "Hello World!"
+    input: SlackWebhookInput
 
     async def run(self) -> BlockIOBase:
         logger.info(f"Starting {self.type}")
-        requests.post(self.url, json={"message": "Mr. S is not the best looking "})
+
+        try:
+            response = requests.post(
+                self.input.url, json={"message": self.input.message}
+            )
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            logger.error(f"Failed to send message to webhook: {err}")
+            raise
+
         return BlockIOBase()

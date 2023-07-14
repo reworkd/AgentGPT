920f27021d9469c49b62f19d13c3a8fc9b15d20d
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-13 20:09:52 -0700

    🌟 Button Glow

--- next/src/components/PrimaryButton.tsx
+++ next/src/components/PrimaryButton.tsx
@@ -10,16 +10,19 @@ type PrimaryButtonProps = {
 };
 export default function PrimaryButton({ children, onClick, icon }: PrimaryButtonProps) {
   return (
-    <Button
-      onClick={onClick}
-      className={clsx(
-        "rounded-full border-[1px] border-black shadow-sm",
-        "transition duration-200 ease-in-out hover:hover:bg-white/90 focus-visible:bg-white/90 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-white/30",
-        "bg-white text-black"
-      )}
-    >
-      {icon}
-      {children}
-    </Button>
+    <div className="group relative inline-flex items-center justify-center">
+      <div className="absolute -inset-1 rounded-full bg-gradient-to-r from-purple-700 to-purple-700 opacity-70 blur-lg transition-all duration-1000 group-hover:-inset-px group-hover:opacity-100 group-hover:duration-200" />
+      <Button
+        onClick={onClick}
+        className={clsx(
+          "relative z-10 rounded-full border-[1px] border-black",
+          "transition duration-200 ease-in-out hover:hover:bg-white/90 focus-visible:bg-white/90 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-white/30",
+          "bg-white text-black"
+        )}
+      >
+        {icon}
+        {children}
+      </Button>
+    </div>
   );
 }

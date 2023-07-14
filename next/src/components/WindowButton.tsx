3d9f2cf22acc39eda1fa20c37685bf0384e9b776
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-13 23:14:53 -0700

    ✨ Fix WindowButton hover

--- next/src/components/WindowButton.tsx
+++ next/src/components/WindowButton.tsx
@@ -14,7 +14,7 @@ const WindowButton = ({ ping, onClick, icon, name, border }: WindowButtonProps) 
   return (
     <div
       className={clsx(
-        "neutral-button-primary relative flex h-8 cursor-pointer items-center gap-2 rounded-lg p-2 font-mono text-sm font-bold transition-all hover:bg-white/10",
+        "background-color-2 text-color-primary hover:background-color-5 relative flex h-8 cursor-pointer items-center gap-2 rounded-lg p-2 font-mono text-sm font-bold transition-all",
         !border && "rounded-none border-none"
       )}
       onClick={onClick}

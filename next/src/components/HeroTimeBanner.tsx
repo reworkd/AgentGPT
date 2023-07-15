907864f9394467848b8680868a8d35bbfa8c383e
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-14 22:40:35 -0700

    Setup

--- next/src/components/HeroTimeBanner.tsx
+++ next/src/components/HeroTimeBanner.tsx
@@ -19,26 +19,32 @@ const HeroTimeBanner: React.FC<HeroTimeBannerProps> = ({
   onClick,
 }) => {
   return (
-    <div
-      className={clsx(
-        "flex flex-row items-center justify-center gap-x-4",
-        "h-fit w-fit py-2.5 pl-3 pr-4",
-        "rounded-[1000px] border-2 border-transparent border-opacity-20 shadow-md",
-        "animate-border-pulse bg-clip-text text-transparent",
-        "bg-gradient-to-r from-white to-transparent",
-        "cursor-pointer"
-      )}
-    >
-      <div>{leftIcon}</div>
-      <div className="flex flex-col font-inter leading-6  tracking-normal">
-        <h2 className="ml-2s text-[12px] font-semibold md:text-[15px]">{title}</h2>
-        <p className="text-[11px] font-medium md:text-[14px]">{subtitle}</p>
-      </div>
-      <GlowWrapper>
-        <div className="relative z-10 flex h-8 w-8 items-center justify-center rounded-[1000px] bg-white">
-          {rightIcon}
+    <div className="relative">
+      <div
+        className="absolute inset-0 -z-10"
+        style={{ backgroundImage: "conic-gradient(purple, transparent)" }}
+      ></div>
+      <div
+        className={clsx(
+          "z-10",
+          "flex flex-row items-center justify-center gap-x-4",
+          "h-fit w-fit py-2.5 pl-3 pr-4",
+          "rounded-[1000px] border-2 border-transparent border-opacity-20 shadow-md",
+          "animate-border-pulse bg-black",
+          "cursor-pointer"
+        )}
+      >
+        <div>{leftIcon}</div>
+        <div className="flex flex-col bg-gradient-to-r from-white to-transparent bg-clip-text font-inter leading-6 tracking-normal text-transparent">
+          <h2 className="ml-2s text-[12px] font-semibold md:text-[15px]">{title}</h2>
+          <p className="text-[11px] font-medium md:text-[14px]">{subtitle}</p>
         </div>
-      </GlowWrapper>
+        <GlowWrapper>
+          <div className="relative z-10 flex h-8 w-8 items-center justify-center rounded-[1000px] bg-white">
+            {rightIcon}
+          </div>
+        </GlowWrapper>
+      </div>
     </div>
   );
 };

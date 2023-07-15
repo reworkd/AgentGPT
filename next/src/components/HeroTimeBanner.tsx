d5e04138b3bee55b320050b1a6f74747cc05abad
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-15 16:14:21 -0700

    🌟 Formatting

--- next/src/components/HeroTimeBanner.tsx
+++ next/src/components/HeroTimeBanner.tsx
@@ -34,33 +34,31 @@ const HeroTimeBanner: React.FC<HeroTimeBannerProps> = ({
   };
 
   return (
-    <div className="relative flex">
-      <div className="pointer-events-none absolute inset-0 z-[-10] max-w-[330px] scale-105 transform rounded-full bg-purple-600 opacity-60"></div>
-      <motion.div
-        className="pointer-events-none absolute inset-0 z-[-5] max-w-[330px] scale-105 transform rounded-full bg-black"
-        style={{ maskImage: "linear-gradient(to right, transparent, black)" }}
-        variants={rotationVariants}
-        initial="initial"
-        animate="animate"
-      ></motion.div>
+    <div className="relative flex content-start items-center">
       <div
         className={clsx(
           "relative z-10",
-          "flex flex-row items-center justify-center gap-x-4",
-          "h-fit w-fit py-2.5 pl-3 pr-4",
-          "rounded-[1000px] border-2 border-transparent border-opacity-20 shadow-md",
+          "flex flex-row items-center justify-center gap-x-2",
+          "overflow-hidden py-2 pl-3 pr-3",
+          "rounded-[1000px] border-2 border-transparent shadow-md",
           "bg-black",
           "cursor-pointer"
         )}
       >
+        <motion.div
+          className="absolute -z-10 h-60 w-60 rounded-full"
+          style={{ background: "conic-gradient(purple, transparent, transparent, purple)" }}
+          variants={rotationVariants}
+          initial="initial"
+          animate="animate"
+        />
         <div>{leftIcon}</div>
         <div className="flex flex-col bg-gradient-to-r from-white via-white to-black bg-clip-text font-inter leading-6 tracking-normal text-transparent">
-          <h2 className="ml-2s text-[12px] font-semibold md:text-[15px]">{title}</h2>
-          <p className="text-[11px] font-medium md:text-[14px]">{subtitle}</p>
+          <h2 className="ml-2s text-[10px] font-semibold md:text-[12px]">{title}</h2>
+          <p className="text-[8px] font-medium md:text-[10px]">{subtitle}</p>
         </div>
-
         <GlowWrapper>
-          <div className="relative z-10 flex h-8 w-8 items-center justify-center rounded-[1000px] bg-white">
+          <div className="relative z-20 flex h-6 w-6 items-center justify-center rounded-[1000px] bg-white">
             {rightIcon}
           </div>
         </GlowWrapper>

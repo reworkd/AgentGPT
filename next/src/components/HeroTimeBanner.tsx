921fff5f3eff47d75493ed767988d50d2ecda7d5
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-15 17:34:02 -0700

    🚄 SteamPower

--- next/src/components/HeroTimeBanner.tsx
+++ next/src/components/HeroTimeBanner.tsx
@@ -34,24 +34,26 @@ const HeroTimeBanner: React.FC<HeroTimeBannerProps> = ({
   };
 
   return (
-    <div className="relative flex content-start items-center">
+    <div className="relative flex items-center justify-start">
+      <div className="absolute z-0 flex h-full w-[235px] scale-105 transform items-center overflow-hidden rounded-[1000px]">
+        <motion.div
+          className="h-80 w-80 rounded-full"
+          style={{ background: "conic-gradient(transparent, transparent, white)" }}
+          variants={rotationVariants}
+          initial="initial"
+          animate="animate"
+        />
+      </div>
       <div
         className={clsx(
           "relative z-10",
           "flex flex-row items-center justify-center gap-x-2",
-          "overflow-hidden py-2 pl-3 pr-3",
+          "py-2 pl-3 pr-3",
           "rounded-[1000px] border-2 border-transparent shadow-md",
           "bg-black",
           "cursor-pointer"
         )}
       >
-        <motion.div
-          className="absolute -z-10 h-60 w-60 rounded-full"
-          style={{ background: "conic-gradient(purple, transparent, transparent, purple)" }}
-          variants={rotationVariants}
-          initial="initial"
-          animate="animate"
-        />
         <div>{leftIcon}</div>
         <div className="flex flex-col bg-gradient-to-r from-white via-white to-black bg-clip-text font-inter leading-6 tracking-normal text-transparent">
           <h2 className="ml-2s text-[10px] font-semibold md:text-[12px]">{title}</h2>

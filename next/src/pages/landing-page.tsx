93708531353dbe19e1e0755565a9bd38fcdec196
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-16 11:58:56 -0700

    ✨ Fix landing page layout

--- next/src/pages/landing-page.tsx
+++ next/src/pages/landing-page.tsx
@@ -7,19 +7,21 @@ import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div className="min-w-screen flex min-h-screen flex-col justify-between px-4 lg:px-12 lg:py-4">
-      <Image src="/stars.svg" alt="stars" fill className="absolute" />
-      <NavBar />
-      <main>
-        <Hero className="lg:ml-16" />
-      </main>
-      <footer className="flex flex-col items-center gap-4 pb-4 lg:flex-row lg:pb-0">
-        <Backing className="flex-grow" />
-        <FooterLinks />
-        <div className="font-inter text-sm font-normal text-white/50 lg:order-first">
-          &copy; 2023 Reworkd AI, Inc.
-        </div>
-      </footer>
+    <div className="min-w-screen grid min-h-screen place-items-center">
+      <div className="flex min-h-screen max-w-7xl flex-col justify-between">
+        <NavBar />
+        <Image src="/stars.svg" alt="stars" fill className="absolute -z-50" />
+        <main>
+          <Hero className="lg:ml-16" />
+        </main>
+        <footer className="flex flex-col items-center gap-4 pb-4 lg:flex-row">
+          <div className="font-inter text-sm font-normal text-white/50 lg:order-first">
+            &copy; 2023 Reworkd AI, Inc.
+          </div>
+          <Backing className="flex-grow" />
+          <FooterLinks />
+        </footer>
+      </div>
     </div>
   );
 };

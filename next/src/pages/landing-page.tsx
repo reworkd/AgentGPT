34c145e2a81f64a1f8487863d3d05c1b98d38168
awtkns <32209255+awtkns@users.noreply.github.com>
2023-07-16 12:07:54 -0700

    🔗 Add stars

--- next/src/pages/landing-page.tsx
+++ next/src/pages/landing-page.tsx
@@ -11,15 +11,15 @@ const HomePage = () => {
       <div className="flex min-h-screen max-w-7xl flex-col justify-between">
         <NavBar />
         <Image src="/stars.svg" alt="stars" fill className="absolute -z-50" />
-        <main>
-          <Hero className="lg:ml-16" />
+        <main className="mx-auto max-w-[1440px]">
+          <Hero />
         </main>
         <footer className="flex flex-col items-center gap-4 pb-4 lg:flex-row">
+          <Backing className="flex-grow" />
+          <FooterLinks />
           <div className="font-inter text-sm font-normal text-white/50 lg:order-first">
             &copy; 2023 Reworkd AI, Inc.
           </div>
-          <Backing className="flex-grow" />
-          <FooterLinks />
         </footer>
       </div>
     </div>

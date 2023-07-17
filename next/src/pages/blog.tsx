551cc6278c5a01e85860ecd286aaae1f7069ab2c
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-16 20:53:42 -0700

    🌟

--- next/src/pages/blog.tsx
+++ next/src/pages/blog.tsx
@@ -24,7 +24,7 @@ interface Post {
 const posts: Post[] = [
   {
     id: 4,
-    title: "Asim - CEO",
+    title: "Asim S",
     href: "#",
     description:
       "Asim is a visionary leader with expertise in marketing strategies and business development. He specializes in leveraging the power of digital marketing to drive growth and brand recognition. Asim is passionate about staying ahead of the curve in the ever-evolving marketing landscape.",
@@ -35,7 +35,7 @@ const posts: Post[] = [
     category: { title: "Marketing", href: "#" },
     author: {
       name: "Asim Patel",
-      role: "CEO",
+      role: "CEO / Founder",
       href: "#",
       imageUrl:
         "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
@@ -43,7 +43,7 @@ const posts: Post[] = [
   },
   {
     id: 5,
-    title: "Adam - CTO",
+    title: "Adam W",
     href: "#",
     description:
       "Adam is a seasoned technologist with a deep understanding of cutting-edge technologies. He leads the technical team and drives innovation in the development of high-performance web applications. Adam is passionate about leveraging technology to solve complex problems and deliver exceptional user experiences.",
@@ -54,7 +54,7 @@ const posts: Post[] = [
     category: { title: "Tech", href: "#" },
     author: {
       name: "Adam Johnson",
-      role: "CTO",
+      role: "CTO / Co-Founder",
       href: "#",
       imageUrl:
         "https://images.unsplash.com/photo-1557683316-e10201644e2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3600&q=80",
@@ -62,7 +62,7 @@ const posts: Post[] = [
   },
   {
     id: 6,
-    title: "Srijan - COO",
+    title: "Srijan S",
     href: "#",
     description:
       "Srijan is a strategic thinker and operations expert. As the COO, he ensures smooth business operations and efficient execution of projects. Srijan excels at streamlining processes, optimizing resources, and fostering collaboration across teams. His strong leadership skills drive organizational success.",
@@ -73,7 +73,7 @@ const posts: Post[] = [
     category: { title: "Operations", href: "#" },
     author: {
       name: "Srijan Gupta",
-      role: "COO",
+      role: "COO / Co-Founder",
       href: "#",
       imageUrl:
         "https://images.unsplash.com/photo-1581091201117-8c7a6811e9c1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
@@ -81,17 +81,17 @@ const posts: Post[] = [
   },
   {
     id: 7,
-    title: "Jasan - Software Developer",
+    title: "Joe - Software Developer",
     href: "#",
     description:
-      "Jasan is a skilled software developer with a passion for building innovative solutions. He specializes in developing robust and scalable applications using the latest technologies. Jasan loves tackling complex coding challenges and is dedicated to delivering high-quality software products.",
+      "Joe is a skilled software developer with a passion for building innovative solutions. He specializes in developing robust and scalable applications using the latest technologies. Jasan loves tackling complex coding challenges and is dedicated to delivering high-quality software products.",
     imageUrl:
       "https://images.unsplash.com/photo-1494905998402-395d579af36f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     date: "Apr 5, 2023",
     datetime: "2023-04-05",
     category: { title: "Tech", href: "#" },
     author: {
-      name: "Jasan Lee",
+      name: "Joe G",
       role: "Software Developer",
       href: "#",
       imageUrl:
@@ -100,17 +100,17 @@ const posts: Post[] = [
   },
   {
     id: 8,
-    title: "Joe - Software Developer",
+    title: "Jasan - Software Developer",
     href: "#",
     description:
-      "Joe is a passionate software developer with expertise in building cutting-edge applications. He loves exploring new technologies and enjoys working on projects that involve artificial intelligence and machine learning. Joe is dedicated to creating software solutions that make a positive impact.",
+      "Jasn is a passionate software developer with expertise in building cutting-edge applications. He loves exploring new technologies and enjoys working on projects that involve artificial intelligence and machine learning. Joe is dedicated to creating software solutions that make a positive impact.",
     imageUrl:
       "https://images.unsplash.com/photo-1560807707-9aa6ce70a7cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     date: "May 10, 2023",
     datetime: "2023-05-10",
     category: { title: "Tech", href: "#" },
     author: {
-      name: "Joe Smith",
+      name: "Jasn Smith",
       role: "Software Developer",
       href: "#",
       imageUrl:

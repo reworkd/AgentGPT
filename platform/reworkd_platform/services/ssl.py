4323134fab9a22ee4ba7bbffebeee6411f258cc3
awtkns <32209255+awtkns@users.noreply.github.com>
2023-07-22 13:43:45 -0700

    🐛 Improve SSL devx

--- platform/reworkd_platform/services/ssl.py
+++ platform/reworkd_platform/services/ssl.py
@@ -1,4 +1,5 @@
 from ssl import create_default_context, SSLContext
+from typing import List
 
 from reworkd_platform.settings import Settings
 
@@ -6,14 +7,17 @@ MACOS_CERT_PATH = "/etc/ssl/cert.pem"
 DOCKER_CERT_PATH = "/etc/ssl/certs/ca-certificates.crt"
 
 
-def get_ssl_context(settings: Settings) -> SSLContext:
+def get_ssl_context(settings: Settings, paths: List[str] = None) -> SSLContext:
     if settings.db_ca_path:
         return create_default_context(cafile=settings.db_ca_path)
 
-    for path in (MACOS_CERT_PATH, DOCKER_CERT_PATH):
+    for path in paths or [MACOS_CERT_PATH, DOCKER_CERT_PATH]:
         try:
             return create_default_context(cafile=path)
         except FileNotFoundError:
             continue
 
-    raise FileNotFoundError("No CA certificates found for your OS.")
+    raise ValueError(
+        "No CA certificates found for your OS. To fix this, please run change "
+        "db_ca_path in your settings.py to point to a valid CA certificate file."
+    )

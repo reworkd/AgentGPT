901123f5494e53666fe58b3b0a649c13141cf406
awtkns <32209255+awtkns@users.noreply.github.com>
2023-07-21 17:07:12 -0700

    🚤 Improve rendering on mobile

--- next/src/components/BannerBadge.tsx
+++ next/src/components/BannerBadge.tsx
@@ -2,17 +2,18 @@ import clsx from "clsx";
 import type { PropsWithChildren } from "react";
 import React from "react";
 import { FaChevronRight } from "react-icons/fa";
-import { IoSparkles } from "react-icons/io5";
 
 type BadgeProps = PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement>>;
 
 const BannerBadge = ({ children, className, ...props }: BadgeProps) => (
-  <div className="rounded-full bg-gradient-to-tl from-[#A02BFE] via-[#02FCF1] to-[#A02BFE] p-[1px] subpixel-antialiased">
+  <div
+    className={clsx(
+      "rounded-full bg-gradient-to-tl from-[#A02BFE] via-[#02FCF1] to-[#A02BFE] p-[1px] subpixel-antialiased",
+      className
+    )}
+  >
     <a
-      className={clsx(
-        "animate-border-pulse py group relative flex w-max cursor-pointer items-center gap-2 rounded-full bg-black px-4 py-2 text-xs text-white",
-        className
-      )}
+      className="animate-border-pulse py group relative flex w-max cursor-pointer items-center gap-2 rounded-full bg-black px-4 py-2 text-xs text-white"
       {...props}
     >
       <span>{children}</span>

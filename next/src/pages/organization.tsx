1cfc19903876ad215ad9919fe4e10976932fbfc2
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-23 16:01:02 -0700

    ✨ Formatting

--- next/src/pages/organization.tsx
+++ next/src/pages/organization.tsx
@@ -12,12 +12,12 @@ const OrganizationManagement = () => {
                 <div className="max-w-3xl w-full justify-starts">
                     <div className="h-20 border-b-[1px] p-4 mb-6 border-white/10">
                         <h1 className="text-3xl font-semibold text-gray-300">Organization</h1>
-                        <span className="font-light text-white/50 font-sm">Manage your Organization settings</span>
+                        <span className="font-light text-sm text-white/50 font-sm">Manage your Organization settings</span>
                     </div>
                     <div className=" border-b-[1px] border-white/10 p-4 mb-6">
                         <h2 className="text-lg text-gray-300 font-light">Logo</h2>
                         <div className="w-20 h-20 rounded-sm bg-gray-400 my-4"></div>
-                        <span className="font-light text-white/50">Pick a logo for your Oraganization</span>
+                        <span className="font-light text-white/50 text-sm">Pick a logo for your Oraganization</span>
                     </div>
                     <div className="border-b-[1px] border-white/10 p-4 mb-6">
                         <div className="max-w-sm">
@@ -45,7 +45,7 @@ const OrganizationManagement = () => {
                                 />
                             </div>
                             <button
-                                className="bg-purple-400/20 text-white py-2 px-4 mt-4 rounded hover:bg-purple-400/20 focus:outline-none focus:ring-2 focus:ring-purple-400/30"
+                                className="block bg-purple-400/20 text-white py-2 text-sm px-4 mt-4 rounded shadow-sm hover:bg-purple-400/20 focus:outline-none focus:ring-2 focus:ring-purple-400/30"
                                 >
                                 Update
                             </button>
@@ -54,17 +54,17 @@ const OrganizationManagement = () => {
                     <div className="px-4 sm:px-6 lg:px-8">
                         <div className="sm:flex sm:items-center">
                             <div className="sm:flex-auto">
-                            <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
-                            <p className="mt-2 text-sm text-gray-700">
+                            <h1 className="text-gray-300 font-light">Manage members</h1>
+                            <p className="mt-2 font-light text-white/50 text-sm">
                                 A list of all the users in your account including their name, title, email and role.
                             </p>
                             </div>
                             <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                             <button
                                 type="button"
-                                className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="block rounded bg-purple-400/20 px-3 py-2 text-center text-sm text-white shadow-sm hover:bg-purple-400/20 focus:ring-purple-400/30"
                             >
-                                Add user
+                                Invite people
                             </button>
                             </div>
                         </div>
@@ -72,25 +72,6 @@ const OrganizationManagement = () => {
                             <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                                 <table className="min-w-full divide-y divide-gray-300">
-                                <thead>
-                                    <tr>
-                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                                        Name
-                                    </th>
-                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Title
-                                    </th>
-                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Status
-                                    </th>
-                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Role
-                                    </th>
-                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                                        <span className="sr-only">Edit</span>
-                                    </th>
-                                    </tr>
-                                </thead>
                                 <tbody className="divide-y divide-gray-200 bg-white">
                                     {/* {people.map((person) => (
                                     <tr key={person.email}>

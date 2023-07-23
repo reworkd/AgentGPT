888c75876cf1b6ffe6da8f7e9365d0efb9530a11
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-23 16:45:46 -0700

    ✨ Fix tests

--- platform/reworkd_platform/schemas/workflow/blocks/conditions/ifcondition.py
+++ platform/reworkd_platform/schemas/workflow/blocks/conditions/ifcondition.py
@@ -1,12 +1,14 @@
-from typing import Literal, Union
+from typing import Literal, TypeVar
 
 from reworkd_platform.schemas.workflow.base import Block, BlockIOBase
 
+T = TypeVar("T", float, str)
+
 
 class IfInput(BlockIOBase):
-    value_one: Union[str, int, float, bool]
+    value_one: str
     operator: Literal["==", "!=", "<", ">", "<=", ">="]
-    value_two: Union[str, int, float, bool]
+    value_two: str
 
 
 class IfOutput(BlockIOBase):
@@ -23,19 +25,38 @@ class IfCondition(Block):
         value_two = self.input.value_two
         operator = self.input.operator
 
-        if operator == "==":
-            result = value_one == value_two
-        elif operator == "!=":
-            result = value_one != value_two
-        elif operator == "<":
-            result = value_one < value_two
-        elif operator == ">":
-            result = value_one > value_two
-        elif operator == "<=":
-            result = value_one <= value_two
-        elif operator == ">=":
-            result = value_one >= value_two
-        else:
-            raise ValueError(f"Invalid operator: {operator}")
+        result = compare(value_one, operator, value_two)
 
         return IfOutput(result=result)
+
+
+def compare(value_one: str, operator: str, value_two: str) -> bool:
+    if is_number(value_one) and is_number(value_two):
+        return perform_operation(float(value_one), operator, float(value_two))
+    else:
+        return perform_operation(value_one, operator, value_two)
+
+
+def perform_operation(value_one: T, operator: str, value_two: T) -> bool:
+    if operator == "==":
+        return value_one == value_two
+    elif operator == "!=":
+        return value_one != value_two
+    elif operator == "<":
+        return value_one < value_two
+    elif operator == ">":
+        return value_one > value_two
+    elif operator == "<=":
+        return value_one <= value_two
+    elif operator == ">=":
+        return value_one >= value_two
+    else:
+        raise ValueError(f"Invalid operator: {operator}")
+
+
+def is_number(string: str) -> bool:
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False

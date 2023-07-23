752cdd5b910ba9a2a86051d8bf78009d8d5ceff5
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-23 15:27:26 -0700

    🛠 General Section

--- next/src/pages/organization.tsx
+++ next/src/pages/organization.tsx
@@ -9,18 +9,47 @@ const OrganizationManagement = () => {
     return (
         <DashboardLayout> 
             <div className="flex justify-center items-center w-full h-screen bg-gray-800">
-                <div className="max-w-2xl justify-starts">
-                    <div className="h-20 border-b-2 border-white/20">
+                <div className="max-w-2xl w-full justify-starts">
+                    <div className="h-20 border-b-2 p-4 mb-6 border-white/20">
                         <h1 className="text-3xl font-semibold text-gray-300">Organization</h1>
                         <span className="font-light text-white/50 font-sm">Manage your Organization settings</span>
                     </div>
                     <div className=" border-b-2 border-white/20 p-4 mb-6">
-                        <h2 className="text-lg text-white/30 font-light">Logo</h2>
+                        <h2 className="text-lg text-gray-300 font-light">Logo</h2>
                         <div className="w-20 h-20 rounded-sm bg-gray-400 my-4"></div>
-                        <span className="font-light text-white/30">Pick a logo for your workspace</span>
+                        <span className="font-light text-white/50">Pick a logo for your Oraganization</span>
                     </div>
-                    <div className="bg-gray-800 bg-opacity-20 border border-gray-300 rounded p-4 mb-6">
-                        <h2 className="text-lg font-semibold">General Section</h2>
+                    <div className="border-b-2 border-white/20 p-4 mb-6">
+                        <div className="max-w-sm">
+                            <h2 className="text-lg text-gray-300 font-light">General </h2>
+                            <div className="mt-4">
+                                <label htmlFor="organizationName" className="block text-sm font-light text-gray-300">
+                                Organization Name
+                                </label>
+                                <input
+                                type="text"
+                                id="organizationName"
+                                className="w-full py-2 px-3 bg-gray-900 text-white border border-white/20 rounded mt-1 focus:ring-white focus:border-white"
+                                placeholder="Enter organization name"
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <label htmlFor="organizationUrl" className="block text-sm font-light text-gray-300">
+                                Organization URL
+                                </label>
+                                <input
+                                type="text"
+                                id="organizationUrl"
+                                className="w-full py-2 px-3 bg-gray-900 text-white border border-white/20 rounded mt-1 focus:ring-white focus:border-white"
+                                placeholder="Enter organization URL"
+                                />
+                            </div>
+                            <button
+                                className="bg-purple-400/20 text-white py-2 px-4 mt-4 rounded hover:bg-purple-400/20 focus:outline-none focus:ring-2 focus:ring-purple-400/30"
+                                >
+                                Update
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>

9783f4ae20adf5ad62b64e329b3fa5d624574056
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-23 13:11:11 -0700

    ✨ Create Organization Page

---
+++ next/src/pages/organization.tsx
@@ -0,0 +1,16 @@
+import DashboardLayout from "../layout/dashboard";
+
+const OrganizationManagement = () => {
+    return (
+        <DashboardLayout> 
+            <div className="relative w-full h-screen bg-gray-950">
+                <div>Title Section</div>
+                <div>Logo Section</div>
+                <div>General Section</div>
+                <div>Member Manage Section</div>
+            </div>
+        </DashboardLayout>
+    );
+}
+
+export default OrganizationManagement;

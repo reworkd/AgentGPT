0f87240205c3732a77b4945f243b847ac0fe28d3
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-23 15:52:11 -0700

    ✨ Better Table

--- next/src/pages/organization.tsx
+++ next/src/pages/organization.tsx
@@ -51,75 +51,83 @@ const OrganizationManagement = () => {
                             </button>
                         </div>
                     </div>
-                    <div className="bg-gray-900">
-                        <div className="mx-auto max-w-7xl">
-                            <div className="bg-gray-900 py-10">
-                            <div className="px-4 sm:px-6 lg:px-8">
-                                <div className="sm:flex sm:items-center">
-                                <div className="sm:flex-auto">
-                                    <h1 className="text-base font-semibold leading-6 text-white">Users</h1>
-                                    <p className="mt-2 text-sm text-gray-300">
-                                    A list of all the users in your account including their name, title, email and role.
-                                    </p>
-                                </div>
-                                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                                    <button
-                                    type="button"
-                                    className="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                                    >
-                                    Add user
-                                    </button>
-                                </div>
-                                </div>
-                                <div className="mt-8 flow-root">
-                                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                                    <table className="min-w-full divide-y divide-gray-700">
-                                        <thead>
-                                        <tr>
-                                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
-                                            Name
-                                            </th>
-                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
-                                            Title
-                                            </th>
-                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
-                                            Email
-                                            </th>
-                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
-                                            Role
-                                            </th>
-                                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                                            <span className="sr-only">Edit</span>
-                                            </th>
-                                        </tr>
-                                        </thead>
-                                        {/* <tbody className="divide-y divide-gray-800">
-                                        {people.map((person) => (
-                                            <tr key={person.email}>
-                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
-                                                {person.name}
-                                            </td>
-                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{person.title}</td>
-                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{person.email}</td>
-                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{person.role}</td>
-                                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                                                <a href="#" className="text-indigo-400 hover:text-indigo-300">
-                                                Edit<span className="sr-only">, {person.name}</span>
-                                                </a>
-                                            </td>
-                                            </tr>
-                                        ))}
-                                        </tbody> */}
-                                    </table>
-                                    </div>
-                                </div>
-                                </div>
+                    <div className="px-4 sm:px-6 lg:px-8">
+                        <div className="sm:flex sm:items-center">
+                            <div className="sm:flex-auto">
+                            <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
+                            <p className="mt-2 text-sm text-gray-700">
+                                A list of all the users in your account including their name, title, email and role.
+                            </p>
+                            </div>
+                            <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                            <button
+                                type="button"
+                                className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Add user
+                            </button>
+                            </div>
+                        </div>
+                        <div className="mt-8 flow-root">
+                            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                                <table className="min-w-full divide-y divide-gray-300">
+                                <thead>
+                                    <tr>
+                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                        Name
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Title
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Status
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Role
+                                    </th>
+                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                                        <span className="sr-only">Edit</span>
+                                    </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200 bg-white">
+                                    {/* {people.map((person) => (
+                                    <tr key={person.email}>
+                                        <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                                        <div className="flex items-center">
+                                            <div className="h-11 w-11 flex-shrink-0">
+                                            <img className="h-11 w-11 rounded-full" src={person.image} alt="" />
+                                            </div>
+                                            <div className="ml-4">
+                                            <div className="font-medium text-gray-900">{person.name}</div>
+                                            <div className="mt-1 text-gray-500">{person.email}</div>
+                                            </div>
+                                        </div>
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                                        <div className="text-gray-900">{person.title}</div>
+                                        <div className="mt-1 text-gray-500">{person.department}</div>
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                                        <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                            Active
+                                        </span>
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{person.role}</td>
+                                        <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                                            Edit<span className="sr-only">, {person.name}</span>
+                                        </a>
+                                        </td>
+                                    </tr>
+                                    ))} */}
+                                </tbody>
+                                </table>
                             </div>
                             </div>
                         </div>
-                        </div>
-
+                    </div>
                 </div>
             </div>
         </DashboardLayout>

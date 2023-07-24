e217846e5f272915a66bbb0c5c162387c0654e2d
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-23 23:28:28 -0700

    ✨ Used Input labels

--- next/src/pages/organization.tsx
+++ next/src/pages/organization.tsx
@@ -1,52 +1,46 @@
 import DashboardLayout from "../layout/dashboard";
 import type { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import React, { useState } from "react";
+import React from "react";
 import nextI18NextConfig from "../../next-i18next.config.js";
 import { languages } from "../utils/languages";
-import { FaEllipsisH } from "react-icons/fa";
 import PrimaryButton from "../components/PrimaryButton";
-import Input from "../components/Input";
+import Input from "../ui/input";
+
 
 const OrganizationManagement = () => {
     return (
         <DashboardLayout> 
             <div className="flex justify-center items-center w-full h-screen bg-zinc-900">
-                <div className="max-w-4xl w-full font-mono justify-starts">
+                <div className="max-w-4xl w-full font-sans justify-starts">
                     <div className="border-b p-4 my-2 border-white/10">
-                        <h1 className="text-5xl mb-4 tracking-wide font-semibold text-gray-300">Organization</h1>
-                        <span className="text-lg text-white/50">Manage your Organization settings</span>
+                        <h1 className="text-5xl mb-4 tracking-wide font-semibold text-white">Organization</h1>
+                        <span className="text-sm text-white/50">Manage your Organization settings</span>
                     </div>
                     <div className=" border-b border-white/10 p-4 mb-2">
-                        <h2 className="text-2xl text-gray-300">Logo</h2>
-                        <div className="w-40 h-40 rounded-lg bg-white my-4"></div>
-                        <span className="text-white/50 text-lg">Pick a logo for your Oraganization</span>
+                        <h2 className="text-2xl text-white">Logo</h2>
+                        <div className="w-20 h-20 rounded-lg bg-white my-4"></div>
+                        <span className="text-white/50 text-sm">Pick a logo for your Oraganization</span>
                     </div>
                     <div className="border-b border-white/10 p-4 mb-6">
                         <div className="max-w-sm">
-                            <h2 className="text-2xl text-gray-300">General </h2>
+                            <h2 className="text-2xl text-white">General </h2>
                             <div className="mt-4">
-                                <label htmlFor="organizationName" className="block text-white/50 text-lg mb-4 text-gray-300">
-                                Organization Name
-                                </label>
                                 <Input
-                                type="text"
-                                id="organizationName"
-                                placeholder="Enter organization name"
+                                 label="Organization Name"
+                                name="organizationName"
+                                 placeholder="Enter organization name"
                                 />
                             </div>
                             <div className="mt-4">
-                                <label htmlFor="organizationUrl" className="block text-white/50 text-lg mb-4 text-gray-300">
-                                Organization URL
-                                </label>
-                                <Input
-                                type="text"
-                                id="organizationUrl"
-                                placeholder="Enter organization URL"
+                            <Input
+                                 label="Organization Url"
+                                name="organizationUrl"
+                                 placeholder="Enter organization url"
                                 />
                             </div>
                             <PrimaryButton
-                                className="py-2 px-4 mt-6"
+                                className=" px-4 mt-6"
                                 >
                                 Update
                             </PrimaryButton>

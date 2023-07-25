b7a35c339078937e127df36c018bf0b615a74ed4
asim-shrestha <asim.shrestha@hotmail.com>
2023-07-25 12:52:10 -0700

    💻 Use a new dialog for workflow saving

--- next/src/pages/workflow/index.tsx
+++ next/src/pages/workflow/index.tsx
@@ -8,7 +8,8 @@ import { useState } from "react";
 import nextI18NextConfig from "../../../next-i18next.config";
 import CreateWorkflowDialog from "../../components/workflow/CreateWorkflowDialog";
 import EmptyWorkflowButton from "../../components/workflow/EmptyWorkflow";
-import WorkflowCard, { WorkflowCardDialog } from "../../components/workflow/WorkflowCard";
+import WorkflowCard from "../../components/workflow/WorkflowCard";
+import WorkflowDialog from "../../components/workflow/WorkflowDialog";
 import { useAuth } from "../../hooks/useAuth";
 import DashboardLayout from "../../layout/dashboard";
 import type { WorkflowMeta } from "../../services/workflow/workflowApi";
@@ -27,7 +28,8 @@ const WorkflowList: NextPage = () => {
 
   const data = query.data ?? [];
   const [selectedWorkflow, setSelectedWorkflow] = useState<WorkflowMeta | null>(null);
-  const [showDialog, setShowDialog] = useState(false);
+  const [showCreateDialog, setShowCreateDialog] = useState(false);
+  const [showWorkflowDialog, setShowWorkflowDialog] = useState(false);
 
   const handleCreateWorkflow = (name: string, description: string) => {
     api
@@ -41,6 +43,10 @@ const WorkflowList: NextPage = () => {
       .catch(console.error);
   };
 
+  const saveWorkflow = (workflow: WorkflowMeta) => {
+    // TODO
+  };
+
   return (
     <DashboardLayout>
       <LayoutGroup>
@@ -51,37 +57,29 @@ const WorkflowList: NextPage = () => {
               workflow={workflow}
               onClick={() => {
                 setSelectedWorkflow(workflow);
+                setShowWorkflowDialog(true);
               }}
             />
           ))}
           <EmptyWorkflowButton
             onClick={() => {
-              setShowDialog(true);
+              setShowCreateDialog(true);
             }}
           />
         </div>
-        {selectedWorkflow != null && (
-          <WorkflowCardDialog
-            workflow={selectedWorkflow}
-            onEdit={() => {
-              void router.push(`workflow/${selectedWorkflow.id}`);
-            }}
-            onDelete={() => {
-              api
-                .delete(selectedWorkflow.id)
-                .then(async () => {
-                  await query.refetch();
-                  setSelectedWorkflow(null);
-                })
-                .catch(console.error);
-            }}
-            onClose={() => void setSelectedWorkflow(null)}
-          />
-        )}
       </LayoutGroup>
+      <WorkflowDialog
+        workflow={selectedWorkflow}
+        openWorkflow={() => {
+          void router.push(`workflow/${selectedWorkflow?.id || ""}`);
+        }}
+        saveWorkflow={saveWorkflow}
+        showDialog={showWorkflowDialog}
+        setShowDialog={setShowWorkflowDialog}
+      />
       <CreateWorkflowDialog
-        showDialog={showDialog}
-        setShowDialog={setShowDialog}
+        showDialog={showCreateDialog}
+        setShowDialog={setShowCreateDialog}
         createWorkflow={handleCreateWorkflow}
       />
     </DashboardLayout>

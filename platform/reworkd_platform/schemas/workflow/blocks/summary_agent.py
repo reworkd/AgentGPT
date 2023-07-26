0fea00cf470ceb6ff3c477b07eee7d1a8c3d8eea
Rishabh <shahrishabh7@gmail.com>
2023-07-26 14:39:33 -0700

    read and extract info from pdf tables

--- platform/reworkd_platform/schemas/workflow/blocks/summary_agent.py
+++ platform/reworkd_platform/schemas/workflow/blocks/summary_agent.py
@@ -1,12 +1,17 @@
+from collections import defaultdict
 import os
 import tempfile
 
-import pinecone
+import tabula
 from langchain.chains.question_answering import load_qa_chain
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings.base import Embeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
+import pinecone
+from llama_index.query_engine import PandasQueryEngine
+from loguru import logger
 
 from reworkd_platform.schemas.agent import ModelSettings
 from reworkd_platform.schemas.user import UserBase
@@ -14,12 +19,13 @@ from reworkd_platform.schemas.workflow.base import Block, BlockIOBase
 from reworkd_platform.services.aws.s3 import SimpleStorageService
 from reworkd_platform.settings import settings
 from reworkd_platform.web.api.agent.model_settings import create_model
-
-INDEX_NAME = "prod"
+import openai
 
 
 class SummaryAgentInput(BlockIOBase):
     company_context: str
+    filename1: str
+    filename2: str
 
 
 class SummaryAgentOutput(SummaryAgentInput):
@@ -33,48 +39,135 @@ class SummaryAgent(Block):
 
     async def run(self, workflow_id: str) -> BlockIOBase:
         with tempfile.TemporaryDirectory() as temp_dir:
+            # files = SimpleStorageService().download_folder(
+            #     bucket_name="test-pdf-123", prefix=f"{workflow_id}/", path=temp_dir
+            # )
+
             files = SimpleStorageService().download_folder(
-                bucket_name="test-pdf-123", prefix=f"{workflow_id}/", path=temp_dir
+                bucket_name="test-pdf-123",
+                prefix="f5957ef2-fca6-449a-9545-8e62b67116d6/",
+                path=temp_dir,
             )
 
             docsearch = self.chunk_documents_to_pinecone(
                 files=files,
+                embeddings=(
+                    OpenAIEmbeddings(
+                        client=None,
+                        # Meta private value but mypy will complain its missing
+                        openai_api_key=settings.openai_api_key,
+                    )
+                ),
                 path=temp_dir,
             )
 
-        response = await self.execute_query_on_pinecone(
-            context=self.input.company_context, docsearch=docsearch
-        )
+            response = await self.execute_query_on_pinecone(
+                company_context=self.input.company_context, docsearch=docsearch
+            )
+            logger.info(f"response: {response}")
 
         return SummaryAgentOutput(**self.input.dict(), result=response)
 
-    def chunk_documents_to_pinecone(self, files: list[str], path: str) -> Pinecone:
-        index = pinecone.Index(INDEX_NAME)
-        index.delete(delete_all=True)  # TODO put this in a namespace
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
-        embedding = OpenAIEmbeddings(
-            client=None,
-            openai_api_key=settings.openai_api_key,
+    def load_pdf(self, filepath) -> list[str]:
+        pdf_data = PyPDFLoader(filepath).load()
+        return pdf_data
+
+    def name_table(self, table: str) -> str:
+        openai.api_key = settings.openai_api_key
+
+        prompt = f"""
+        Write a title for the table that is less than 9 words: {table}
+        """
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=1,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
         )
 
-        # TODO do this async in parallel
+        response_message_content = response["choices"][0]["message"]["content"]
+
+        return response_message_content
+
+    def read_and_preprocess_tables(
+        self, relevant_table_metadata: defaultdict(list)
+    ) -> list[str]:
+        processed = []
+        parsed_dfs_from_file = []
+        for source in relevant_table_metadata.keys():
+            page_numbers = relevant_table_metadata[source]
+            filtered_page_numbers = list(filter(lambda x: x != 0, page_numbers))
+            if len(filtered_page_numbers) > 1:
+                filtered_page_numbers.sort()
+                start_page = filtered_page_numbers[0]
+                end_page = filtered_page_numbers[-1]
+
+                pages_to_read = ",".join(str(page) for page in filtered_page_numbers)
+                parsed_dfs_from_file = tabula.read_pdf(
+                    source, pages=f"{start_page}-{end_page}"
+                )
+                for df in parsed_dfs_from_file:
+                    if not df.empty:
+                        df_name = self.name_table(str(df.iloc[:5]))
+                        processed_df = "\n".join([df.to_csv(index=False)])
+                        processed_df_with_title = "\n".join([df_name, processed_df])
+                        processed.append(processed_df_with_title)
+
+        return processed
+
+    def chunk_documents_to_pinecone(
+        self, files: list[str], embeddings: Embeddings, path: str
+    ) -> Pinecone:
+        index_name = "prod"
+        index = pinecone.Index(index_name)
+        index.delete(delete_all=True)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
         texts = []
         for file in files:
             filepath = os.path.join(path, file)
-            pdf_data = PyPDFLoader(filepath).load()
+            # table_data = self.read_and_preprocess_tables(filepath)
+            pdf_data = self.load_pdf(filepath)
             texts.extend(text_splitter.split_documents(pdf_data))
+            # texts.extend(text_splitter.create_documents(table_data))
 
-        return Pinecone.from_texts(
-            [t.page_content for t in texts],
-            embedding,
-            index_name=INDEX_NAME,
+        docsearch = Pinecone.from_documents(
+            [t for t in texts],
+            embeddings,
+            index_name=index_name,
         )
 
-    async def execute_query_on_pinecone(self, context: str, docsearch: Pinecone) -> str:
-        docs = docsearch.similarity_search(context, k=7)
+        return docsearch
+
+    async def execute_query_on_pinecone(
+        self, company_context: str, docsearch: Pinecone
+    ) -> str:
+        docs = docsearch.similarity_search(company_context, k=7)
+        relevant_table_metadata = defaultdict(list)
+        for doc in docs:
+            doc_source = doc.metadata["source"]
+            page_number = int(doc.metadata["page"])
+            relevant_table_metadata[doc_source].append(page_number)
+
+        processed_tables = self.read_and_preprocess_tables(relevant_table_metadata)
+        logger.info(f"relevant processed tables: {processed_tables}")
 
         prompt = f"""
-        Help extract information relevant to a company with the following details: {context} from the following documents. Include information relevant to the market, strategies, and products. Here are the documents: {docs}. After each point, reference the source you got each piece of information from (cite the source). If there's multiple sources, include information from all sources.
+        Help extract information relevant to a company with the following details: {company_context} from the following documents. Start with the company background info. Then, include information relevant to the market, strategies, and products. Here are the documents: {docs}. After each point, reference the source you got the information from. Include relevant quantitative metrics/trends from these tables to back your claims as well, do not make anything up: {processed_tables}.
+        """
+
+        prompt = f"""Help extract information relevant to a company with the following details: {company_context} from the following documents. Start with the company background info. Then, include information relevant to the market, strategies, and products. Here are the documents: {docs}. After each point, reference the source you got the information from.
+
+        Also list any interesting quantitative metrics or trends based on the following tables: {processed_tables}. Include which table you got information from.
+
+        Cite sources for sentences using the page number from original source document. Do not list sources at the end of the writing.
+
+        Example: "This is a cited sentence. (Source: Luxury Watch Market Size Report, Page 17).
+
+        Format your response as slack markdown.
         """
 
         llm = create_model(

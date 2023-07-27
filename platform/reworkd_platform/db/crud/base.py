f1ac5b91adc6909d37da4450d86fefb8798d0e3b
awtkns <32209255+awtkns@users.noreply.github.com>
2023-07-27 01:14:42 -0700

    🤌 Generic oauth installer

--- platform/reworkd_platform/db/crud/base.py
+++ platform/reworkd_platform/db/crud/base.py
@@ -1,6 +1,20 @@
+from typing import TypeVar
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from reworkd_platform.db.dependencies import get_db_session
+
+T = TypeVar("T", bound="BaseCrud")
 
 
 class BaseCrud:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    @classmethod
+    async def inject(
+        cls,
+        session: AsyncSession = Depends(get_db_session),
+    ) -> T:
+        return cls(session)

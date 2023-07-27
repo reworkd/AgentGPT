6e4d6db7d7684b26ff14a045dd4ac7c7269c3b0b
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-07-26 20:11:14 -0700

    ✨ Better animations

--- next/src/pages/welcome.tsx
+++ next/src/pages/welcome.tsx
@@ -1,50 +1,66 @@
 import { motion, useAnimation } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 import Image from "next/image";
 import FadeIn from "../components/motions/FadeIn";
 
 const welcome = () => {
     const controls = useAnimation();
+    const [buttonClicked, setButtonClicked] = useState(false);
+
     useEffect(() => {
         controls.start({
             scale: 1,
             y: 0,
-            transition: { type: 'spring', delay: 0.5, stiffness: 20, damping: 20, mass: 1 }
+            transition: { type: 'spring', stiffness: 50, damping: 15, mass: 1 }
         });
     }, [controls]);
 
+    useEffect(() => {
+        if (buttonClicked) {
+            controls.start({
+                opacity: 0,
+                transition: { duration: 1 }
+            });
+        }
+    }, [buttonClicked, controls]);
+
+    const handleButtonClick = () => {
+        setButtonClicked(true);
+    };
+
     return (
-        <div className="flex justify-center items-center w-full h-full min-h-screen bg-black">
-            <div className="max-w-4xl w-full max-h-4xl h-full items-center flex flex-col font-sans justify-start">
-                <motion.div
-                    initial={{ scale: 4, y: 700 }}
-                    animate={controls}
-                >
+        <div className="flex overflow-hidden justify-center items-center w-full h-full min-h-screen bg-black">
+            <motion.div
+                className="max-w-4xl w-full max-h-4xl h-full items-center flex flex-col font-sans justify-center text-center"
+                initial={{ scale: 5, y: 1100, opacity: 1}}
+                animate={controls}
+            >
+                <motion.div>
                     <Image
-                        src="/logos/dark-default-gradient.svg"
+                        src="/logos/dark-default-solid.svg"
                         width="100"
                         height="100"
                         alt="Reworkd AI"
                         className="m-4"
                     />
                 </motion.div>
-                <FadeIn duration={3} delay={5}>
-                    <h1 className="text-white font-light tracking-widest text-4xl mb-6">
-                        Welcome to Workflows
+                <FadeIn duration={2} delay={1.5}>
+                    <h1 className="text-white font-semibold tracking-widest text-4xl mb-6">
+                        Welcome to Reworkd
                     </h1>
                 </FadeIn >
-                <FadeIn duration={3} delay={6}>
-                    <p className="text-white/50 font-thin mb-8">
-                        Workflows help you to streamline your business processes
+                <FadeIn duration={2} delay={2.5}>
+                    <p className="text-white/50 max-w-lg text-center font-light mb-8">
+                         Reworkd allows you to leverage AI Agents to automate business workflows you once spent countless human hours on. Experience a new way of working.
                     </p>
                 </FadeIn>
-                <FadeIn duration={3} delay={7}>
-                    <PrimaryButton className="px-16">
+                <FadeIn duration={2} delay={3.5}>
+                    <PrimaryButton className="px-16" onClick={handleButtonClick}>
                         Get Started
                     </PrimaryButton>
                 </FadeIn>
-            </div>
+            </motion.div>
         </div>
     );
 }

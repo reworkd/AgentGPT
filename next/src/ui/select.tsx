aa9c55327febccc31ae2b16b83991c4d7eb85969
asim-shrestha <asim.shrestha@hotmail.com>
2023-08-02 18:43:07 -0700

    ✨ Display message when no items available in select

--- next/src/ui/select.tsx
+++ next/src/ui/select.tsx
@@ -51,6 +51,9 @@ export default function Select<T>(props: Props<T>) {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {(!props.items || props.items?.length == 0) && (
+                  <div className="px-1 text-xs">No options available</div>
+                )}
                 {props.items?.map((item, i) => (
                   <Listbox.Option
                     key={i}

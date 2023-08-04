5d19c41f6844d344387903179451cc7f143e7169
asim-shrestha <asim.shrestha@hotmail.com>
2023-08-04 10:25:14 -0700

    ✨ Logs for manual workflow trigger

--- platform/reworkd_platform/schemas/workflow/blocks/triggers/manual_trigger.py
+++ platform/reworkd_platform/schemas/workflow/blocks/triggers/manual_trigger.py
@@ -1,5 +1,15 @@
-from reworkd_platform.schemas.workflow.blocks.do_nothing import DoNothingBlock
+from typing import Any
+
+from reworkd_platform.schemas.workflow.base import Block, BlockIOBase
+from reworkd_platform.services.sockets import websockets
 
 
-class ManualTriggerBlock(DoNothingBlock):
-    pass
+class ManualTriggerBlock(Block):
+    type = "ManualTrigger"
+    description = "Manually trigger the workflow"
+    image_url = ""
+    input: BlockIOBase
+
+    async def run(self, workflow_id: Any, **kwargs: Any) -> BlockIOBase:
+        websockets.log(workflow_id, f"Manual workflow started.")
+        return BlockIOBase()

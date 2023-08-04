309b97a38e537ef178bcd5864b361fcc9a965f63
asim-shrestha <asim.shrestha@hotmail.com>
2023-08-04 11:34:39 -0700

    ✨ Add logging to diff doc

--- platform/reworkd_platform/schemas/workflow/blocks/pdf/diff_doc.py
+++ platform/reworkd_platform/schemas/workflow/blocks/pdf/diff_doc.py
@@ -1,12 +1,13 @@
 import difflib
 import io
-from typing import List, Any
+from typing import Any, List
 
 from docx import Document
 from docx.shared import RGBColor
 
 from reworkd_platform.schemas.workflow.base import Block, BlockIOBase
 from reworkd_platform.services.aws.s3 import SimpleStorageService
+from reworkd_platform.services.sockets import websockets
 from reworkd_platform.settings import settings
 
 
@@ -28,17 +29,21 @@ class DiffDoc(Block):
 
     async def run(self, workflow_id: str, **kwargs: Any) -> DiffDocOutput:
         with io.BytesIO() as diff_doc_file:
+            websockets.log(workflow_id, "Creating diff of original and updated text")
             diffs = get_diff(self.input.original, self.input.updated)
             diff_doc_file = get_diff_doc(diffs, diff_doc_file)
 
+            websockets.log(workflow_id, "Uploading diff doc to S3")
             s3_service = SimpleStorageService(settings.s3_bucket_name)
             s3_service.upload_to_bucket(
                 object_name=f"docs/{workflow_id}/{self.id}.docx",
                 file=diff_doc_file,
             )
+
             file_url = s3_service.create_presigned_download_url(
                 object_name=f"docs/{workflow_id}/{self.id}.docx",
             )
+            websockets.log(workflow_id, f"Doc successfully uploaded to S3: {file_url}")
 
             return DiffDocOutput(file_url=file_url)
 

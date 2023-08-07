baac8a8341d8d58c5c7b9921695296e6494ef58e
Rishabh <shahrishabh7@gmail.com>
2023-08-07 14:40:22 -0700

    better log on competitor removal

--- platform/reworkd_platform/schemas/workflow/blocks/agents/content_refresher_agent.py
+++ platform/reworkd_platform/schemas/workflow/blocks/agents/content_refresher_agent.py
@@ -188,7 +188,7 @@ def remove_competitors(
         if competitor_pattern.search(
             source["url"].replace(" ", "").lower()
         ) or competitor_pattern.search(source["title"].replace(" ", "").lower()):
-            log(f"Removing source due to competitor match: '{source['title']}'")
+            log(f"Removing competitive source: '{source['title']}'")
         else:
             filtered_sources.append(source)
 

b8b933daad76c1bcba1e40c1f6b0f07f7ff364d7
Rishabh <shahrishabh7@gmail.com>
2023-08-07 13:40:30 -0700

    linting

--- platform/reworkd_platform/tests/agent/content_refresh_test.py
+++ platform/reworkd_platform/tests/agent/content_refresh_test.py
@@ -1,8 +1,12 @@
 import pytest
-from reworkd_platform.schemas.workflow.blocks.agents.content_refresher_agent import remove_competitors
+from reworkd_platform.schemas.workflow.blocks.agents.content_refresher_agent import (
+    remove_competitors,
+)
+
 
 def log(message: str):
     print(message)
+
 
 @pytest.mark.parametrize(
     "sources, competitors, expected",
@@ -11,30 +15,45 @@ def log(message: str):
             [
                 {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
                 {"url": "https://www.competitor.com/news", "title": "Competitor news"},
-                {"url": "https://www.othercompany.com/news", "title": "Other company news"},
-                {"url": "https://www.dougandbeane.com/news", "title": "Doug and Beane news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
+                {
+                    "url": "https://www.dougandbeane.com/news",
+                    "title": "Doug and Beane news",
+                },
             ],
-            ["competitor","Doug and Beane", "ourfirm"],
+            ["competitor", "Doug and Beane", "ourfirm"],
             [
                 {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
-                {"url": "https://www.othercompany.com/news", "title": "Other company news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
             ],
         ),
         (
             [
                 {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
                 {"url": "https://www.competitor2.com/news", "title": "Competitor news"},
-                {"url": "https://www.othercompany.com/news", "title": "Other company news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
             ],
             ["competitor2"],
             [
                 {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
-                {"url": "https://www.othercompany.com/news", "title": "Other company news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
             ],
         ),
         (
             [],
-            ["competitor","Doug and Beane", "ourfirm"],
+            ["competitor", "Doug and Beane", "ourfirm"],
             [],
         ),
         (
@@ -45,8 +64,8 @@ def log(message: str):
             ],
             ["competitor1", "competitor2", "competitor3"],
             [],
-        )
-    ]
+        ),
+    ],
 )
 def test_remove_competitors(sources, competitors, expected):
     filtered_sources = remove_competitors(sources, competitors, log)

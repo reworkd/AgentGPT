d46e42f9397ce39e74b51450d4146b228d325a42
awtkns <32209255+awtkns@users.noreply.github.com>
2023-08-08 09:26:25 -0700

    ♻️ Fix failing tests

--- platform/reworkd_platform/tests/test_content_refresher_agent.py
+++ platform/reworkd_platform/tests/test_content_refresher_agent.py
@@ -40,3 +40,69 @@ async def test_run():
 
     service = ContentRefresherService(settings, log=lambda msg: print(msg))
     await service.refresh(input_)
+
+
+@pytest.mark.parametrize(
+    "sources, competitors, expected",
+    [
+        (
+            [
+                {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
+                {"url": "https://www.competitor.com/news", "title": "Competitor news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
+                {
+                    "url": "https://www.dougandbeane.com/news",
+                    "title": "Doug and Beane news",
+                },
+            ],
+            ["competitor", "Doug and Beane", "ourfirm"],
+            [
+                {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
+            ],
+        ),
+        (
+            [
+                {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
+                {"url": "https://www.competitor2.com/news", "title": "Competitor news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
+            ],
+            ["competitor2"],
+            [
+                {"url": "https://www.ourcompany.com/news", "title": "Our company news"},
+                {
+                    "url": "https://www.othercompany.com/news",
+                    "title": "Other company news",
+                },
+            ],
+        ),
+        (
+            [],
+            ["competitor", "Doug and Beane", "ourfirm"],
+            [],
+        ),
+        (
+            [
+                {"url": "https://www.competitor1.com/news", "title": "Competitor news"},
+                {"url": "https://www.competitor2.com/news", "title": "Competitor news"},
+                {"url": "https://www.competitor3.com/news", "title": "Competitor news"},
+            ],
+            ["competitor1", "competitor2", "competitor3"],
+            [],
+        ),
+    ],
+)
+def test_remove_competitors(sources, competitors, expected):
+    service = ContentRefresherService(settings=Settings(), log=lambda msg: print(msg))
+
+    filtered_sources = service.remove_competitors(sources, competitors, service.log)
+    assert filtered_sources == expected

56b42189763f56bde0f190d46fb4a194665358e8
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-08-09 21:29:32 -0700

    ✨ Truncate

--- next/src/components/SourceCard.tsx
+++ next/src/components/SourceCard.tsx
@@ -11,12 +11,10 @@ const SourceCard = ({ link, position }: SourceCardProps) => {
   return (
     <FadeIn duration={2} delay={0.5}>
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="items-center p-2 border max-w-full flex hover:border-white/40 bg-white/20 border-white/10 rounded-md">
-          <FaGlobe className="w-14 h-6 text-white mr-2" />
-          <div className="max-w-fit max-h-6 text-inline whitespace-nowrap overflow-hidden">
-            <div className="text-blue-500 text-sm hover:underline">
-              {link}
-            </div>
+        <div className="flex items-center p-2 border max-w-full hover:border-white/40 bg-white/20 border-white/10 rounded-md">
+          <FaGlobe className="h-6 w-6 text-white mr-2" />
+          <div className="max-w-[11rem] flex-grow text-blue-500 text-sm hover:underline truncate">
+            {link}
           </div>
           <span className="text-xs px-4 font-bold">[{position}]</span>
         </div>

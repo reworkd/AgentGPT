fa40b2122b32f2be42dc419b26da79d02c06fbba
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-08-09 21:09:20 -0700

    ✨ Formatting

--- next/src/components/SourceCard.tsx
+++ next/src/components/SourceCard.tsx
@@ -9,18 +9,16 @@ interface SourceCardProps {
 
 const SourceCard = ({ link, position }: SourceCardProps) => {
   return (
-    <FadeIn duration={2} delay={.5}>  
-      <div className="items-center p-2 border max-w-full flex hover:border-white/40 bg-white/20 border-white/10 rounded-md">
-        <FaGlobe className="w-6 h-6 text-white mr-2" />
-        <div className="max-w-[9rem] max-h-6 text-inline whitespace-nowrap overflow-hidden">
-          <a
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400 text-sm hover:underline"
-          >
-            {link}
-          </a>
+    <FadeIn duration={2} delay={0.5}>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <div className="items-center p-2 border max-w-full flex hover:border-white/40 bg-white/20 border-white/10 rounded-md">
+          <FaGlobe className="w-14 h-6 text-white mr-2" />
+          <div className="max-w-fit max-h-6 text-inline whitespace-nowrap overflow-hidden">
+            <div className="text-blue-500 text-sm hover:underline">
+              {link}
+            </div>
+          </div>
+          <span className="text-xs px-4 font-bold">[{position}]</span>
         </div>
       </a>
     </FadeIn>

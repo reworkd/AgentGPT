a71f6c14565a75173af7dc8bb8ec4618ab20d86d
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-08-09 18:17:08 -0700

    ✨ FadeIn

--- next/src/components/SourceCard.tsx
+++ next/src/components/SourceCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaGlobe } from 'react-icons/fa';
+import FadeIn from './motions/FadeIn';
 
 interface SourceCardProps {
   link: string;
@@ -8,20 +9,22 @@ interface SourceCardProps {
 
 const SourceCard = ({ link, position }: SourceCardProps) => {
   return (
-    <div className="items-center p-2 border max-w-full flex gap-x-4 hover:border-white/40 bg-white/20 border-white/10 rounded-md">
-      <FaGlobe className="w-6 h-6 text-white mr-2" />
-      <div className="max-w-[9rem] max-h-6 text-inline whitespace-nowrap overflow-hidden">
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-400 text-sm hover:underline"
-        >
-          {link}
-        </a>
+    <FadeIn duration={2} delay={.5}>  
+      <div className="items-center p-2 border max-w-full flex hover:border-white/40 bg-white/20 border-white/10 rounded-md">
+        <FaGlobe className="w-6 h-6 text-white mr-2" />
+        <div className="max-w-[9rem] max-h-6 text-inline whitespace-nowrap overflow-hidden">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 text-sm hover:underline"
+          >
+            {link}
+          </a>
+        </div>
+        <span className="text-xs px-4 font-bold">[{position}]</span>
       </div>
-      <span className="text-xs px-4 font-bold">[{position}]</span>
-    </div>
+    </FadeIn>
   );
 }
 

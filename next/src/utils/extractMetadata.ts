d4b069e89a99c6049266727cc7753bf6f458faae
AkashDeep000 <akashdeepdas970@gmail.com>
2023-08-25 16:26:24 +0000

    fixed regx for cheaking valid url

--- next/src/utils/extractMetadata.ts
+++ next/src/utils/extractMetadata.ts
@@ -53,9 +53,9 @@ const isValidUrl = (url: string) => {
       "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
       "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
       "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" /*, + // validate query string
+      "(\\?[;&a-z\\d%_.~+=-]*)?",
     "i"
-  ); // validate fragment locator
+  );
   return !!urlPattern.test(url) as boolean;
 };
 

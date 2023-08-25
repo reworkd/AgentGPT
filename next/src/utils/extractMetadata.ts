15bd74c6300c60810093fe66ed744626a46e5708
Akashdeep <76022812+AkashDeep000@users.noreply.github.com>
2023-08-25 21:18:52 +0530

    Update next/src/utils/extractMetadata.ts
    Co-authored-by: Adam Watkins <cadamrun@gmail.com>

--- next/src/utils/extractMetadata.ts
+++ next/src/utils/extractMetadata.ts
@@ -53,8 +53,7 @@ const isValidUrl = (url: string) => {
       "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
       "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
       "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" /* + // validate query string
-        "(\\#[-a-z\\d_]*)?$"*/,
+      "(\\?[;&a-z\\d%_.~+=-]*)?" /*, + // validate query string
     "i"
   ); // validate fragment locator
   return !!urlPattern.test(url) as boolean;

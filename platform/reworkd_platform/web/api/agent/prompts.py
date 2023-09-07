748a76a559943320d318db1c12b0aa916deafb1f
awtkns <32209255+awtkns@users.noreply.github.com>
2023-09-07 10:38:05 -0700

    🐛 Fix sid refresh tokens

--- platform/reworkd_platform/web/api/agent/prompts.py
+++ platform/reworkd_platform/web/api/agent/prompts.py
@@ -25,9 +25,9 @@ analyze_task_prompt = PromptTemplate(
     Current task: "{task}"
 
     Based on this information, use the best function to make progress or accomplish the task entirely.
-    Select the correct function by being smart and efficient. Ensure "reasoning" and only "reasoning" is in the 
+    Select the correct function by being smart and efficient. Ensure "reasoning" and only "reasoning" is in the
     {language} language.
-    
+
     Note you MUST select a function.
     """,
     input_variables=["goal", "task", "language"],
@@ -41,7 +41,7 @@ code_prompt = PromptTemplate(
     For reference, your high level goal is {goal}
 
     Write code in English but explanations/comments in the "{language}" language.
-    
+
     Provide no information about who you are and focus on writing code.
     Ensure code is bug and error free and explain complex concepts through comments
     Respond in well-formatted markdown. Ensure code blocks are used for code sections.
@@ -66,15 +66,15 @@ execute_task_prompt = PromptTemplate(
 
 create_tasks_prompt = PromptTemplate(
     template="""You are an AI task creation agent. You must answer in the "{language}"
-    language. You have the following objective `{goal}`. 
-    
-    You have the following incomplete tasks: 
-    `{tasks}` 
-    
+    language. You have the following objective `{goal}`.
+
+    You have the following incomplete tasks:
+    `{tasks}`
+
     You just completed the following task:
-    `{lastTask}` 
-    
-    And received the following result: 
+    `{lastTask}`
+
+    And received the following result:
     `{result}`.
 
     Based on this, create a single new task to be completed by your AI system such that your goal is closer reached.
@@ -90,51 +90,51 @@ create_tasks_prompt = PromptTemplate(
 )
 
 summarize_prompt = PromptTemplate(
-    template="""You must answer in the "{language}" language. 
+    template="""You must answer in the "{language}" language.
 
-    Combine the following text into a cohesive document: 
-    
+    Combine the following text into a cohesive document:
+
     "{text}"
-    
-    Write using clear markdown formatting in a style expected of the goal "{goal}".    
-    Be as clear, informative, and descriptive as necessary.  
-    You will not make up information or add any information outside of the above text. 
-    Only use the given information and nothing more. 
-    
-    If there is no information provided, say "There is nothing to summarize".  
+
+    Write using clear markdown formatting in a style expected of the goal "{goal}".
+    Be as clear, informative, and descriptive as necessary.
+    You will not make up information or add any information outside of the above text.
+    Only use the given information and nothing more.
+
+    If there is no information provided, say "There is nothing to summarize".
     """,
     input_variables=["goal", "language", "text"],
 )
 
 company_context_prompt = PromptTemplate(
-    template="""You must answer in the "{language}" language. 
+    template="""You must answer in the "{language}" language.
 
     Create a short description on "{company_name}".
     Find out what sector it is in and what are their primary products.
-    
+
     Be as clear, informative, and descriptive as necessary.
-    You will not make up information or add any information outside of the above text. 
-    Only use the given information and nothing more. 
-    
-    If there is no information provided, say "There is nothing to summarize".  
+    You will not make up information or add any information outside of the above text.
+    Only use the given information and nothing more.
+
+    If there is no information provided, say "There is nothing to summarize".
     """,
     input_variables=["company_name", "language"],
 )
 
 summarize_pdf_prompt = PromptTemplate(
-    template="""You must answer in the "{language}" language. 
+    template="""You must answer in the "{language}" language.
 
     For the given text: "{text}", you have the following objective "{query}".
-    
+
     Be as clear, informative, and descriptive as necessary.
-    You will not make up information or add any information outside of the above text. 
+    You will not make up information or add any information outside of the above text.
     Only use the given information and nothing more.
     """,
     input_variables=["query", "language", "text"],
 )
 
 summarize_with_sources_prompt = PromptTemplate(
-    template="""You must answer in the "{language}" language. 
+    template="""You must answer in the "{language}" language.
 
     Parse and summarize the following text snippets "{snippets}".
     Write using clear markdown formatting in a style expected of the goal "{goal}".
@@ -142,22 +142,24 @@ summarize_with_sources_prompt = PromptTemplate(
     answer the query: "{query}" as best as possible.
 
     Cite sources for as many sentences as possible by using the corresponding source link. Use the index as the citation text.
-    Incorporate the source using a markdown link directly at the end of the sentence that the source is used in. 
-    Do not separately list sources at the end of the writing. 
+    Incorporate the source using a markdown link directly at the end of the sentence that the source is used in.
+    Do not separately list sources at the end of the writing.
 
-    Example: "So this is a cited sentence at the end of a paragraph[1](https://test.com). This is another sentence." 
+    Example: "So this is a cited sentence at the end of a paragraph[1](https://test.com). This is another sentence."
     """,
     input_variables=["goal", "language", "query", "snippets"],
 )
 
 summarize_sid_prompt = PromptTemplate(
-    template="""You must answer in the "{language}" language. 
+    template="""You must answer in the "{language}" language.
 
     Parse and summarize the following text snippets "{snippets}".
     Write using clear markdown formatting in a style expected of the goal "{goal}".
     Be as clear, informative, and descriptive as necessary and attempt to
     answer the query: "{query}" as best as possible.
-    If any of the snippets are not relevant to the query, ignore them, and do not include them in the summary.
+    If any of the snippets are not relevant to the query,
+    ignore them, and do not include them in the summary.
+    Do not mention that you are ignoring them.
 
     If there is no information provided, say "There is nothing to summarize".
     """,
@@ -165,14 +167,14 @@ summarize_sid_prompt = PromptTemplate(
 )
 
 chat_prompt = PromptTemplate(
-    template="""You must answer in the "{language}" language. 
- 
-    You are a helpful AI Assistant that will provide responses based on the current conversation history. 
-    
+    template="""You must answer in the "{language}" language.
+
+    You are a helpful AI Assistant that will provide responses based on the current conversation history.
+
     The human will provide previous messages as context. Use ONLY this information for your responses.
     Do not make anything up and do not add any additional information.
-    If you have no information for a given question in the conversation history, 
-    say "I do not have any information on this". 
+    If you have no information for a given question in the conversation history,
+    say "I do not have any information on this".
     """,
     input_variables=["language"],
 )

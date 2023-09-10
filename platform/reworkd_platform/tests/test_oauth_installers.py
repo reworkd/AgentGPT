a4b7790f528c22302fb7d965857992c3856c667b
asim-shrestha <asim.shrestha@hotmail.com>
2023-09-10 13:42:27 -0700

    ✨ Fix oauth stuff

--- platform/reworkd_platform/tests/test_oauth_installers.py
+++ platform/reworkd_platform/tests/test_oauth_installers.py
@@ -5,7 +5,7 @@ from reworkd_platform.services.oauth_installers import installer_factory
 
 def test_installer_factory(mocker):
     crud = mocker.Mock()
-    installer_factory("slack", crud)
+    installer_factory("sid", crud)
 
 
 def test_integration_dne(mocker):

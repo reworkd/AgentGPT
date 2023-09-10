6a2d220db26c472306adb70e141388c9d02ad040
asim-shrestha <asim.shrestha@hotmail.com>
2023-09-10 12:59:25 -0700

    ✨ DELETE WORKFLOW STUFF

--- platform/reworkd_platform/web/api/auth/views.py
+++ platform/reworkd_platform/web/api/auth/views.py
@@ -1,6 +1,6 @@
-from typing import Annotated, Dict, List, Any, Optional
+from typing import Annotated, Any, Dict, List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Form
+from fastapi import APIRouter, Depends, Form, HTTPException
 from fastapi.responses import RedirectResponse
 from loguru import logger
 from pydantic import BaseModel
@@ -11,12 +11,8 @@ from reworkd_platform.db.crud.oauth import OAuthCrud
 from reworkd_platform.db.crud.organization import OrganizationCrud, OrganizationUsers
 from reworkd_platform.schemas import UserBase
 from reworkd_platform.schemas.user import OrganizationRole
-from reworkd_platform.services.oauth_installers import (
-    installer_factory,
-    OAuthInstaller,
-)
+from reworkd_platform.services.oauth_installers import OAuthInstaller, installer_factory
 from reworkd_platform.services.security import encryption_service
-from reworkd_platform.services.sockets import websockets
 from reworkd_platform.settings import settings
 from reworkd_platform.web.api.dependencies import get_current_user, get_organization
 from reworkd_platform.web.api.http_responses import not_found
@@ -31,33 +27,6 @@ async def organizations(
     if org := await crud.get_by_name(name):
         return org
     raise HTTPException(status_code=404)
-
-
-# @router.post("/organization")
-# async def create_organization():
-#     """Create an organization"""
-#     pass
-#
-#
-# @router.get("/organization/{organization_id}")
-# async def organization(organization_id: str):
-#     """Get an organization by ID"""
-#     pass
-#
-#
-# @router.put("/organization/{organization_id}")
-# async def update_organization(organization_id: str):
-#     """Update an organization by ID"""
-#     pass
-
-
-@router.post("/pusher")
-async def pusher_authentication(
-    channel_name: Annotated[str, Form()],
-    socket_id: Annotated[str, Form()],
-    user: UserBase = Depends(get_current_user),
-) -> Dict[str, str]:
-    return websockets.authenticate(user, channel_name, socket_id)
 
 
 @router.get("/{provider}")
@@ -111,24 +80,6 @@ async def sid_info(
 class Channel(BaseModel):
     name: str
     id: str
-
-
-@router.get("/slack/info")
-async def slack_channels(
-    role: OrganizationRole = Depends(get_organization),
-    crud: OAuthCrud = Depends(OAuthCrud.inject),
-) -> List[Channel]:
-    """Install an OAuth App"""
-    creds = await crud.get_installation_by_organization_id(
-        role.organization_id, "slack"
-    )
-
-    if not creds:
-        raise not_found()
-
-    token = encryption_service.decrypt(creds.access_token_enc)
-    client = WebClient(token=token)
-    return get_all_conversations(client)
 
 
 def get_all_conversations(client: WebClient) -> List[Channel]:

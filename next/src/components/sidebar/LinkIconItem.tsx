b760364050f53445cb29c9078311edf564e1b8ad
asim-shrestha <asim.shrestha@hotmail.com>
2023-09-10 16:59:24 -0700

    ✨light mode sidebar

--- next/src/components/sidebar/LinkIconItem.tsx
+++ next/src/components/sidebar/LinkIconItem.tsx
@@ -1,14 +1,9 @@
-import clsx from "clsx";
 import type { ReactNode } from "react";
 
 const LinkIconItem = (props: { children: ReactNode; href?: string; onClick: () => void }) => (
   <a
     href={props.href}
-    className={clsx(
-      "grid h-10 w-10 cursor-pointer place-items-center rounded-xl text-2xl",
-      "neutral-button-primary border",
-      "group group-hover:scale-110"
-    )}
+    className="group grid h-10 w-10 cursor-pointer place-items-center rounded-xl text-2xl group-hover:scale-110 group-hover:bg-slate-12"
     onClick={(e) => {
       e.preventDefault();
       props.onClick();

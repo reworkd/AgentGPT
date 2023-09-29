4026d5a3c061e0b7101cfd36cd16c4ad12ddcbb6
awtkns <32209255+awtkns@users.noreply.github.com>
2023-09-28 20:05:14 -0400

    🐛 Fix azure with custom key

--- platform/reworkd_platform/web/api/agent/model_factory.py
+++ platform/reworkd_platform/web/api/agent/model_factory.py
@@ -71,7 +71,16 @@ def get_base_and_headers(
     settings_: Settings, model_settings: ModelSettings, user: UserBase
 ) -> Tuple[str, Optional[Dict[str, str]], bool]:
     use_helicone = settings_.helicone_enabled and not model_settings.custom_api_key
-    base = settings_.helicone_api_base if use_helicone else settings_.openai_api_base
+    base = (
+        settings_.helicone_api_base
+        if use_helicone
+        else (
+            "https://api.openai.com/v1"
+            if model_settings.custom_api_key
+            else settings_.openai_api_base
+        )
+    )
+
     headers = (
         {
             "Helicone-Auth": f"Bearer {settings_.helicone_api_key}",
